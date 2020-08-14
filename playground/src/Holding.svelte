b3caae36654159fa1a902c98e2b2396feaaf20a7
Daniel Von Fange <daniel@leancoder.com>
2020-08-14 12:38:06 -0400

    Gratuitous animations of balance changes.

--- playground/src/Holding.svelte
+++ playground/src/Holding.svelte
@@ -1,14 +1,25 @@
 <script>
+    import { tweened } from 'svelte/motion';
+    import { cubicOut } from 'svelte/easing';
+    import { onDestroy } from 'svelte';
     export let token;
     export let holding;
-    $: amount = parseFloat($holding)
+    const progress = tweened(0.0, {
+		duration: 200,
+		easing: cubicOut
+    });
+    const unsubscribe = holding.subscribe(v => {
+		progress.set(parseFloat(v))
+    });
+    onDestroy(unsubscribe);
 </script>
 
-{#if amount > 0}
-    <tr>
+<style>
+    .fade{opacity:0.1}
+</style>
+
+    <tr class="{$progress==0?'fade':''}">
         <td>💰</td>
-        <td style="text-align:right">{amount.toLocaleString()}</td>
+        <td style="text-align:right">{$progress.toLocaleString(undefined, {minimumFractionDigits:2, maximumFractionDigits:2})}</td>
         <td>{token.toLowerCase()}</td>
-        <td />
     </tr>
-{/if}
