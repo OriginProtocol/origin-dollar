4a92197d4174ffcb4853ba090f89a81dca393553
Tom Linton <tlinton@fastmail.fm>
2020-08-16 13:06:18 +1200

    Test scaffolds

--- contracts/test/vault.js
+++ contracts/test/vault.js
@@ -1,5 +1,5 @@
-const { expect } = require("chai");
-const { defaultFixture } = require("./_fixture");
+const {expect} = require("chai");
+const {defaultFixture} = require("./_fixture");
 
 describe("Vault", function () {
   beforeEach(async () => {
@@ -7,7 +7,15 @@ describe("Vault", function () {
   });
 
   it("Should error when adding a market that already exists", async function () {
-    const { vault, usdt } = await waffle.loadFixture(defaultFixture);
+    const {vault, usdt} = await waffle.loadFixture(defaultFixture);
     await expect(vault.supportAsset(usdt.address)).to.be.reverted;
   });
+
+  it("Should deprecate an asset", async function () {});
+
+  it("Should correctly ratio deposited currencies of differing decimals", async function () {});
+
+  it("Should increase the totalBalance of the deposited asset", async function () {});
+
+  it("Should mint the correct amount of OUSD for varying priced assets", async function () {});
 });
