1e4534151d87ccb2016c85272db9fea7f44e3548
Daniel Von Fange <daniel@leancoder.com>
2020-08-17 09:19:53 -0400

    Test deposits of different currencies.

--- contracts/test/vault.js
+++ contracts/test/vault.js
@@ -1,11 +1,15 @@
 const { expect } = require("chai");
-const { defaultFixture } = require("./_fixture");
+const {
+  ousdUnits,
+  usdtUnits,
+  usdcUnits,
+  daiUnits,
+  tusdUnits,
+  defaultFixture,
+  expectBalance,
+} = require("./_fixture");
 
 describe("Vault", function () {
-  beforeEach(async () => {
-    await deployments.fixture();
-  });
-
   it("Should error when adding a market that already exists", async function () {
     const { vault, usdt } = await waffle.loadFixture(defaultFixture);
     await expect(vault.supportAsset(usdt.address)).to.be.reverted;
@@ -13,7 +17,23 @@ describe("Vault", function () {
 
   it("Should deprecate an asset", async function () {});
 
-  it("Should correctly ratio deposited currencies of differing decimals", async function () {});
+  it("Should correctly ratio deposited currencies of differing decimals", async function () {
+    const { ousd, vault, usdt, dai, matt } = await waffle.loadFixture(
+      defaultFixture
+    );
+
+    await expectBalance(ousd, matt, ousdUnits("100.0"));
+
+    // Matt deposits USDT, 6 decimals
+    await usdt.connect(matt).approve(vault.address, usdtUnits("2.0"));
+    await vault.connect(matt).depositAndMint(usdt.address, usdtUnits("2.0"));
+    await expectBalance(ousd, matt, ousdUnits("102.0"));
+
+    // Matt deposits DAI, 18 decimals
+    await dai.connect(matt).approve(vault.address, daiUnits("4.0"));
+    await vault.connect(matt).depositAndMint(dai.address, daiUnits("4.0"));
+    await expectBalance(ousd, matt, ousdUnits("106.0"));
+  });
 
   it("Should increase the totalBalance of the deposited asset", async function () {});
 
