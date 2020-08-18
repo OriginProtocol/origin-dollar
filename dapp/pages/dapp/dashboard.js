391214f743967f61cf347e55a5dfefbba0425073
Domen Grabec <grabec@gmail.com>
2020-08-18 15:00:21 +0200

    add some more dashboard helper methods

--- dapp/pages/dapp/dashboard.js
+++ dapp/pages/dapp/dashboard.js
@@ -14,18 +14,43 @@ const governorAddress = '0xeAD9C93b79Ae7C1591b1FB5323BD777E86e150d4'
 const Dashboard = ({ locale, onLocale }) => {
   const allowances = useStoreState(AccountStore, s => s.allowances)
   const balances = useStoreState(AccountStore, s => s.balances)
+
   const account = useStoreState(AccountStore, s => s.address)
-  const { Vault, MockUSDT } = useStoreState(ContractStore, s => s.contracts || {})
+
+  const { Vault, MockUSDT, MockDAI, MockTUSD, MockUSDC } = useStoreState(ContractStore, s => s.contracts || {})
 
   const isGovernor = account && account === governorAddress
 
+
+  const mintUSDT = async () => {
+    await MockUSDT.mint(
+      ethers.utils.parseUnits('1500.0', await MockUSDT.decimals())
+    )
+  }
+
+  const mintDai = async () => {
+    await MockDAI.mint(
+      ethers.utils.parseUnits('1500.0', await MockDAI.decimals())
+    )
+  }
+
+  const mintTusd = async () => {
+    await MockTUSD.mint(
+      ethers.utils.parseUnits('1500.0', await MockTUSD.decimals())
+    )
+  }
+
+  const mintUsdc = async () => {
+    await MockUSDC.mint(
+      ethers.utils.parseUnits('1500.0', await MockUSDC.decimals())
+    )
+  }
 
   const buyOusd = async () => {
     await Vault.depositAndMint(
       MockUSDT.address,
       ethers.utils.parseUnits('100.0', await MockUSDT.decimals())
     )
-    //await loadBalances()
   }
 
   const depositYield = async () => {
@@ -33,7 +58,6 @@ const Dashboard = ({ locale, onLocale }) => {
       MockUSDT.address,
       ethers.utils.parseUnits('10.0', await MockUSDT.decimals())
     )
-    //await loadBalances()
   }
 
   const tableRows = () => {
@@ -81,6 +105,18 @@ const Dashboard = ({ locale, onLocale }) => {
 	              Deposit $10 Yield
 	            </div>
 	          )}
+            <div className="btn btn-primary my-4 mr-3" onClick={mintUSDT}>
+              Mint USDT
+            </div>
+            <div className="btn btn-primary my-4 mr-3" onClick={mintDai}>
+              Mint DAI
+            </div>
+            <div className="btn btn-primary my-4 mr-3" onClick={mintTusd}>
+              Mint TUSD
+            </div>
+            <div className="btn btn-primary my-4 mr-3" onClick={mintUsdc}>
+              Mint USDC
+            </div>
 	          <div className="btn btn-primary my-4 mr-3" onClick={buyOusd}>
 	            Buy OUSD
 	          </div>
