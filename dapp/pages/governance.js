f3e1472fc7896b1150ba205211faeda32549865a
Micah Alcorn <mail@micahalcorn.com>
2020-08-26 12:35:23 -0700

    Replace missing graphic

--- dapp/pages/governance.js
+++ dapp/pages/governance.js
@@ -301,7 +301,7 @@ export default function Governance({ locale, onLocale }) {
           <div className="row">
             <div className="col offset-md-2 col-md-4">
               <div className="image-container">
-                <img src="/images/ogn-icon.svg" alt="Origin Token (OGN) icon" />
+                <img src="/images/convert-icon.svg" alt="Token conversion icon" />
               </div>
               <div className="action">
                 {
