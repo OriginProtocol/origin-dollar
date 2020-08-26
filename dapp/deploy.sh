62eb70c3729508a0ee9143fdb420919db650e460
Franck <franck@originprotocol.com>
2020-08-26 13:56:23 -0700

    AppEngine deployment (#101)
    * add support for env vars
    
    * add prod.network.json
    
    * remove prod.env

---
+++ dapp/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+mode=$1
+branch=$(git branch | awk '/\*/ { print $2; }') 
+
+if [ $branch != "master" ]; then
+  if [ $mode == "staging" ]; then
+    echo "Deploying non-master branch '$branch'"
+  else
+    echo "You are on '$branch'. Switch to 'master' branch before you deploy"
+    exit 1
+  fi
+fi
+
+echo "Decrypting secrets..."
+npm run decrypt-secrets:$mode
+echo "Starting deployment..."
+npm run deploy:$mode
+echo "Deployment done."
