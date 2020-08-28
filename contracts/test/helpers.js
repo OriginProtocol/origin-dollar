28c5656336eb50681a457c5562120e6995ff8cb1
Tom Linton <tlinton@fastmail.fm>
2020-08-28 20:46:26 +1200

    Update helper

--- contracts/test/helpers.js
+++ contracts/test/helpers.js
@@ -80,7 +80,7 @@ async function expectApproxSupply(contract, expected, message) {
   chai.expect(balance, message).lt(expected.mul("1001").div("1000"));
 }
 
-const isGanacheFork = bre.network.name === "fork";
+const isGanacheFork = process.env.FORK === "true";
 
 // The coverage network soliditycoverage uses Ganache
 const isGanache =
