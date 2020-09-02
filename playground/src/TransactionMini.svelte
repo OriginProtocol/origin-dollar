6a9d2f1187ac5af844a2b79d499d1d929c109bbe
Daniel Von Fange <daniel@leancoder.com>
2020-09-02 12:25:17 -0400

    Show transactions in playground

---
+++ playground/src/TransactionMini.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let tx;
+</script>
+
+<tr on:click={() => console.log(tx)}>
+  <td>⁍</td>
+  <td>{tx.blockNumber}</td>
+  <td>{tx.userName}</td>
+  <td>{tx.contractName}</td>
+  <td>{tx.methodName}</td>
+  <td style="text-align:right">
+    {tx.reciept.gasUsed.toNumber().toLocaleString()}
+  </td>
+</tr>
