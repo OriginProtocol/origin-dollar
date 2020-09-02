9f20b36514d1bb2f80d92263b178724b64edf862
Domen Grabec <grabec@gmail.com>
2020-09-02 15:35:54 +0200

    add etherscan links

--- dapp/src/components/AccountListener.js
+++ dapp/src/components/AccountListener.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import ethers from 'ethers'
 import { useCookies } from 'react-cookie'
+import { useWeb3React } from '@web3-react/core'
 
 import { AccountStore } from 'stores/AccountStore'
 import { usePrevious } from 'utils/hooks'
 import { isCorrectNetwork } from 'utils/web3'
-import { useWeb3React } from '@web3-react/core'
 import { useStoreState } from 'pullstate'
 import { setupContracts } from 'utils/contracts'
 import { login } from 'utils/account'
