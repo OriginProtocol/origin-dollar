85c0cebc0e67b20c5746cc646de62b959b533128
Tom Linton <tlinton@fastmail.fm>
2020-09-03 15:17:11 +1200

    Move mockVault fixture tests

--- contracts/test/vault/rebase.js
+++ contracts/test/vault/rebase.js
@@ -4,6 +4,7 @@ const {
   compoundVaultFixture,
 } = require("../_fixture");
 const { expect } = require("chai");
+const { utils } = require("ethers");
 
 const {
   ousdUnits,
@@ -98,26 +99,6 @@ describe("Vault rebasing", async () => {
     );
     await expect(matt).has.a.balanceOf("300.00", ousd);
   });
-
-  /*
-  it("Should increase users balance on rebase after increased Vault value", async () => {
-    const { vault, matt, ousd, josh } = await loadFixture(mockVaultFixture);
-    // Total OUSD supply is 200, mock an increase
-    await vault.setTotalValue(utils.parseUnits("220", 18));
-    await vault.rebase();
-    await expect(matt).has.an.approxBalanceOf("110.00", ousd);
-    await expect(josh).has.an.approxBalanceOf("110.00", ousd);
-  });
-
-  it("Should decrease users balance on rebase after decreased Vault value", async () => {
-    const { vault, matt, ousd, josh } = await loadFixture(mockVaultFixture);
-    // Total OUSD supply is 200, mock a decrease
-    await vault.setTotalValue(utils.parseUnits("180", 18));
-    await vault.rebase();
-    await expect(matt).has.an.approxBalanceOf("90.00", ousd);
-    await expect(josh).has.an.approxBalanceOf("90.00", ousd);
-  });
-  */
 
   it("Should alter balances after supported asset deposited and rebase called", async () => {
     let { ousd, vault, matt, usdc, josh } = await loadFixture(defaultFixture);
