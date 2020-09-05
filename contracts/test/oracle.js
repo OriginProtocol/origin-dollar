e83c998f0d8688d5895d656bcc43c8c225762f42
Franck Chastagnol <franck@originprotocol.com>
2020-09-04 22:19:48 -0700

    Add more mix oracle test cases

--- contracts/test/oracle.js
+++ contracts/test/oracle.js
@@ -15,10 +15,13 @@ const feedDecimals = {
 
 // The oracle returns prices in ETH with 6 digits, but 8 digits for tokens.
 const oracleDecimals = {
-  ETH: 6,
-  DAI: 8,
-  USDC: 8,
-  USDT: 8
+  ETH_USD: 6,
+  DAI_ETH: 8,
+  DAI_USD: 6,
+  USDC_ETH: 8,
+  USDC_USD: 6,
+  USDT_ETH: 8,
+  USDT_USD: 6,
 }
 
 const feedPrices = {
@@ -26,20 +29,20 @@ const feedPrices = {
   ETH_USD: parseUnits("370", feedDecimals.ETH),
   // 1 DAI = 1/370 ETH = 0.002702 ETH = ~$0.999740
   DAI_ETH: parseUnits("0.002702", feedDecimals.DAI),
-  // 1 USDT = 1/360 ETH = 0.002777 ETH = ~$1.027490
-  USDT_ETH: parseUnits("0.002777", feedDecimals.USDT),
   // 1 USDC = 1/250 ETH = 0.004 ETH = $1.48
   USDC_ETH: parseUnits("0.004", feedDecimals.USDC),
+  // 1 USDT = 1/360 ETH = 0.002777 ETH = ~$1.027490
+  USDT_ETH: parseUnits("0.002777", feedDecimals.USDT),
 }
 
 const oraclePrices = {
-  ETH_USD: parseUnits("370", oracleDecimals.ETH),
-  DAI_ETH: parseUnits("0.002702", oracleDecimals.DAI),
-  DAI_USD: parseUnits("0.999740", oracleDecimals.DAI),
-  USDT_ETH: parseUnits("0.002777", oracleDecimals.USDT),
-  USDT_USD: parseUnits("1.027490", oracleDecimals.USDT),
-  USDC_ETH: parseUnits("0.004", oracleDecimals.USDC),
-  USDC_USD: parseUnits("1.48", oracleDecimals.USDC),
+  ETH_USD: parseUnits("370", oracleDecimals.ETH_USD),
+  DAI_ETH: parseUnits("0.002702", oracleDecimals.DAI_ETH),
+  DAI_USD: parseUnits("0.999740", oracleDecimals.DAI_USD),
+  USDC_ETH: parseUnits("0.004", oracleDecimals.USDC_ETH),
+  USDC_USD: parseUnits("1.48", oracleDecimals.USDC_USD),
+  USDT_ETH: parseUnits("0.002777", oracleDecimals.USDT_ETH),
+  USDT_USD: parseUnits("1.027490", oracleDecimals.USDT_USD),
 }
 
 describe("Oracle", function () {
@@ -51,21 +54,21 @@ describe("Oracle", function () {
     const {
       chainlinkOracleFeedETH,
       chainlinkOracleFeedDAI,
+      chainlinkOracleFeedUSDC,
       chainlinkOracleFeedUSDT,
-      chainlinkOracleFeedUSDC
     } = await loadFixture(defaultFixture);
 
-    await chainlinkOracleFeedETH.setPrice(feedPrices.ETH_USD)
-    await chainlinkOracleFeedETH.setDecimals(feedDecimals.ETH)
+    await chainlinkOracleFeedETH.setPrice(feedPrices.ETH_USD);
+    await chainlinkOracleFeedETH.setDecimals(feedDecimals.ETH);
 
-    await chainlinkOracleFeedDAI.setPrice(feedPrices.DAI_ETH)
-    await chainlinkOracleFeedDAI.setDecimals(feedDecimals.DAI)
+    await chainlinkOracleFeedDAI.setPrice(feedPrices.DAI_ETH);
+    await chainlinkOracleFeedDAI.setDecimals(feedDecimals.DAI);
 
-    await chainlinkOracleFeedUSDT.setPrice(feedPrices.USDT_ETH)
-    await chainlinkOracleFeedUSDT.setDecimals(feedDecimals.USDT)
+    await chainlinkOracleFeedUSDC.setPrice(feedPrices.USDC_ETH);
+    await chainlinkOracleFeedUSDC.setDecimals(feedDecimals.USDC);
 
-    await chainlinkOracleFeedUSDC.setPrice(feedPrices.USDC_ETH)
-    await chainlinkOracleFeedUSDC.setDecimals(feedDecimals.USDC)
+    await chainlinkOracleFeedUSDT.setPrice(feedPrices.USDT_ETH);
+    await chainlinkOracleFeedUSDT.setDecimals(feedDecimals.USDT);
   }
 
   it("Should allow multiple prices to be set and read", async () => {
@@ -83,14 +86,15 @@ describe("Oracle", function () {
 
     expect(await chainlinkOracle.ethUsdPrice()).to.eq(oraclePrices.ETH_USD);
     expect(await chainlinkOracle.tokEthPrice("DAI")).to.eq(oraclePrices.DAI_ETH);
-    expect(await chainlinkOracle.tokEthPrice("USDT")).to.eq(oraclePrices.USDT_ETH);
     expect(await chainlinkOracle.tokEthPrice("USDC")).to.eq(oraclePrices.USDC_ETH);
+    expect(await chainlinkOracle.tokEthPrice("USDT")).to.eq(oraclePrices.USDT_ETH);
   });
 
   it("Mix oracle", async () => {
     const { mixOracle } = await loadFixture(defaultFixture);
     await initFeeds()
 
+    // Test ETH prices.
     let [min, max] = await mixOracle.priceEthMinMax();
     expect(min).to.eq(oraclePrices.ETH_USD);
     expect(max).to.eq(oraclePrices.ETH_USD);
@@ -106,6 +110,23 @@ describe("Oracle", function () {
     [min, max] = await mixOracle.priceTokEthMinMax("USDC");
     expect(min).to.eq(oraclePrices.USDC_ETH);
     expect(max).to.eq(oraclePrices.USDC_ETH);
+
+    // Test USD prices.
+    [min, max] = await mixOracle.priceMinMax("ETH");
+    expect(min).to.eq(oraclePrices.ETH_USD);
+    expect(max).to.eq(oraclePrices.ETH_USD);
+
+    [min, max] = await mixOracle.priceMinMax("DAI");
+    expect(min).to.eq(oraclePrices.DAI_USD);
+    expect(max).to.eq(oraclePrices.DAI_USD);
+
+    [min, max] = await mixOracle.priceMinMax("USDT");
+    expect(min).to.eq(oraclePrices.USDT_USD);
+    expect(max).to.eq(oraclePrices.USDT_USD);
+
+    [min, max] = await mixOracle.priceMinMax("USDC");
+    expect(min).to.eq(oraclePrices.USDC_USD);
+    expect(max).to.eq(oraclePrices.USDC_USD);
   });
 
 });
