61cfa39f70b83f78aef5d84788411d3f6bc6b9ef
Tom Linton <tlinton@fastmail.fm>
2020-09-18 16:06:40 +1200

    Update slither script

--- contracts/scripts/utils/slither.sh
+++ contracts/scripts/utils/slither.sh
@@ -2,4 +2,5 @@
 
 slither . \
     --exclude-dependencies \
-    --exclude conformance-to-solidity-naming-conventions
+    --exclude conformance-to-solidity-naming-conventions,different-pragma-directives-are-used \
+    --filter-paths=@openzeppelin
