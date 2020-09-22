bd83a125ae86a10421894625f8f9cbf0a8531c4d
Domen Grabec <grabec@gmail.com>
2020-09-23 00:15:21 +0200

    automatically redirect to http when not in localhost mode

--- dapp/pages/_document.js
+++ dapp/pages/_document.js
@@ -13,6 +13,16 @@ class MyDocument extends Document {
           <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous" />
           <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous" />
           <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous" />
+          {/* If not on localhost and request's protocl was HTTP, redirect to HTTPS */}
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            var href = window.location.href;
+            var isLocal = /^http:\\/\\/localhost(.*)$/.exec(href);
+            var http = /^http:\\/\\/(.*)$/.exec(href);
+            if (!isLocal && http) {
+              window.location.replace('https://' + http[1]);
+            }
+          `}} />
           {!FULLSTORY_ORG_ID ? null : (
             <script dangerouslySetInnerHTML={{ __html: `
               window['_fs_debug'] = false;
