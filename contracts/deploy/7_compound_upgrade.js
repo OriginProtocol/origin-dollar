529d55e2bcc726495fee22dd1408b3622f68da43
Tom Linton <tlinton@fastmail.fm>
2020-09-22 12:55:40 +1200

    Add COMP upgrade script

---
+++ contracts/deploy/7_compound_upgrade.js
@@ -0,0 +1,72 @@
+const addresses = require("../utils/addresses");
+const { isMainnet, isRinkeby } = require("../test/helpers.js");
+const { getTxOpts } = require("../utils/tx");
+
+let totalDeployGasUsed = 0;
+
+// Wait for 3 blocks confirmation on Mainnet/Rinkeby.
+const NUM_CONFIRMATIONS = isMainnet || isRinkeby ? 3 : 0;
+
+function log(msg, deployResult = null) {
+  if (isMainnet || isRinkeby || process.env.VERBOSE) {
+    if (deployResult) {
+      const gasUsed = Number(deployResult.receipt.gasUsed.toString());
+      totalDeployGasUsed += gasUsed;
+      msg += ` Address: ${deployResult.address} Gas Used: ${gasUsed}`;
+    }
+    console.log("INFO:", msg);
+  }
+}
+
+const upgradeCompound = async ({ getNamedAccounts, deployments }) => {
+  let transaction;
+
+  const { deploy } = deployments;
+  const { governorAddr, deployerAddr } = await getNamedAccounts();
+
+  console.log("Running 6_compound_upgrade deployment...");
+
+  const sGovernor = ethers.provider.getSigner(governorAddr);
+
+  // Deploy a new vault.
+  const dCompoundStrategy = await deploy("CompoundStrategy", {
+    from: deployerAddr,
+    ...(await getTxOpts()),
+  });
+  await ethers.provider.waitForTransaction(
+    dCompoundStrategy.receipt.transactionHash,
+    NUM_CONFIRMATIONS
+  );
+  log("Deployed Compound strategy", dCompoundStrategy);
+
+  // Update the proxy to use the new vault.
+  const cCompoundStrategyProxy = await ethers.getContract("VaultProxy");
+  transaction = await cCompoundStrategyProxy
+    .connect(sGovernor)
+    .upgradeTo(dCompoundStrategy.address);
+  await ethers.provider.waitForTransaction(transaction.hash, NUM_CONFIRMATIONS);
+  log("Upgraded proxy to use new Compound strategy");
+
+  const cCompoundStrategy = await ethers.getContractAt(
+    "CompoundStrategy",
+    cCompoundStrategyProxy.address
+  );
+
+  // Note no COMP on Rinkeby per https://compound.finance/docs
+  await cCompoundStrategy
+    .connect(sGovernor)
+    .setRewardTokenAddress(addresses.mainnet.COMP);
+  log("Set reward token address on Compound strategy");
+
+  console.log(
+    "6_compound_upgrade deploy done. Total gas used for deploys:",
+    totalDeployGasUsed
+  );
+
+  return true;
+};
+
+upgradeCompound.dependencies = ["core"];
+upgradeCompound.skip = () => !(isMainnet || isRinkeby);
+
+module.exports = upgradeCompound;
