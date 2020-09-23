3e1bb6d0a8a5160454c17c858ba63713925792d4
Yu Pan <yupan@originprotocol.com>
2020-09-23 16:55:49 -0700

    New storage contracts that makes the Core Vault contract about 20% smaller

---
+++ contracts/contracts/vault/VaultStorage.sol
@@ -0,0 +1,80 @@
+pragma solidity 0.5.11;
+
+/**
+ * @title OUSD VaultStorage Contract
+ * @notice The VaultStorage contract defines the storage for the Vault contracts
+ * @author Origin Protocol Inc
+ */
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import {
+    Initializable
+} from "@openzeppelin/upgrades/contracts/Initializable.sol";
+
+import { IStrategy } from "../interfaces/IStrategy.sol";
+import { Governable } from "../governance/Governable.sol";
+import { OUSD } from "../token/OUSD.sol";
+import "../utils/Helpers.sol";
+import { StableMath } from "../utils/StableMath.sol";
+
+contract VaultStorage is Initializable, Governable {
+    using SafeMath for uint256;
+    using StableMath for uint256;
+    using SafeMath for int256;
+    using SafeERC20 for IERC20;
+
+    event AssetSupported(address _asset);
+    event StrategyAdded(address _addr);
+    event StrategyRemoved(address _addr);
+    event Mint(address _addr, uint256 _value);
+    event Redeem(address _addr, uint256 _value);
+    event StrategyWeightsUpdated(
+        address[] _strategyAddresses,
+        uint256[] weights
+    );
+    event DepositsPaused();
+    event DepositsUnpaused();
+
+    // Assets supported by the Vault, i.e. Stablecoins
+    struct Asset {
+        bool isSupported;
+    }
+    mapping(address => Asset) assets;
+    address[] allAssets;
+
+    // Strategies supported by the Vault
+    struct Strategy {
+        bool isSupported;
+        uint256 targetWeight; // 18 decimals. 100% = 1e18
+    }
+    mapping(address => Strategy) strategies;
+    address[] allStrategies;
+
+    // Address of the Oracle price provider contract
+    address public priceProvider;
+    // Pausing bools
+    bool public rebasePaused = false;
+    bool public depositPaused = true;
+    // Redemption fee in basis points
+    uint256 public redeemFeeBps;
+    // Buffer of assets to keep in Vault to handle (most) withdrawals
+    uint256 public vaultBuffer;
+    // Mints over this amount automatically allocate funds. 18 decimals.
+    uint256 public autoAllocateThreshold;
+    // Mints over this amount automatically rebase. 18 decimals.
+    uint256 public rebaseThreshold;
+
+    OUSD oUSD;
+
+    bytes32 constant adminImplPosition = 0xa2bd3d3cf188a41358c8b401076eb59066b09dec5775650c0de4c55187d17bd9;
+    //keccak256("OUSD.vault.governor.admin.impl");
+
+    function setConfigImpl(address newImpl) external onlyGovernor {
+        bytes32 position = adminImplPosition;
+        assembly {
+            sstore(position, newImpl)
+        }
+    }
+}
