1a6b7037e21cda783a26296cc061a9113583923f
Tom Linton <tlinton@fastmail.fm>
2020-09-24 14:38:29 +1200

    Change function ordering

--- contracts/contracts/vault/VaultAdmin.sol
+++ contracts/contracts/vault/VaultAdmin.sol
@@ -222,12 +222,6 @@ contract VaultAdmin is VaultStorage {
                     Pricing
     ****************************************/
 
-    function _priceUSDMint(string memory symbol) internal returns (uint256) {
-        // Price from Oracle is returned with 8 decimals
-        // scale to 18 so 18-8=10
-        return IMinMaxOracle(priceProvider).priceMin(symbol).scaleBy(10);
-    }
-
     /**
      * @dev Returns the total price in 18 digit USD for a given asset.
      *      Using Min since min is what we use for mint pricing
@@ -240,14 +234,14 @@ contract VaultAdmin is VaultStorage {
 
     /**
      * @dev Returns the total price in 18 digit USD for a given asset.
-     *      Using Max since max is what we use for redeem pricing
+     *      Using Min since min is what we use for mint pricing
      * @param symbol String symbol of the asset
      * @return uint256 USD price of 1 of the asset
      */
-    function _priceUSDRedeem(string memory symbol) internal returns (uint256) {
+    function _priceUSDMint(string memory symbol) internal returns (uint256) {
         // Price from Oracle is returned with 8 decimals
         // scale to 18 so 18-8=10
-        return IMinMaxOracle(priceProvider).priceMax(symbol).scaleBy(10);
+        return IMinMaxOracle(priceProvider).priceMin(symbol).scaleBy(10);
     }
 
     /**
@@ -260,5 +254,17 @@ contract VaultAdmin is VaultStorage {
         // Price from Oracle is returned with 8 decimals
         // scale to 18 so 18-8=10
         return _priceUSDRedeem(symbol);
+    }
+
+    /**
+     * @dev Returns the total price in 18 digit USD for a given asset.
+     *      Using Max since max is what we use for redeem pricing
+     * @param symbol String symbol of the asset
+     * @return uint256 USD price of 1 of the asset
+     */
+    function _priceUSDRedeem(string memory symbol) internal returns (uint256) {
+        // Price from Oracle is returned with 8 decimals
+        // scale to 18 so 18-8=10
+        return IMinMaxOracle(priceProvider).priceMax(symbol).scaleBy(10);
     }
 }
