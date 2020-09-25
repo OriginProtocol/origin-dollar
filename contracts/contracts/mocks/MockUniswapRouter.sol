df020d2837ec3576190fdf187ffb8ae469ba84e8
Tom Linton <tlinton@fastmail.fm>
2020-09-25 15:18:04 +1200

    Remove console import

--- contracts/contracts/mocks/MockUniswapRouter.sol
+++ contracts/contracts/mocks/MockUniswapRouter.sol
@@ -1,6 +1,5 @@
 pragma solidity 0.5.11;
 
-import "@nomiclabs/buidler/console.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { IUniswapV2Router } from "../interfaces/uniswap/IUniswapV2Router02.sol";
