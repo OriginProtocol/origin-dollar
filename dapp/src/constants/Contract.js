50002d6948fe3f7510e6ea473cdbb8a57c6117f3
Micah Alcorn <mail@micahalcorn.com>
2020-10-29 15:06:08 -0700

    Add more buffer to default gas limits

--- dapp/src/constants/Contract.js
+++ dapp/src/constants/Contract.js
@@ -12,17 +12,17 @@ export const currencies = {
 
 export const gasLimits = {
   // simple mint involving a single coin
-  MINT_GAS_LIMIT: 184989,
+  MINT_GAS_LIMIT: 193398,
   // simple mint involving multiple coins
-  MINT_MULTIPLE_GAS_LIMIT: 436247,
+  MINT_MULTIPLE_GAS_LIMIT: 456076,
   // when the amount minted using a single coin triggers the rebase function and not the allocate function
-  MINT_REBASE_GAS_LIMIT: 687074,
+  MINT_REBASE_GAS_LIMIT: 718305,
   // when the amount minted using multiple coins triggers the rebase function and not the allocate function
   MINT_MULTIPLE_REBASE_GAS_LIMIT: 952760,
   // when the amount minted using a single coin triggers the allocate function
-  MINT_ALLOCATE_GAS_LIMIT: 3404875,
+  MINT_ALLOCATE_GAS_LIMIT: 2889998,
   // when the amount minted using multiple coins triggers the allocate function
-  MINT_MULTIPLE_ALLOCATE_GAS_LIMIT: 3704875,
+  MINT_MULTIPLE_ALLOCATE_GAS_LIMIT: 2989998,
   // redeem/redeemAll gas limit
   REDEEM_GAS_LIMIT: 826804,
   // when the amount redeemed triggers the rebase function
