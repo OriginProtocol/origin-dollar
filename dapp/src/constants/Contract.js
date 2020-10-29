6e3a02a1b63d9ae5eb371ec5bcbae4cb4dd0f8d0
Micah Alcorn <mail@micahalcorn.com>
2020-10-29 00:22:07 -0700

    Reduce default gas limits for reversion to Compound

--- dapp/src/constants/Contract.js
+++ dapp/src/constants/Contract.js
@@ -12,19 +12,19 @@ export const currencies = {
 
 export const gasLimits = {
   // simple mint involving a single coin
-  MINT_GAS_LIMIT: 235343,
+  MINT_GAS_LIMIT: 184989,
   // simple mint involving multiple coins
-  MINT_MULTIPLE_GAS_LIMIT: 556138,
+  MINT_MULTIPLE_GAS_LIMIT: 436247,
   // when the amount minted using a single coin triggers the rebase function and not the allocate function
-  MINT_REBASE_GAS_LIMIT: 732727,
+  MINT_REBASE_GAS_LIMIT: 687074,
   // when the amount minted using multiple coins triggers the rebase function and not the allocate function
-  MINT_MULTIPLE_REBASE_GAS_LIMIT: 988882,
+  MINT_MULTIPLE_REBASE_GAS_LIMIT: 952760,
   // when the amount minted using a single coin triggers the allocate function
-  MINT_ALLOCATE_GAS_LIMIT: 4404875,
+  MINT_ALLOCATE_GAS_LIMIT: 3404875,
   // when the amount minted using multiple coins triggers the allocate function
-  MINT_MULTIPLE_ALLOCATE_GAS_LIMIT: 4704875,
+  MINT_MULTIPLE_ALLOCATE_GAS_LIMIT: 3704875,
   // redeem/redeemAll gas limit
-  REDEEM_GAS_LIMIT: 1830103,
+  REDEEM_GAS_LIMIT: 826804,
   // when the amount redeemed triggers the rebase function
-  REDEEM_REBASE_GAS_LIMIT: 2777548,
+  REDEEM_REBASE_GAS_LIMIT: 1859856,
 }
