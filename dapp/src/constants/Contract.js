1d3eee36a58528be0ce76f35589d85c1d44d48a7
Micah Alcorn <mail@micahalcorn.com>
2020-11-13 10:37:49 -0800

    Switch to estimated gas with buffer

--- dapp/src/constants/Contract.js
+++ dapp/src/constants/Contract.js
@@ -9,22 +9,3 @@ export const currencies = {
     localStorageSettingKey: 'usdc-manual-setting',
   },
 }
-
-export const gasLimits = {
-  // simple mint involving a single coin
-  MINT_GAS_LIMIT: 201806,
-  // simple mint involving multiple coins
-  MINT_MULTIPLE_GAS_LIMIT: 475906,
-  // when the amount minted using a single coin triggers the rebase function and not the allocate function
-  MINT_REBASE_GAS_LIMIT: 749536,
-  // when the amount minted using multiple coins triggers the rebase function and not the allocate function
-  MINT_MULTIPLE_REBASE_GAS_LIMIT: 1039374,
-  // when the amount minted using a single coin triggers the allocate function
-  MINT_ALLOCATE_GAS_LIMIT: 3052725,
-  // when the amount minted using multiple coins triggers the allocate function
-  MINT_MULTIPLE_ALLOCATE_GAS_LIMIT: 3152725,
-  // redeem/redeemAll gas limit
-  REDEEM_GAS_LIMIT: 901968,
-  // when the amount redeemed triggers the rebase function
-  REDEEM_REBASE_GAS_LIMIT: 2028934,
-}
