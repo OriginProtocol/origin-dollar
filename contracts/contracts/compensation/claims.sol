5451d8fca0fc49e9cb4bf70ebde556c1f20ffb41
Daniel Von Fange <daniel@leancoder.com>
2020-12-03 10:26:11 -0500

    Initial design for CompensationClaim contract and tests.

---
+++ contracts/contracts/compensation/claims.sol
@@ -0,0 +1,46 @@
+pragma solidity 0.5.11;
+
+import {
+    Initializable
+} from "@openzeppelin/upgrades/contracts/Initializable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { Governable } from "../governance/Governable.sol";
+
+contract CompensationClaims is Initializable, Governable {
+    address adjuster;
+    address token;
+
+    function initialize(address _adjuster, address _token)
+        external
+        onlyGovernor
+        initializer
+    {
+        token = token;
+        adjuster = adjuster;
+    }
+
+    function balanceOf() external view returns (uint256) {
+        return 0;
+    }
+
+    function totalClaims() external view returns (uint256) {
+        return 0;
+    }
+
+    function claim() external onlyInClaimPeriod;
+
+    function setClaims(address[] _users, uint256[] _amounts)
+        external
+        onlyUnlockedAdjuster
+        notInClaimPeriod;
+
+    function lockAdjuster() external onlyGovernor notInClaimPeriod;
+
+    function unlockAdjuster() external onlyGovernor notInClaimPeriod;
+
+    function start(uint256 _endTime) external onlyGovernor notInClaimPeriod;
+
+    function collect(address _coin) external onlyGovernor notInClaimPeriod;
+}
---
+++ contracts/test/compensation/compensationClaims.js
@@ -0,0 +1,67 @@
+const { defaultFixture } = require("../_fixture");
+const { expect } = require("chai");
+const { utils, BigNumber } = require("ethers");
+const {
+  ognUnits,
+  advanceTime,
+  loadFixture,
+  expectApproxSupply,
+  isGanacheFork,
+} = require("../helpers");
+
+const day = 24 * 60 * 60;
+const year = 360 * day;
+
+
+// const { ogn, anna, governor, ognStaking } = await loadFixture(
+//     defaultFixture
+//   );
+
+describe("Compensation Claims", async() => {
+    if (isGanacheFork) {
+      this.timeout(0);
+    }
+    
+    describe("User claims", async ()=> {
+        it("should show a user their funds")
+        it("should show total of all funds")
+        it("should not allow a user to withdraw their funds before the claim period")
+        it("should allow a user to withdraw their funds after the start of the claim period")
+        it("should allow a user to withdraw their funds just before the end of the claim period")
+        it("should not allow a user to withdraw their funds after the claim period")
+        it("should throw if the user never had a claim")
+        it("should throw if the user has already claimed their funds")
+    })
+
+    describe("Adjuster", async ()=> {
+        it("should set one claim amount")
+        it("should set multiple claim amounts")
+        it("should not be able to overflow the total amount")
+        it("should not be able to set mismatching addresses and amounts")
+
+        it("should not be able to set claims before being unlocked")
+        it("should not be able to set claims after being locked")
+        it("should not be able to set claims during the claim period")
+    })
+
+    describe("Admin", async ()=> {
+        it("should be able to unlock adjuster")
+        it("should be able to lock adjuster")
+        it("should not be able to unlock adjuster during claims period")
+        it("should not be able to lock adjuster during claims period")
+
+        it("should be able to start a claims period")
+        it("should not be able to start a claims period with insufficient funds")
+        it("should not be able to start a claims period if a claim period is running")
+        it("should not be able to start a claims period if adjuster is unlocked")
+        it("should not be able to start a claims period if end time is too far in the future")
+        it("should not be able to start a claims period if end time is in past")
+
+        it("should be able to collect before claims period")
+        it("should be able to collect after claims period")
+        it("should not be able to collect during claims period")
+    })
+}
+
+    
+      
