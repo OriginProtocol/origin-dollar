412b790b80b72ae4cac466608f7b21885fb565ae
Nick Geoca <nickgeoca@gmail.com>
2020-12-04 15:21:55 -0800

    Erc20 balances updating based on events

--- dapp/src/components/AccountListener.js
+++ dapp/src/components/AccountListener.js
@@ -25,148 +25,121 @@ const AccountListener = (props) => {
     return ethers.utils.formatUnits(balance, await contract.decimals())
   }
 
-  const startEventListening = () => {
-    function start() {
-      const { usdt, dai, usdc, ousd, vault } = contracts
-      const rpcProvider = props.rpcProvider
-
-      // Setup event listening
-      rpcProvider.on(
-        dai.filters.Approval(account, vault.address, null),
-        (result) =>
-          displayCurrency(result.data, dai).then((b) =>
-            AccountStore.update((s) => {
-              s.allowances.dai = b
-            })
-          )
-      )
-      rpcProvider.on(
-        usdt.filters.Approval(account, vault.address, null),
-        (result) =>
-          displayCurrency(result.data, usdt).then((b) =>
-            AccountStore.update((s) => {
-              s.allowances.usdt = b
-            })
-          )
-      )
-      rpcProvider.on(
-        usdc.filters.Approval(account, vault.address, null),
-        (result) =>
-          displayCurrency(result.data, usdc).then((b) =>
-            AccountStore.update((s) => {
-              s.allowances.usdc = b
-            })
-          )
-      )
-      rpcProvider.on(
-        ousd.filters.Approval(account, vault.address, null),
-        (result) =>
-          displayCurrency(result.data, ousd).then((b) =>
-            AccountStore.update((s) => {
-              s.allowances.ousd = b
-            })
-          )
-      )
-
-      // Poll one time
-      Promise.all([
-        ousd
-          .allowance(account, vault.address)
-          .then((b) => displayCurrency(b, ousd)),
-        usdt
-          .allowance(account, vault.address)
-          .then((b) => displayCurrency(b, usdt)),
-        dai
-          .allowance(account, vault.address)
-          .then((b) => displayCurrency(b, dai)),
-        usdc
-          .allowance(account, vault.address)
-          .then((b) => displayCurrency(b, usdc)),
-      ])
-        .then((balances) =>
-          AccountStore.update((s) => {
-            s.allowances = {
-              ousd: balances[0],
-              usdt: balances[1],
-              dai: balances[2],
-              usdc: balances[3],
-            }
-          })
-        )
-        .catch((e) =>
-          console.error(
-            'AccountListener.js error - can not load account balances: ',
-            e
-          )
-        )
-    }
-
-    function asyncStartWhenReady() {
-      if (!account || !contracts || !isCorrectNetwork(chainId)) {
-        setTimeout(asyncStartWhenReady, 100)
-        return
-      }
-      start()
-    }
-
-    asyncStartWhenReady()
-
-    return () => props.rpcProvider.removeAllListeners('Approval')
-  }
-
-  const loadData = async (contracts) => {
-    if (!account) {
-      return
-    }
-    if (!contracts) {
-      console.warn('Contracts not yet loaded!')
-      return
-    }
-    if (!isCorrectNetwork(chainId)) {
-      return
-    }
-
+  const pollOnce = (contracts) => {
     const { usdt, dai, usdc, ousd, vault } = contracts
 
-    const loadBalances = async () => {
-      if (!account) return
+    Promise.all([
+      // balance
+      ousd.balanceOf(account).then((b) => displayCurrency(b, ousd)),
+      usdt.balanceOf(account).then((b) => displayCurrency(b, usdt)),
+      dai.balanceOf(account).then((b) => displayCurrency(b, dai)),
+      usdc.balanceOf(account).then((b) => displayCurrency(b, usdc)),
 
-      try {
-        const [
-          ousdBalance,
-          usdtBalance,
-          daiBalance,
-          usdcBalance,
-        ] = await Promise.all([
-          displayCurrency(await ousd.balanceOf(account), ousd),
-          displayCurrency(await usdt.balanceOf(account), usdt),
-          displayCurrency(await dai.balanceOf(account), dai),
-          displayCurrency(await usdc.balanceOf(account), usdc),
-        ])
-
+      // allowance
+      ousd
+        .allowance(account, vault.address)
+        .then((b) => displayCurrency(b, ousd)),
+      usdt
+        .allowance(account, vault.address)
+        .then((b) => displayCurrency(b, usdt)),
+      dai
+        .allowance(account, vault.address)
+        .then((b) => displayCurrency(b, dai)),
+      usdc
+        .allowance(account, vault.address)
+        .then((b) => displayCurrency(b, usdc)),
+    ])
+      .then((data) => {
+        // balance
         AccountStore.update((s) => {
           s.balances = {
-            usdt: usdtBalance,
-            dai: daiBalance,
-            usdc: usdcBalance,
-            ousd: ousdBalance,
+            ousd: data[0],
+            usdt: data[1],
+            dai: data[2],
+            usdc: data[3],
           }
         })
-      } catch (e) {
+
+        // allowance
+        AccountStore.update((s) => {
+          s.allowances = {
+            ousd: data[4],
+            usdt: data[5],
+            dai: data[6],
+            usdc: data[7],
+          }
+        })
+      })
+      .catch((e) =>
         console.error(
           'AccountListener.js error - can not load account balances: ',
           e
         )
-      }
+      )
+  }
+
+  const subscribeToEvents = (contracts) => {
+    // Polls data first time, then rely on events
+    const { usdt, dai, usdc, ousd, vault } = contracts
+    const rpcProvider = props.rpcProvider
+    const pollNTimes = (n,promiseFn) => {
+      if (n === 0) return
+      // Poll every 5 seconds
+      setTimeout(() => {
+        promiseFn().then(pollNTimes(n-1, promiseFn))  
+      }, 5000)
     }
-    await loadBalances()
+
+    const updateOnAllowanceEvent = (contract, name) =>
+      rpcProvider.on(
+        contract.filters.Approval(account, vault.address, null),
+        (result) =>
+          displayCurrency(result.data, contract).then((allowance) =>
+            AccountStore.update((s) => {
+              s.allowances[name] = allowance
+            }
+                                                     ))
+      )
+    // Subscribe to Transfer event. Then poll balance once event received
+    const updateOnTransferEvent = (contract, name) => {
+      // Account sends tokens
+      rpcProvider.on(
+        contract.filters.Transfer(account, null, null), // event Transfer(address indexed from, address indexed to, uint tokens);
+        (result) => pollNTimes(5, () =>
+          contract
+            .balanceOf(account)
+            .then((balance) => displayCurrency(balance, contract))
+            .then((balance) =>
+                  AccountStore.update((s) => {s.balances[name] = balance})
+                 ))
+      )
+      // Account receives tokens
+      rpcProvider.on(
+        contract.filters.Transfer(null, account, null), // event Transfer(address indexed from, address indexed to, uint tokens);
+        (result) => pollNTimes(5, () =>
+          contract
+            .balanceOf(account)
+            .then((balance) => displayCurrency(balance, contract))
+            .then((balance) => 
+                  AccountStore.update((s) => {s.balances[name] = balance})
+                 ))
+      )
+    }
+
+    // balance
+    updateOnTransferEvent(ousd, 'ousd')
+    updateOnTransferEvent(dai, 'dai')
+    updateOnTransferEvent(usdt, 'usdt')
+    updateOnTransferEvent(usdc, 'usdc')
+
+    // allowance
+    updateOnAllowanceEvent(ousd, 'ousd')
+    updateOnAllowanceEvent(dai, 'dai')
+    updateOnAllowanceEvent(usdt, 'usdt')
+    updateOnAllowanceEvent(usdc, 'usdc')
   }
 
   useEffect(() => {
-    if (account) {
-      login(account, setCookie)
-    }
-
     const setupContractsAndLoad = async () => {
       /* If we have a web3 provider present and is signed into the allowed network:
        * - NODE_ENV === production -> mainnet
@@ -189,31 +162,29 @@ const AccountListener = (props) => {
 
       const contracts = await setupContracts(account, usedLibrary, usedChainId)
       setContracts(contracts)
-
-      setTimeout(() => {
-        loadData(contracts)
-      }, 1)
     }
-
+    
+    if (account) {
+      login(account, setCookie)
+    }
     setupContractsAndLoad()
   }, [account, chainId])
-
+  
   useEffect(() => {
-    let balancesInterval
-    let stopEventListening = () => 0
-    if (contracts && userActive === 'active' && isCorrectNetwork(chainId)) {
-      loadData(contracts)
-      stopEventListening = startEventListening()
-      balancesInterval = setInterval(() => {
-        loadData(contracts)
-      }, 7000)
+    if (
+      account &&
+      contracts &&
+      userActive === 'active' &&
+      isCorrectNetwork(chainId)
+    ) {
+      pollOnce(contracts)
+      subscribeToEvents(contracts)
     }
 
     return () => {
-      stopEventListening()
-      if (balancesInterval) {
-        clearInterval(balancesInterval)
-      }
+      // Stop event listening
+      props.rpcProvider.removeAllListeners('Transfer')
+      props.rpcProvider.removeAllListeners('Approval')
     }
   }, [userActive, contracts])
 
