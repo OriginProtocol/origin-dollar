1477d1745158840a86451a8509a9815fe4a4a68b
Tom Linton <tlinton@fastmail.fm>
2020-12-08 11:42:57 +1300

    Add balance check on transfer

--- contracts/contracts/token/OUSD.sol
+++ contracts/contracts/token/OUSD.sol
@@ -117,6 +117,7 @@ contract OUSD is Initializable, InitializableERC20Detailed, Governable {
      */
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(_to != address(0), "Transfer to zero address");
+        require(_value <= balanceOf(msg.sender), "Transfer greater than balance");
 
         _executeTransfer(msg.sender, _to, _value);
 
@@ -137,6 +138,7 @@ contract OUSD is Initializable, InitializableERC20Detailed, Governable {
         uint256 _value
     ) public returns (bool) {
         require(_to != address(0), "Transfer to zero address");
+        require(_value <= balanceOf(msg.sender), "Transfer greater than balance");
 
         _allowances[_from][msg.sender] = _allowances[_from][msg.sender].sub(
             _value
