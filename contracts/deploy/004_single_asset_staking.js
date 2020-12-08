00e1fca5ce3f595b83bf157515865555e0759e15
Franck Chastagnol <franck@originprotocol.com>
2020-12-08 13:43:57 -0800

    clean up staking deploy script

--- contracts/deploy/004_single_asset_staking.js
+++ contracts/deploy/004_single_asset_staking.js
@@ -1,12 +1,12 @@
+//
+// Script to deploy the Single Asset Staking contract.
+//
 const {
   getAssetAddresses,
   isMainnet,
-  isRinkeby,
   isTest,
-  isGanacheFork,
   isMainnetOrRinkebyOrFork,
 } = require("../test/helpers.js");
-const addresses = require("../utils/addresses.js");
 const { utils } = require("ethers");
 const {
   log,
@@ -14,16 +14,9 @@ const {
   withConfirmation,
 } = require("../utils/deploy");
 
-// Wait for 3 blocks confirmation on Mainnet/Rinkeby.
-const NUM_CONFIRMATIONS = isMainnet || isRinkeby ? 3 : 0;
-
-//
-// 1. Deploy new Single Asset Staking contract
-//
 const singleAssetStaking = async ({ getNamedAccounts, deployments }) => {
   console.log("Running 004_single_asset_staking deployment...");
 
-  const { deploy } = deployments;
   const { governorAddr, deployerAddr } = await getNamedAccounts();
 
   const assetAddresses = await getAssetAddresses(deployments);
@@ -31,58 +24,50 @@ const singleAssetStaking = async ({ getNamedAccounts, deployments }) => {
   const sDeployer = ethers.provider.getSigner(deployerAddr);
   const sGovernor = ethers.provider.getSigner(governorAddr);
 
-  // Deploy the staking proxy.
-  let d = await deploy("OGNStakingProxy", {
-    contract: "InitializeGovernedUpgradeabilityProxy",
-    from: deployerAddr,
-  });
-
-  await ethers.provider.waitForTransaction(
-    d.receipt.transactionHash,
-    NUM_CONFIRMATIONS
+  //
+  // Deploy contracts.
+  //
+  await deployWithConfirmation(
+    "OGNStakingProxy",
+    [],
+    "InitializeGovernedUpgradeabilityProxy"
   );
-  log("Deployed OGNStakingProxy", d);
-
-  // Deploy the SingleAssetStaking.
-  const dSingleAssetStaking = await deploy("SingleAssetStaking", {
-    from: deployerAddr,
-  });
-  await ethers.provider.waitForTransaction(
-    dSingleAssetStaking.receipt.transactionHash,
-    NUM_CONFIRMATIONS
+  const dSingleAssetStaking = await deployWithConfirmation(
+    "SingleAssetStaking"
   );
-  log("Deployed SingleAssetStaking", dSingleAssetStaking);
+
+  //
+  // Initialize
+  //
 
   // Initialize the proxy.
   const cOGNStakingProxy = await ethers.getContract("OGNStakingProxy");
-  let t = await cOGNStakingProxy["initialize(address,address,bytes)"](
-    dSingleAssetStaking.address,
-    deployerAddr,
-    []
+  await withConfirmation(
+    cOGNStakingProxy["initialize(address,address,bytes)"](
+      dSingleAssetStaking.address,
+      deployerAddr,
+      []
+    )
   );
-  await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
   log("Initialized OGNStakingProxy");
 
-  // Initialize the SingleAssetStaking
-  // Note: we are only doing DAI with Aave.
+  // Initialize the SingleAssetStaking contract.
   const cOGNStaking = await ethers.getContractAt(
     "SingleAssetStaking",
     cOGNStakingProxy.address
   );
 
-  // let's give a 5 percent reward rate
-  const rate = utils.parseUnits("0.05", 18);
   const minute = 60;
   const day = 24 * 60 * minute;
-  let durations
+  let durations;
   if (isMainnet || isTest) {
     // starting durations are 90 days, 180 days, 365 days
-    durations = [ 90 * day, 180 * day, 360 * day];
+    durations = [90 * day, 180 * day, 360 * day];
   }
   // Rinkeby or localhost or ganacheFork need a shorter stake for testing purposes
   else {
     // add a very quick vesting rate ideal for testing (10 minutes)
-    durations = [ 90 * day, 4 * minute, 360 * day];
+    durations = [90 * day, 4 * minute, 360 * day];
   }
   const rates = [
     utils.parseUnits("0.085", 18),
@@ -96,10 +81,11 @@ const singleAssetStaking = async ({ getNamedAccounts, deployments }) => {
     : "0x5195f035B980B265C9cA9A83BD8A498dd9160Dff";
 
   console.log("OGN Asset address:", assetAddresses.OGN);
-  t = await cOGNStaking
-    .connect(sDeployer)
-    .initialize(assetAddresses.OGN, durations, rates, preApprover);
-  await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
+  await withConfirmation(
+    cOGNStaking
+      .connect(sDeployer)
+      .initialize(assetAddresses.OGN, durations, rates, preApprover)
+  );
   log("Initialized OGNStaking");
 
   //
@@ -115,20 +101,18 @@ const singleAssetStaking = async ({ getNamedAccounts, deployments }) => {
     strategyGovAddr = governorAddr;
   }
 
-  t = await cOGNStaking.connect(sDeployer).transferGovernance(strategyGovAddr);
-  await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
+  await withConfirmation(
+    cOGNStaking.connect(sDeployer).transferGovernance(strategyGovAddr)
+  );
   log(`OGNStaking transferGovernance(${strategyGovAddr} called`);
 
   if (!isMainnetOrRinkebyOrFork) {
-    t = await cOGNStaking
-      .connect(sGovernor) // Claim governance with governor
-      .claimGovernance();
-    await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
+    await withConfirmation(cOGNStaking.connect(sGovernor).claimGovernance());
     log("Claimed governance for OGNStaking");
 
     const ogn = await ethers.getContract("MockOGN");
-    // amount to load in for rewards
-    // put in a small amount so that we can hit limits for testing
+    // Amount to load in for rewards
+    // Put in a small amount so that we can hit limits for testing
     const loadAmount = utils.parseUnits("299", 18);
     await ogn.connect(sGovernor).mint(loadAmount);
     await ogn.connect(sGovernor).transfer(cOGNStaking.address, loadAmount);
--- contracts/deploy/006_liquidity_reward.js
+++ contracts/deploy/006_liquidity_reward.js
@@ -13,7 +13,6 @@ const {
   withConfirmation,
 } = require("../utils/deploy");
 
-
 // Wait for 3 blocks confirmation on Mainnet/Rinkeby.
 const NUM_CONFIRMATIONS = isMainnet || isRinkeby ? 3 : 0;
 
@@ -28,23 +27,50 @@ const liquidityReward = async ({ getNamedAccounts, deployments }) => {
 
   const assetAddresses = await getAssetAddresses(deployments);
 
-  await Promise.all(['USDT', 'USDC', 'DAI'].map(async stablecoin => {
-    if (!isMainnet && !isGanacheFork) {
-      // Mock Uniswap pair for OUSD -> USDT is dependent on OUSD being deployed
-      const cOUSDProxy = await ethers.getContract("OUSDProxy");
+  await Promise.all(
+    ["USDT", "USDC", "DAI"].map(async (stablecoin) => {
+      if (!isMainnet && !isGanacheFork) {
+        // Mock Uniswap pair for OUSD -> USDT is dependent on OUSD being deployed
+        const cOUSDProxy = await ethers.getContract("OUSDProxy");
 
-      const reserve100OUSD = utils.parseUnits("100", 18);
-      const reserve100STABLECOIN = utils.parseUnits("100", stablecoin === 'DAI' ? 18 : 6);
+        const reserve100OUSD = utils.parseUnits("100", 18);
+        const reserve100STABLECOIN = utils.parseUnits(
+          "100",
+          stablecoin === "DAI" ? 18 : 6
+        );
 
-      let d = await deploy(`MockUniswapPairOUSD_${stablecoin}`, {
+        let d = await deploy(`MockUniswapPairOUSD_${stablecoin}`, {
+          from: deployerAddr,
+          contract: "MockMintableUniswapPair",
+          args: [
+            cOUSDProxy.address,
+            assetAddresses[stablecoin],
+            reserve100OUSD,
+            reserve100STABLECOIN,
+          ],
+        });
+
+        await ethers.provider.waitForTransaction(
+          d.receipt.transactionHash,
+          NUM_CONFIRMATIONS
+        );
+
+        log(`Deployed Uniswap OUSD-${stablecoin} pair`, d);
+      }
+
+      const UniswapOUSD_STABLECOIN =
+        isMainnet || isGanacheFork
+          ? addresses.mainnet[`uniswapOUSD_${stablecoin}`]
+          : (await ethers.getContract(`MockUniswapPairOUSD_${stablecoin}`))
+              .address;
+
+      const sDeployer = ethers.provider.getSigner(deployerAddr);
+      const sGovernor = ethers.provider.getSigner(governorAddr);
+
+      // Deploy the liquidity reward proxy.
+      let d = await deploy(`LiquidityRewardOUSD_${stablecoin}Proxy`, {
+        contract: "InitializeGovernedUpgradeabilityProxy",
         from: deployerAddr,
-        contract: "MockMintableUniswapPair",
-        args: [
-          cOUSDProxy.address,
-          assetAddresses[stablecoin],
-          reserve100OUSD,
-          reserve100STABLECOIN,
-        ],
       });
 
       await ethers.provider.waitForTransaction(
@@ -52,104 +78,84 @@ const liquidityReward = async ({ getNamedAccounts, deployments }) => {
         NUM_CONFIRMATIONS
       );
 
-      log(`Deployed Uniswap OUSD-${stablecoin} pair`, d);
-    }
+      log(`Deployed LiquidityRewardProxy for ${stablecoin}`, d);
 
-    const UniswapOUSD_STABLECOIN =
-      isMainnet || isGanacheFork
-        ? addresses.mainnet[`uniswapOUSD_${stablecoin}`]
-        : (await ethers.getContract(`MockUniswapPairOUSD_${stablecoin}`)).address;
+      // Deploy the liquidityReward.
+      const dLiquidityReward = await deploy("LiquidityReward", {
+        from: deployerAddr,
+      });
 
-    const sDeployer = ethers.provider.getSigner(deployerAddr);
-    const sGovernor = ethers.provider.getSigner(governorAddr);
+      await ethers.provider.waitForTransaction(
+        dLiquidityReward.receipt.transactionHash,
+        NUM_CONFIRMATIONS
+      );
+      log(`Deployed LiqudityReward for ${stablecoin}`, dLiquidityReward);
 
-    // Deploy the liquidity reward proxy.
-    let d = await deploy(`LiquidityRewardOUSD_${stablecoin}Proxy`, {
-      contract: "InitializeGovernedUpgradeabilityProxy",
-      from: deployerAddr,
-    });
-
-    await ethers.provider.waitForTransaction(
-      d.receipt.transactionHash,
-      NUM_CONFIRMATIONS
-    );
-
-    log(`Deployed LiquidityRewardProxy for ${stablecoin}`, d);
-
-    // Deploy the liquidityReward.
-    const dLiquidityReward = await deploy("LiquidityReward", {
-      from: deployerAddr,
-    });
-    
-    await ethers.provider.waitForTransaction(
-      dLiquidityReward.receipt.transactionHash,
-      NUM_CONFIRMATIONS
-    );
-    log(`Deployed LiqudityReward for ${stablecoin}`, dLiquidityReward);
-
-    // Initialize the proxy.
-    const cLiquidityRewardOUSD_STABLECOINProxy = await ethers.getContract(
-      `LiquidityRewardOUSD_${stablecoin}Proxy`
-    );
-    let t = await cLiquidityRewardOUSD_STABLECOINProxy[
-      "initialize(address,address,bytes)"
-    ](dLiquidityReward.address, deployerAddr, []);
-    await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
-    log(`Initialized LiquidityRewardProxy for ${stablecoin}`);
-
-    // Initialize the LquidityReward
-    const cLiquidityRewardOUSD_STABLECOIN = await ethers.getContractAt(
-      "LiquidityReward",
-      cLiquidityRewardOUSD_STABLECOINProxy.address
-    );
-
-    console.log("OGN Asset address:", assetAddresses.OGN);
-    t = await cLiquidityRewardOUSD_STABLECOIN
-      .connect(sDeployer)
-      .initialize(assetAddresses.OGN, UniswapOUSD_STABLECOIN);
-    await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
-    log(`Initialized LiquidRewardStrategy for ${stablecoin}`);
-
-    //
-    // Transfer governance of the Reward proxy to the governor
-    //  - On Mainnet the governance transfer gets executed separately, via the multi-sig wallet.
-    //  - On other networks, this migration script can claim governance by the governor.
-    //
-    let strategyGovAddr;
-    if (isMainnet) {
-      // On Mainnet the governor is the TimeLock
-      strategyGovAddr = (await ethers.getContract("MinuteTimelock")).address;
-    } else {
-      strategyGovAddr = governorAddr;
-    }
-
-    t = await cLiquidityRewardOUSD_STABLECOIN
-      .connect(sDeployer)
-      .transferGovernance(strategyGovAddr);
-    await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
-    log(`LiquidReward transferGovernance(${strategyGovAddr} called for ${stablecoin}`);
-
-    if (!isMainnetOrRinkebyOrFork) {
-      t = await cLiquidityRewardOUSD_STABLECOIN
-        .connect(sGovernor) // Claim governance with governor
-        .claimGovernance();
+      // Initialize the proxy.
+      const cLiquidityRewardOUSD_STABLECOINProxy = await ethers.getContract(
+        `LiquidityRewardOUSD_${stablecoin}Proxy`
+      );
+      let t = await cLiquidityRewardOUSD_STABLECOINProxy[
+        "initialize(address,address,bytes)"
+      ](dLiquidityReward.address, deployerAddr, []);
       await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
-      log(`Claimed governance for LiquidityReward for ${stablecoin}`);
+      log(`Initialized LiquidityRewardProxy for ${stablecoin}`);
 
-      const ogn = await ethers.getContract("MockOGN");
-      const loadAmount = utils.parseUnits("7200000", 18);
-      const rate = utils.parseUnits("6.1538461538", 18);
-      await ogn.connect(sGovernor).mint(loadAmount);
-      await ogn
-        .connect(sGovernor)
-        .transfer(cLiquidityRewardOUSD_STABLECOIN.address, loadAmount);
+      // Initialize the LquidityReward
+      const cLiquidityRewardOUSD_STABLECOIN = await ethers.getContractAt(
+        "LiquidityReward",
+        cLiquidityRewardOUSD_STABLECOINProxy.address
+      );
 
-      await cLiquidityRewardOUSD_STABLECOIN
-        .connect(sGovernor)
-        .startCampaign(rate, 0, 6500 * 180);
-    }
-  }))
+      console.log("OGN Asset address:", assetAddresses.OGN);
+      t = await cLiquidityRewardOUSD_STABLECOIN
+        .connect(sDeployer)
+        .initialize(assetAddresses.OGN, UniswapOUSD_STABLECOIN);
+      await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
+      log(`Initialized LiquidRewardStrategy for ${stablecoin}`);
 
+      //
+      // Transfer governance of the Reward proxy to the governor
+      //  - On Mainnet the governance transfer gets executed separately, via the multi-sig wallet.
+      //  - On other networks, this migration script can claim governance by the governor.
+      //
+      let strategyGovAddr;
+      if (isMainnet) {
+        // On Mainnet the governor is the TimeLock
+        strategyGovAddr = (await ethers.getContract("MinuteTimelock")).address;
+      } else {
+        strategyGovAddr = governorAddr;
+      }
+
+      t = await cLiquidityRewardOUSD_STABLECOIN
+        .connect(sDeployer)
+        .transferGovernance(strategyGovAddr);
+      await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
+      log(
+        `LiquidReward transferGovernance(${strategyGovAddr} called for ${stablecoin}`
+      );
+
+      if (!isMainnetOrRinkebyOrFork) {
+        t = await cLiquidityRewardOUSD_STABLECOIN
+          .connect(sGovernor) // Claim governance with governor
+          .claimGovernance();
+        await ethers.provider.waitForTransaction(t.hash, NUM_CONFIRMATIONS);
+        log(`Claimed governance for LiquidityReward for ${stablecoin}`);
+
+        const ogn = await ethers.getContract("MockOGN");
+        const loadAmount = utils.parseUnits("7200000", 18);
+        const rate = utils.parseUnits("6.1538461538", 18);
+        await ogn.connect(sGovernor).mint(loadAmount);
+        await ogn
+          .connect(sGovernor)
+          .transfer(cLiquidityRewardOUSD_STABLECOIN.address, loadAmount);
+
+        await cLiquidityRewardOUSD_STABLECOIN
+          .connect(sGovernor)
+          .startCampaign(rate, 0, 6500 * 180);
+      }
+    })
+  );
 
   // For mainnet we'd want to transfer OGN to the contract and then start the campaign
   // The Reward rate should start out as:
@@ -175,5 +181,8 @@ const liquidityReward = async ({ getNamedAccounts, deployments }) => {
 
 liquidityReward.id = "003_liquidity_reward";
 liquidityReward.dependencies = ["core"];
+
+// Liquidity mining will get deployed to Rinkeby and Mainnet at a later date.
+liquidityReward.skip = () => isMainnet || isRinkeby;
 
 module.exports = liquidityReward;
