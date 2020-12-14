dd63d163cb0dc467f94824497a9f35ff88ad099c
Tom <tlinton@fastmail.fm>
2020-12-15 12:20:44 +1300

    Repair withdrawAll

--- contracts/contracts/strategies/ThreePoolStrategy.sol
+++ contracts/contracts/strategies/ThreePoolStrategy.sol
@@ -201,24 +201,19 @@ contract ThreePoolStrategy is InitializableAbstractStrategy {
      */
     function withdrawAll() external onlyVaultOrGovernor nonReentrant {
         // Withdraw all from Gauge
-        (, uint256 gaugePTokens, ) = _getTotalPTokens();
+        (, uint256 gaugePTokens, uint256 totalPTokens) = _getTotalPTokens();
         ICurveGauge(crvGaugeAddress).withdraw(gaugePTokens);
-        // Remove entire balance, 3pool strategies only support a single asset
-        // so safe to use assetsMapped[0]
-        IERC20 asset = IERC20(assetsMapped[0]);
-        uint256 pTokenBalance = IERC20(assetToPToken[address(asset)]).balanceOf(
-            address(this)
-        );
-        uint256 minWithdrawAmount = pTokenBalance.mulTruncate(
+        uint256 minWithdrawAmount = totalPTokens.mulTruncate(
             uint256(1e18).sub(maxSlippage)
         );
-        ICurvePool(platformAddress).remove_liquidity_one_coin(
-            pTokenBalance,
-            poolCoinIndex,
-            minWithdrawAmount
-        );
-        // Transfer the asset out to Vault
-        asset.safeTransfer(vaultAddress, asset.balanceOf(address(this)));
+        ICurvePool threePool = ICurvePool(platformAddress);
+        threePool.remove_liquidity(totalPTokens, minWithdrawAmount);
+        // Note that Curve will provide all 3 of the assets in 3pool even if
+        // we have not set PToken addresses for all of them in this strategy
+        for (uint256 i = 0; i < 3; i++) {
+            IERC20 asset = IERC20(threePool.coins(i));
+            asset.safeTransfer(vaultAddress, asset.balanceOf(address(this)));
+        }
     }
 
     /**
