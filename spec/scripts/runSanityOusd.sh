3717a1c3190fb3d7d3ee52047c0f7d528963eefa
Shelly Grossman <shellygr151@gmail.com>
2021-01-14 18:53:29 +0200

    updates

--- spec/scripts/runSanityOusd.sh
+++ spec/scripts/runSanityOusd.sh
@@ -1,1 +1,5 @@
-certoraRun.py contracts/token/OUSD.sol --verify OUSD:../spec/sanity.spec --solc solc5.11 --settings -t=300,-ignoreViewFunctions --staging origin1 --msg "OUSD Sanity"
+certoraRun contracts/token/OUSD.sol \
+  --verify OUSD:../spec/sanity.spec \
+  --solc solc5.11 \
+  --settings -t=300 \
+  --staging origin1 --msg "OUSD Sanity"
