8b999e9f0bdcaec53ccfcf3a8aa5810a2f908843
Shelly Grossman <shellygr151@gmail.com>
2021-01-14 22:26:24 +0200

    java 11

--- spec/scripts/runPrivilegedOusd.sh
+++ spec/scripts/runPrivilegedOusd.sh
@@ -2,4 +2,3 @@ certoraRun contracts/token/OUSD.sol \
 	--verify OUSD:../spec/PrivilegedOUSD.spec \
 	--settings -t=300,-ignoreViewFunctions \
 	--msg "OUSD Privileged"
-	--disableLocalTypeChecking
--- spec/scripts/runPrivilegedVaultAdmin.sh
+++ spec/scripts/runPrivilegedVaultAdmin.sh
@@ -2,4 +2,3 @@ certoraRun contracts/vault/VaultAdmin.sol \
   --verify VaultAdmin:../spec/PrivilegedVaultAdmin.spec \
   --settings -b=2,-assumeUnwindCond,-ignoreViewFunctions \
   --msg "VaultCore Privileged"
-  --disableLocalTypeChecking
