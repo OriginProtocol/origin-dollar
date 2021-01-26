1c0920f014c7329b4fb7035c34be91ea867d8a32
Domen Grabec <grabec@gmail.com>
2021-01-27 00:54:37 +0100

    to spaces

--- contracts/tasks/smokeTest.js
+++ contracts/tasks/smokeTest.js
@@ -11,59 +11,59 @@
 const { getDeployScripts, getLastDeployScript, getFilesInFolder } = require('../utils/fileSystem');
 
 async function getAllTests() {
-	const testsFolder = `${__dirname}/../scripts/test/smokeTest`;
-	const testFiles = await getFilesInFolder(testsFolder);
-	const tests = [];
+  const testsFolder = `${__dirname}/../scripts/test/smokeTest`;
+  const testFiles = await getFilesInFolder(testsFolder);
+  const tests = [];
 
-	testFiles.forEach(testFile => {
-		const test = require(`${testsFolder}/${testFile}`);
-		if (!test.beforeDeploy || test.beforeDeploy.length !== 1) {
-			throw new Error("Missing beforeDeploy(hre) smoke test function")
-		}
-		if (!test.afterDeploy || test.afterDeploy.length !== 2) {
-			throw new Error("Missing afterDeploy(hre, beforeDeployData) smoke test function")
-		}
-		test.name = testFile
-		tests.push(test)
-	})
+  testFiles.forEach(testFile => {
+    const test = require(`${testsFolder}/${testFile}`);
+    if (!test.beforeDeploy || test.beforeDeploy.length !== 1) {
+      throw new Error("Missing beforeDeploy(hre) smoke test function")
+    }
+    if (!test.afterDeploy || test.afterDeploy.length !== 2) {
+      throw new Error("Missing afterDeploy(hre, beforeDeployData) smoke test function")
+    }
+    test.name = testFile
+    tests.push(test)
+  })
 
-	return tests
+  return tests
 }
 
 async function smokeTest(taskArguments, hre) {
-	const deployId = taskArguments.deployid
-	let deployScript
+  const deployId = taskArguments.deployid
+  let deployScript
 
-	if (deployId) {
-		const scripts = await getDeployScripts()
-		deployScript = scripts[parseInt(deployId)]
-	} else {
-		deployScript = await getLastDeployScript()
-	}
+  if (deployId) {
+    const scripts = await getDeployScripts()
+    deployScript = scripts[parseInt(deployId)]
+  } else {
+    deployScript = await getLastDeployScript()
+  }
 
-	const main = require(deployScript.fullPath)
+  const main = require(deployScript.fullPath)
 
-	if (main.length !== 1) {
-		throw new Error("Main deploy script function needs to accept Hardhat runtime environment as the first and only parameter in order to be smoke tested.");
-	}
+  if (main.length !== 1) {
+    throw new Error("Main deploy script function needs to accept Hardhat runtime environment as the first and only parameter in order to be smoke tested.");
+  }
 
-	const allTests = await getAllTests();
-	const beforeDeployReturns = []
+  const allTests = await getAllTests();
+  const beforeDeployReturns = []
 
-	// execute before deploy step on each of the tests
-	for (let i = 0; i < allTests.length; i++) {
-		beforeDeployReturns.push(await allTests[i].beforeDeploy(hre))
-	}
+  // execute before deploy step on each of the tests
+  for (let i = 0; i < allTests.length; i++) {
+    beforeDeployReturns.push(await allTests[i].beforeDeploy(hre))
+  }
 
-	// execute contract deployment script
-	await main(hre)
+  // execute contract deployment script
+  await main(hre)
 
-	// execute after deploy step on each of the tests
-	for (let i = 0; i < allTests.length; i++) {
-		const test = allTests[i]
-		await test.afterDeploy(hre, beforeDeployReturns[i])
-		console.log(`✅ ${test.name} passed!`)
-	}
+  // execute after deploy step on each of the tests
+  for (let i = 0; i < allTests.length; i++) {
+    const test = allTests[i]
+    await test.afterDeploy(hre, beforeDeployReturns[i])
+    console.log(`✅ ${test.name} passed!`)
+  }
 }
 
 module.exports = {
