3874140be42d33c5f455d0adfcde2d1659e72565
Domen Grabec <grabec@gmail.com>
2021-02-01 19:45:24 +0100

    fix tests

--- contracts/tasks/storageSlots.js
+++ contracts/tasks/storageSlots.js
@@ -10,9 +10,10 @@ const {
   isCurrentValidationData,
   assertStorageUpgradeSafe
 } = require('@openzeppelin/upgrades-core');
+const isFork = process.env.FORK === "true";
+const isTest = process.env.IS_TEST === "true";
 
 const getStorageFileLocation = (hre, contractName) => {
-  const isFork = process.env.FORK === "true";
   const isLocalhost = !isFork && hre.network.name === "localhost";
   const isRinkeby = hre.network.name === "rinkeby";
   const isMainnet = hre.network.name === "mainnet";
@@ -66,6 +67,10 @@ const storeStorageLayoutForContract = async (hre, contractName) => {
 const getAllEligibleContractNames = async (hre) => {
   const contractNames = Object.keys(await hre.deployments.all());
 
+  return contractNames.filter(isContractEligible)
+}
+
+const isContractEligible = (contractName) => {
   // These contracts have been deprecated source files are no longer in repo but are still under deployments.
   // For that reason they need to be excluded. 
   const excludeContracts = [
@@ -77,8 +82,9 @@ const getAllEligibleContractNames = async (hre) => {
   ];
 
   // Need to exclude proxies as well since they are not upgradeable
-  return contractNames.filter(name => !name.endsWith("Proxy") && !excludeContracts.includes(name))
+  return !contractName.endsWith("Proxy") && !contractName.startsWith("Mock") && !excludeContracts.includes(contractName);
 }
+
 const storeStorageLayoutForAllContracts = async (taskArguments, hre) => {
   const allContracts = await getAllEligibleContractNames(hre);
 
@@ -102,11 +108,20 @@ const assertStorageLayoutChangeSafe = async (taskArguments, hre) => {
 }
 
 const assertUpgradeIsSafe = async (hre, contractName) => {
+  if (!isContractEligible(contractName)) {
+    if (!isTest) {
+      console.warn(`Skipping storage slot validation of ${contractName}.`)
+    }
+    return true
+  }
+
   const layout = await getStorageLayoutForContract(hre, contractName);
 
   const oldLayout = await loadPreviousStorageLayoutForContract(hre, contractName);
   if (!oldLayout) {
-    console.warn(`Previous storage layout for ${contractName} not found. Treating ${contractName} as a new contract`)
+    if (!isTest) {
+      console.warn(`Previous storage layout for ${contractName} not found. Treating ${contractName} as a new contract`)
+    }
   } else {
     // 3rd param is opts.unsafeAllowCustomTypes
     assertStorageUpgradeSafe(oldLayout, layout, false);
