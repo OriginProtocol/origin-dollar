187e0c1c06358b758389b12da8e3a34dd3ae29e4
Tom Linton <tlinton@fastmail.fm>
2021-02-17 11:14:35 +1300

    Test structure for BuyBack

---
+++ contracts/test/buyback/buyback.js
@@ -0,0 +1,15 @@
+const { expect } = require("chai");
+
+describe("OGN Buyback", function () {
+  before(async () => {});
+
+  it("Should allow Governor to set Uniswap address");
+
+  it("Should not allow non-Governor to set Uniswap address");
+
+  it("Should swap OUSD balance for OGN");
+
+  it("Should allow withdrawal of arbitrary token by Governor");
+
+  it("Should not allow withdrawal of arbitrary token by non-Governor");
+});
