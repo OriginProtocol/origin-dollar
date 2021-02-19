53184c9ddb48f99082624011f60a81a82b1bda85
Micah Alcorn <mail@micahalcorn.com>
2021-02-18 17:20:47 -0800

    Add notice regarding Uniswap alternative

--- dapp/src/components/layout.js
+++ dapp/src/components/layout.js
@@ -11,6 +11,9 @@ import { useEagerConnect, useInactiveListener } from 'utils/hooks'
 import AppFooter from './AppFooter'
 import MarketingFooter from './MarketingFooter'
 
+const UNISWAP_URL =
+  'https://app.uniswap.org/#/swap?inputCurrency=0xdac17f958d2ee523a2206206994597c13d831ec7&outputCurrency=0x2A8e1E676Ec238d8A992307B495b45B3fEAa5e86'
+
 const Layout = ({
   locale,
   onLocale,
@@ -20,6 +23,7 @@ const Layout = ({
   shorter,
   medium,
   isStakePage,
+  showUniswapNotice,
 }) => {
   return (
     <>
@@ -56,11 +60,29 @@ const Layout = ({
         )}
       </Head>
       <div
-        className={classnames('d-none notice text-white text-center p-3', {
-          dapp,
-        })}
+        className={classnames(
+          'notice text-white text-center p-3',
+          {
+            dapp,
+          },
+          showUniswapNotice ? '' : 'd-none'
+        )}
       >
-        {/* Hiding this banner for now */}
+        <div className="container d-flex flex-column flex-md-row align-items-center">
+          <img src="/images/horsey.svg" className="mb-2 mb-md-0 mr-md-3" />
+          {fbt(
+            'Gas fees are high right now. It might be cheaper to buy OUSD on Uniswap.',
+            'Uniswap notice'
+          )}
+          <a
+            href={UNISWAP_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-dark mt-3 mt-md-0 ml-md-auto"
+          >
+            Try Uniswap
+          </a>
+        </div>
       </div>
       <main className={classnames({ dapp, short, shorter, medium })}>
         {dapp && <div className="container">{children}</div>}
@@ -70,7 +92,7 @@ const Layout = ({
       {dapp && <AppFooter dapp={dapp} locale={locale} onLocale={onLocale} />}
       <style jsx>{`
         .notice {
-          background-color: black;
+          background-color: #7a26f3;
           margin-bottom: 35px;
         }
 
@@ -78,8 +100,10 @@ const Layout = ({
           margin-bottom: 0px;
         }
 
-        a {
-          text-decoration: underline;
+        .notice .btn {
+          font-size: 12px;
+          height: auto;
+          padding: 5px 20px;
         }
 
         .container {
