6432a9e5ca5d244791a2c66bf3286f65d20a2ac0
Shelly Grossman <shellygr151@gmail.com>
2021-02-24 20:37:14 +0200

    sync

--- spec/scripts/ruleRunVault.sh
+++ spec/scripts/ruleRunVault.sh
@@ -1,14 +1,10 @@
-BASE=${1}
-RULE=${2}
-sed "s/VAULT_BASE/${BASE}/g" ../spec/harnesses/VaultHarnessTemplate.sol > VaultHarness.sol
-# Use either Vault or VaultAdmin
-certoraRun VaultHarness.sol \
+certoraRun ../spec/harnesses/VaultHarness.sol \
+  ../spec/harnesses/PTokenA.sol ../spec/harnesses/PTokenB.sol \
   ../spec/harnesses/DummyERC20A.sol ../spec/harnesses/DummyERC20B.sol \
-  contracts/mocks/MockCToken.sol contracts/mocks/MockAave.sol:MockAToken \
   ../spec/harnesses/SimpleStrategy.sol \
   --verify VaultHarness:../spec/vault.spec \
   --cache vault \
   --solc solc5.11 \
-  --settings -assumeUnwindCond,-b=2,-t=300,-enableStorageAnalysis=true,-ignoreViewFunctions \
-  --rule ${RULE} \
-  --msg "Vault - ${BASE} verification"
+  --settings -assumeUnwindCond,-b=2,-t=150,-enableStorageAnalysis=true,-ignoreViewFunctions,-copyLoopUnroll=2 \
+  --rule $1 --staging shelly/sirenmarkets2 --settings -ciMode=true,-graphDrawLimit=0,-globalTimeout=23000 \
+  --msg "Vault verification"
