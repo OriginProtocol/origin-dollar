59bb521aba64cf3127f03a52fdadb87f6871d637
Shelly Grossman <shellygr151@gmail.com>
2021-03-07 13:52:21 +0200

    oops

--- spec/scripts/runReentrancyVault.sh
+++ spec/scripts/runReentrancyVault.sh
@@ -3,7 +3,7 @@ SRC=${2}
 # vault Vault
 # vault VaultCore
 sed "s/PATH/${P}/g; s/SRC/${SRC}/g" ../spec/harnesses/ReentrancyHarnessTemplate.sol > ReentrancyHarness.sol
-certoraRun.py ReentrancyHarness.sol --verify ReentrancyHarness:../spec/reentrancyVault.spec \
+certoraRun ReentrancyHarness.sol --verify ReentrancyHarness:../spec/reentrancyVault.spec \
   --solc solc5.11 \
   --cache reentrancy \
   --settings -assumeUnwindCond,-b=2,-t=300 \
