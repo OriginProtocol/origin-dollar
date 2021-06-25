2bb9b608f186d8262af51024fd162f57af31f1cf
Daniel Von Fange <daniel@leancoder.com>
2021-06-25 16:19:01 -0400

    Use a separate storage contract.

--- contracts/contracts/token/OUSD.sol
+++ contracts/contracts/token/OUSD.sol
@@ -6,17 +6,13 @@ pragma solidity 0.5.11;
  * @dev Implements an elastic supply
  * @author Origin Protocol Inc
  */
-import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
-import {
-    Initializable
-} from "@openzeppelin/upgrades/contracts/Initializable.sol";
-import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
+import { OUSDStorage } from "./OUSDStorage.sol";
 import {
     InitializableERC20Detailed
 } from "../utils/InitializableERC20Detailed.sol";
-import { StableMath } from "../utils/StableMath.sol";
-import { Governable } from "../governance/Governable.sol";
+
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
 /**
  * NOTE that this is an ERC20 token but the invariant that the sum of
@@ -24,31 +20,13 @@ import { Governable } from "../governance/Governable.sol";
  * rebasing design. Any integrations with OUSD should be aware.
  */
 
-contract OUSD is Initializable, InitializableERC20Detailed, Governable {
-    using SafeMath for uint256;
-    using StableMath for uint256;
+contract OUSD is OUSDStorage {
 
     event TotalSupplyUpdated(
         uint256 totalSupply,
         uint256 rebasingCredits,
         uint256 rebasingCreditsPerToken
     );
-
-    enum RebaseOptions { NotSet, OptOut, OptIn }
-
-    uint256 private constant MAX_SUPPLY = ~uint128(0); // (2^128) - 1
-    uint256 public _totalSupply;
-    mapping(address => mapping(address => uint256)) private _allowances;
-    address public vaultAddress = address(0);
-    mapping(address => uint256) public _creditBalances; // change later
-    uint256 public rebasingCredits;
-    uint256 public rebasingCreditsPerToken;
-    // Frozen address/credits are non rebasing (value is held in contracts which
-    // do not receive yield unless they explicitly opt in)
-    uint256 public nonRebasingSupply;
-    mapping(address => uint256) public nonRebasingCreditsPerToken;
-    mapping(address => RebaseOptions) public rebaseState;
-    mapping(address => uint256) public isUpgraded;
 
     function initialize(
         string calldata _nameArg,
