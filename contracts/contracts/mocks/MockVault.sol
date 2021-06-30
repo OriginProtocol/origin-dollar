eba2f83d9ba5314c89c06c98a6332c36963d7c91
Daniel Von Fange <daniel@leancoder.com>
2021-06-30 16:21:11 -0400

    Reduce redeem gas costs by 719K/$319 by removing unnecessary totalValues() (#588)
    Massively reduce redeem gas costs by removing duplicated total value calculations

--- contracts/contracts/mocks/MockVault.sol
+++ contracts/contracts/mocks/MockVault.sol
@@ -2,6 +2,7 @@ pragma solidity 0.5.11;
 
 import { VaultCore } from "../vault/VaultCore.sol";
 import { VaultInitializer } from "../vault/VaultInitializer.sol";
+import "../utils/Helpers.sol";
 
 contract MockVault is VaultCore, VaultInitializer {
     uint256 storedTotalValue;
@@ -16,6 +17,21 @@ contract MockVault is VaultCore, VaultInitializer {
 
     function _totalValue() internal view returns (uint256) {
         return storedTotalValue;
+    }
+
+    function _checkBalance(address _asset)
+        internal
+        view
+        returns (uint256 balance)
+    {
+        // Avoids rounding errors by returning the total value
+        // in a single currency
+        if (allAssets[0] == _asset) {
+            uint256 decimals = Helpers.getDecimals(_asset);
+            return storedTotalValue.scaleBy(int8(decimals - 18));
+        } else {
+            return 0;
+        }
     }
 
     function setMaxSupplyDiff(uint256 _maxSupplyDiff) external onlyGovernor {
