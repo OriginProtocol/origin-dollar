5a3b7f7d3291f21c20f889aae94a9bf2f6f0bff9
Daniel Von Fange <daniel@leancoder.com>
2021-08-20 17:49:41 -0400

    Prettier (#647)

--- contracts/contracts/oracle/OracleRouter.sol
+++ contracts/contracts/oracle/OracleRouter.sol
@@ -57,7 +57,6 @@ contract OracleRouter is OracleRouterBase {
             return address(0x3E7d1eAB13ad0104d2750B8863b489D65364e32D);
         } else {
             revert("Asset not available");
-
         }
     }
 }
