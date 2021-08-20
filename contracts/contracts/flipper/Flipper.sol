7a1441ee10541c10603050b393b5db9d601a07a7
Daniel Von Fange <daniel@leancoder.com>
2021-08-20 17:50:36 -0400

    Remove unneeded constructor (#646)

--- contracts/contracts/flipper/Flipper.sol
+++ contracts/contracts/flipper/Flipper.sol
@@ -22,11 +22,6 @@ contract Flipper is Governable {
     IERC20 usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     Tether constant usdt = Tether(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
-    // -----------
-    // Constructor
-    // -----------
-    constructor() public {}
-
     // -----------------
     // Trading functions
     // -----------------
