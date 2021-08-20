c7f5afb1f3c69682608f06816f21639a8b34529d
Tom Linton <tom@originprotocol.com>
2021-08-21 00:54:16 +1200

    Cleanup require for revert (#644)

--- contracts/contracts/oracle/OracleRouter.sol
+++ contracts/contracts/oracle/OracleRouter.sol
@@ -56,7 +56,8 @@ contract OracleRouter is OracleRouterBase {
         ) {
             return address(0x3E7d1eAB13ad0104d2750B8863b489D65364e32D);
         } else {
-            require(false, "Asset not available");
+            revert("Asset not available");
+
         }
     }
 }
