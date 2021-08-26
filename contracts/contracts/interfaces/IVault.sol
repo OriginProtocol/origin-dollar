514a9721c045f456f43ffaea71efdac11dec5a2b
Tom Linton <tlinton@fastmail.fm>
2021-08-26 14:41:04 +1200

    Sync vault events in IVault

--- contracts/contracts/interfaces/IVault.sol
+++ contracts/contracts/interfaces/IVault.sol
@@ -2,12 +2,27 @@ pragma solidity 0.5.11;
 
 interface IVault {
     event AssetSupported(address _asset);
+    event AssetDefaultStrategyUpdated(address _asset, address _strategy);
+    event AssetAllocated(address _asset, address _strategy, uint256 _amount);
     event StrategyApproved(address _addr);
     event StrategyRemoved(address _addr);
     event Mint(address _addr, uint256 _value);
     event Redeem(address _addr, uint256 _value);
-    event DepositsPaused();
-    event DepositsUnpaused();
+    event CapitalPaused();
+    event CapitalUnpaused();
+    event RebasePaused();
+    event RebaseUnpaused();
+    event VaultBufferUpdated(uint256 _vaultBuffer);
+    event RedeemFeeUpdated(uint256 _redeemFeeBps);
+    event PriceProviderUpdated(address _priceProvider);
+    event AllocateThresholdUpdated(uint256 _threshold);
+    event RebaseThresholdUpdated(uint256 _threshold);
+    event UniswapUpdated(address _address);
+    event StrategistUpdated(address _address);
+    event MaxSupplyDiffChanged(uint256 maxSupplyDiff);
+    event YieldDistribution(address _to, uint256 _yield, uint256 _fee);
+    event TrusteeFeeBpsChanged(uint256 _basis);
+    event TrusteeAddressChanged(address _address);
 
     // Governable.sol
     function transferGovernance(address _newGovernor) external;
