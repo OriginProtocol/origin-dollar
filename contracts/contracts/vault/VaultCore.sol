dba7a20879eaba6cc616e7e3a9ec5b59c5604bcb
Tom Linton <tlinton@fastmail.fm>
2021-09-06 12:11:14 +1200

    Upgrade VaultCore fallback function for Solidity changes

--- contracts/contracts/vault/VaultCore.sol
+++ contracts/contracts/vault/VaultCore.sol
@@ -658,28 +658,28 @@ contract VaultCore is VaultStorage {
      * @dev Falldown to the admin implementation
      * @notice This is a catch all for all functions not declared in core
      */
-    function() external payable {
+    fallback() external payable {
         bytes32 slot = adminImplPosition;
         assembly {
             // Copy msg.data. We take full control of memory in this inline assembly
             // block because it will not return to Solidity code. We overwrite the
             // Solidity scratch pad at memory position 0.
-            calldatacopy(0, 0, calldatasize)
+            calldatacopy(0, 0, calldatasize())
 
             // Call the implementation.
             // out and outsize are 0 because we don't know the size yet.
-            let result := delegatecall(gas, sload(slot), 0, calldatasize, 0, 0)
+            let result := delegatecall(gas(), sload(slot), 0, calldatasize(), 0, 0)
 
             // Copy the returned data.
-            returndatacopy(0, 0, returndatasize)
+            returndatacopy(0, 0, returndatasize())
 
             switch result
             // delegatecall returns 0 on error.
             case 0 {
-                revert(0, returndatasize)
+                revert(0, returndatasize())
             }
             default {
-                return(0, returndatasize)
+                return(0, returndatasize())
             }
         }
     }
