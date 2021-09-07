dde78828b38182d015da12c38a876ea34dd47284
Tom Linton <tlinton@fastmail.fm>
2021-09-07 13:54:30 +1200

    Change proxies to EIP1967

--- contracts/contracts/proxies/Proxies.sol
+++ contracts/contracts/proxies/Proxies.sol
@@ -1,31 +1,25 @@
 pragma solidity ^0.8.0;
 
-import { InitializeGovernedUpgradeabilityProxy } from "./InitializeGovernedUpgradeabilityProxy.sol";
+
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
 
 /**
  * @notice OUSDProxy delegates calls to an OUSD implementation
  */
-contract OUSDProxy is InitializeGovernedUpgradeabilityProxy {
-
-}
+contract OUSDProxy is ERC1967Proxy {}
 
 /**
  * @notice VaultProxy delegates calls to a Vault implementation
  */
-contract VaultProxy is InitializeGovernedUpgradeabilityProxy {
-
-}
+contract VaultProxy is ERC1967Proxy {}
 
 /**
  * @notice CompoundStrategyProxy delegates calls to a CompoundStrategy implementation
  */
-contract CompoundStrategyProxy is InitializeGovernedUpgradeabilityProxy {
-
-}
+contract CompoundStrategyProxy is ERC1967Proxy {}
 
 /**
  * @notice ThreePoolStrategyProxy delegates calls to a ThreePoolStrategy implementation
  */
-contract ThreePoolStrategyProxy is InitializeGovernedUpgradeabilityProxy {
-
-}
+contract ThreePoolStrategyProxy is ERC1967Proxy {}
