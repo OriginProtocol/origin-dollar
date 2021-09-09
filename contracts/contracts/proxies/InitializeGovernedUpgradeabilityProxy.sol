a524c9c1c4e6091be25e136f67d720d01edd29b4
Tom Linton <tlinton@fastmail.fm>
2021-09-10 11:39:53 +1200

    Revert proxy changes and refactor for Solidity 0.8.*

---
+++ contracts/contracts/proxies/InitializeGovernedUpgradeabilityProxy.sol
@@ -0,0 +1,200 @@
+pragma solidity ^0.8.0;
+
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+
+import { Governable } from "../governance/Governable.sol";
+
+/**
+ * @title BaseGovernedUpgradeabilityProxy
+ * @dev This contract combines an upgradeability proxy with our governor system.
+ * It is based on an older version of OpenZeppelins BaseUpgradeabilityProxy
+ * (https://github.com/OpenZeppelin/openzeppelin-sdk/blob/master/packages/lib/contracts/upgradeability/BaseUpgradeabilityProxy.sol) with necessary changes to make it work
+ * with Solidity ^0.8.0.
+ * @author Origin Protocol Inc
+ */
+contract InitializeGovernedUpgradeabilityProxy is Governable {
+    /**
+     * @dev Emitted when the implementation is upgraded.
+     * @param implementation Address of the new implementation.
+     */
+    event Upgraded(address indexed implementation);
+
+    /**
+     * @dev Contract initializer with Governor enforcement
+     * @param _logic Address of the initial implementation.
+     * @param _initGovernor Address of the initial Governor.
+     * @param _data Data to send as msg.data to the implementation to initialize
+     * the proxied contract.
+     * It should include the signature and the parameters of the function to be
+     * called, as described in
+     * https://solidity.readthedocs.io/en/v0.4.24/abi-spec.html#function-selector-and-argument-encoding.
+     * This parameter is optional, if no data is given the initialization call
+     * to proxied contract will be skipped.
+     */
+    function initialize(
+        address _logic,
+        address _initGovernor,
+        bytes memory _data
+    ) public payable onlyGovernor {
+        require(_implementation() == address(0));
+        assert(
+            IMPLEMENTATION_SLOT ==
+                bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1)
+        );
+        _changeGovernor(_initGovernor);
+        _setImplementation(_logic);
+        if (_data.length > 0) {
+            (bool success, ) = _logic.delegatecall(_data);
+            require(success);
+        }
+    }
+
+    /**
+     * @return The address of the proxy admin/it's also the governor.
+     */
+    function admin() external view returns (address) {
+        return _governor();
+    }
+
+    /**
+     * @return The address of the implementation.
+     */
+    function implementation() external view returns (address) {
+        return _implementation();
+    }
+
+    /**
+     * @dev Upgrade the backing implementation of the proxy.
+     * Only the admin can call this function.
+     * @param newImplementation Address of the new implementation.
+     */
+    function upgradeTo(address newImplementation) external onlyGovernor {
+        _upgradeTo(newImplementation);
+    }
+
+    /**
+     * @dev Upgrade the backing implementation of the proxy and call a function
+     * on the new implementation.
+     * This is useful to initialize the proxied contract.
+     * @param newImplementation Address of the new implementation.
+     * @param data Data to send as msg.data in the low level call.
+     * It should include the signature and the parameters of the function to be called, as described in
+     * https://solidity.readthedocs.io/en/v0.4.24/abi-spec.html#function-selector-and-argument-encoding.
+     */
+    function upgradeToAndCall(address newImplementation, bytes calldata data)
+        external
+        payable
+        onlyGovernor
+    {
+        _upgradeTo(newImplementation);
+        (bool success, ) = newImplementation.delegatecall(data);
+        require(success);
+    }
+
+    /**
+     * @dev Fallback function.
+     * Implemented entirely in `_fallback`.
+     */
+    fallback () external payable {
+        _fallback();
+    }
+
+    /**
+     * @dev Delegates execution to an implementation contract.
+     * This is a low level function that doesn't return to its internal call site.
+     * It will return to the external caller whatever the implementation returns.
+     * @param implementation Address to delegate.
+     */
+    function _delegate(address implementation) internal {
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+            calldatacopy(0, 0, calldatasize())
+
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+            let result := delegatecall(
+                gas(),
+                implementation,
+                0,
+                calldatasize(),
+                0,
+                0
+            )
+
+            // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    /**
+     * @dev Function that is run as the first thing in the fallback function.
+     * Can be redefined in derived contracts to add functionality.
+     * Redefinitions must call super._willFallback().
+     */
+    function _willFallback() internal {}
+
+    /**
+     * @dev fallback implementation.
+     * Extracted to enable manual triggering.
+     */
+    function _fallback() internal {
+        _willFallback();
+        _delegate(_implementation());
+    }
+
+    /**
+     * @dev Storage slot with the address of the current implementation.
+     * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1, and is
+     * validated in the constructor.
+     */
+    bytes32 internal constant IMPLEMENTATION_SLOT =
+        0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    /**
+     * @dev Returns the current implementation.
+     * @return impl Address of the current implementation
+     */
+    function _implementation() internal view returns (address impl) {
+        bytes32 slot = IMPLEMENTATION_SLOT;
+        assembly {
+            impl := sload(slot)
+        }
+    }
+
+    /**
+     * @dev Upgrades the proxy to a new implementation.
+     * @param newImplementation Address of the new implementation.
+     */
+    function _upgradeTo(address newImplementation) internal {
+        _setImplementation(newImplementation);
+        emit Upgraded(newImplementation);
+    }
+
+    /**
+     * @dev Sets the implementation address of the proxy.
+     * @param newImplementation Address of the new implementation.
+     */
+    function _setImplementation(address newImplementation) internal {
+        require(
+            Address.isContract(newImplementation),
+            "Cannot set a proxy implementation to a non-contract address"
+        );
+
+        bytes32 slot = IMPLEMENTATION_SLOT;
+
+        assembly {
+            sstore(slot, newImplementation)
+        }
+    }
+}
--- contracts/contracts/proxies/Proxies.sol
+++ contracts/contracts/proxies/Proxies.sol
@@ -1,31 +1,31 @@
 pragma solidity ^0.8.0;
 
-import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { InitializeGovernedUpgradeabilityProxy } from "./InitializeGovernedUpgradeabilityProxy.sol";
 
 /**
  * @notice OUSDProxy delegates calls to an OUSD implementation
  */
-abstract contract OUSDProxy is ERC1967Proxy {
+contract OUSDProxy is InitializeGovernedUpgradeabilityProxy {
 
 }
 
 /**
  * @notice VaultProxy delegates calls to a Vault implementation
  */
-abstract contract VaultProxy is ERC1967Proxy {
+contract VaultProxy is InitializeGovernedUpgradeabilityProxy {
 
 }
 
 /**
  * @notice CompoundStrategyProxy delegates calls to a CompoundStrategy implementation
  */
-abstract contract CompoundStrategyProxy is ERC1967Proxy {
+contract CompoundStrategyProxy is InitializeGovernedUpgradeabilityProxy {
 
 }
 
 /**
  * @notice ThreePoolStrategyProxy delegates calls to a ThreePoolStrategy implementation
  */
-abstract contract ThreePoolStrategyProxy is ERC1967Proxy {
+contract ThreePoolStrategyProxy is InitializeGovernedUpgradeabilityProxy {
 
 }
