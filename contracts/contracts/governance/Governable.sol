a5ba7729fc3db58dbc5a1bda77fd6cc2e46ece76
Tom Linton <tlinton@fastmail.fm>
2021-09-30 14:41:34 +1300

    Remove debug function

--- contracts/contracts/governance/Governable.sol
+++ contracts/contracts/governance/Governable.sol
@@ -61,13 +61,6 @@ contract Governable {
         }
     }
 
-    function pendingGovernor() public view returns (address governorOut) {
-        bytes32 position = pendingGovernorPosition;
-        assembly {
-            governorOut := sload(position)
-        }
-    }
-
     /**
      * @dev Returns the address of the pending Governor.
      */
