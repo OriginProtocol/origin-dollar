f0f33936e17b857543e792a3e56401b749cf83a1
Tom Linton <tlinton@fastmail.fm>
2021-10-12 10:21:30 +1300

    Add set fee and allocate

--- contracts/deploy/023_set_dai_default_strategy.js
+++ contracts/deploy/023_set_dai_default_strategy.js
@@ -1,12 +1,17 @@
-const hre = require("hardhat");
-
-const { log, deploymentWithProposal } = require("../utils/deploy");
+const { deploymentWithProposal } = require("../utils/deploy");
 
 module.exports = deploymentWithProposal(
   { deployName: "020_set_dai_default_strategy" },
   async ({ ethers, assetAddresses }) => {
     const cVaultProxy = await ethers.getContract("VaultProxy");
-    const cVault = await ethers.getContractAt("Vault", cVaultProxy.address);
+    const cVaultAdmin = await ethers.getContractAt(
+      "VaultAdmin",
+      cVaultProxy.address
+    );
+    const cVaultCore = await ethers.getContractAt(
+      "VaultCore",
+      cVaultProxy.address
+    );
     const cCompoundStrategyProxy = await ethers.getContract(
       "CompoundStrategyProxy"
     );
@@ -16,9 +21,20 @@ module.exports = deploymentWithProposal(
       actions: [
         {
           // Set DAI default to Compound
-          contract: cVault,
+          contract: cVaultAdmin,
           signature: "setAssetDefaultStrategy(address,address)",
           args: [assetAddresses.DAI, cCompoundStrategyProxy.address],
+        },
+        {
+          // Set
+          contract: cVaultAdmin,
+          signature: "setTrusteeFeeBps(uint256)",
+          args: [1000], // 1000 BPS = 10%
+        },
+        {
+          // Allocate DAI
+          contract: cVaultCore,
+          signature: "allocate()",
         },
       ],
     };
