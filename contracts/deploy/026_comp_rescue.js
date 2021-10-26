a0a8b03bad6a5950a0f9f7f02fcb125357b2320c
Daniel Von Fange <daniel@leancoder.com>
2021-10-26 17:16:06 -0400

    Draft proposal for rescuing COMP

---
+++ contracts/deploy/026_comp_rescue.js
@@ -0,0 +1,35 @@
+const { deploymentWithProposal } = require("../utils/deploy");
+
+module.exports = deploymentWithProposal(
+  { deployName: "026_comp_rescue", forceDeploy: true },
+  async ({ ethers }) => {
+    const cVaultProxy = await ethers.getContract("VaultProxy");
+    const cVaultAdmin = await ethers.getContract(
+      "VaultAdmin",
+      cVaultProxy.address
+    );
+
+    const OLD_COMP_STRAT = "0xD5433168Ed0B1F7714819646606DB509D9d8EC1f";
+
+    return {
+      name: "Collects COMP from old Contract",
+      actions: [
+        {
+          contract: cVaultAdmin,
+          signature: "approveStrategy(address)",
+          args: [OLD_COMP_STRAT],
+        },
+        {
+          contract: cVaultAdmin,
+          signature: "harvestAndSwap(address)",
+          args: [OLD_COMP_STRAT],
+        },
+        {
+          contract: cVaultAdmin,
+          signature: "removeStrategy(address)",
+          args: [OLD_COMP_STRAT],
+        },
+      ],
+    };
+  }
+);
