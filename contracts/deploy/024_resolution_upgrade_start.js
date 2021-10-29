a5a435b24c8695ea62ed7529de43d947f43f56c5
Daniel Von Fange <daniel@leancoder.com>
2021-10-29 12:44:20 -0400

    Pause capital before upgrade

--- contracts/deploy/024_resolution_upgrade_start.js
+++ contracts/deploy/024_resolution_upgrade_start.js
@@ -8,6 +8,13 @@ module.exports = deploymentWithProposal(
     );
     const cOUSDProxy = await ethers.getContract("OUSDProxy");
 
+    // Deployments
+    const cVaultProxy = await ethers.getContract("VaultProxy");
+    const cVaultAdmin = await ethers.getContractAt(
+      "VaultAdmin",
+      cVaultProxy.address
+    );
+
     // Governance proposal
     return {
       name: "Switch OUSD into resolution upgrade mode",
@@ -16,6 +23,11 @@ module.exports = deploymentWithProposal(
           contract: cOUSDProxy,
           signature: "upgradeTo(address)",
           args: [dOUSDResolutionUpgrade.address],
+        },
+        {
+          contract: cVaultAdmin,
+          signature: "pauseCapital()",
+          args: [],
         },
       ],
     };
