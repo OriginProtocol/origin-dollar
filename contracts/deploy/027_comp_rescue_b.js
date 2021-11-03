849f3835448abdbc8ae041524871242cb907d387
Daniel Von Fange <daniel@leancoder.com>
2021-11-03 09:35:06 -0400

    Add proposal from old governor to activate rewards on old Compound strategy.

--- contracts/deploy/027_comp_rescue_b.js
+++ contracts/deploy/027_comp_rescue_b.js
@@ -1,15 +1,15 @@
 const { deploymentWithProposal } = require("../utils/deploy");
 
+const OLD_COMP_STRAT = "0xD5433168Ed0B1F7714819646606DB509D9d8EC1f";
+
 module.exports = deploymentWithProposal(
-  { deployName: "026_comp_rescue", forceDeploy: false },
+  { deployName: "027_comp_rescue_b", forceDeploy: false },
   async ({ ethers }) => {
     const cVaultProxy = await ethers.getContract("VaultProxy");
     const cVaultAdmin = await ethers.getContractAt(
       "VaultAdmin",
       cVaultProxy.address
     );
-
-    const OLD_COMP_STRAT = "0xD5433168Ed0B1F7714819646606DB509D9d8EC1f";
 
     return {
       name: "Collects COMP from old Contract",
