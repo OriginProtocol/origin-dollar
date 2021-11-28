52ad27171d10d292ae7b1696628bda9b3a9785a5
Calvintoes <cxt6655@rit.edu>
2021-11-28 14:28:59 -0800

    deleted extra etherscan file

--- dapp/src/components/sidePanel/SidePanelTransactionMessage.js
+++ dapp/src/components/sidePanel/SidePanelTransactionMessage.js
@@ -636,7 +636,7 @@ const SidePanelTransactionMessage = ({
           cursor: pointer;
           width: 17px;
           height: 17px;
-          background-image: url('/images/etherscan-icon-hover.svg');
+          background-image: url('/images/etherscan-icon.svg');
           background-size: 17px 17px;
         }
 
