6842ecf792ed78e80e61a1da995bee29572f1bc4
Evgeny Boxer <evgeny.boxer@gmail.com>
2022-03-29 08:59:42 +1100

    [wip] refactor: use react-query for tx history fetching (#954)
    * refactor: use react-query for tx history fetching
    
    * feat: add react-query devtools
    
    * fix: run prettier

---
+++ dapp/src/services/transaction-history.service.js
@@ -0,0 +1,15 @@
+export default class TransactionHistoryService {
+  constructor() {
+    this.baseURL = `${process.env.ANALYTICS_ENDPOINT}/api/v1/address`
+  }
+
+  async fetchHistory(account) {
+    const response = await fetch(
+      `${this.baseURL}/${account.toLowerCase()}/history`
+    ).then((res) => res.json())
+
+    return response.history
+  }
+}
+
+export const transactionHistoryService = new TransactionHistoryService()
