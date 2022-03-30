8692371aa7046c12c6d444bc6009cadc58891a2a
Daniel Von Fange <daniel@leancoder.com>
2022-03-30 14:43:26 -0400

    Switch to Open Zeppelin 4626

--- contracts/contracts/token/WrappedOusd.sol
+++ contracts/contracts/token/WrappedOusd.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { ERC20 } from "../../lib/solmate/src/tokens/ERC20.sol";
-import { ERC4626 } from "../../lib/solmate/src/mixins/ERC4626.sol";
+import { ERC4626 } from "../../lib/openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -14,42 +14,41 @@ contract WrappedOusd is ERC4626, Governable, Initializable {
     using SafeERC20 for IERC20;
 
     constructor(
-        ERC20 _underlying,
-        string memory _name,
-        string memory _symbol
-    ) ERC4626(_underlying, _name, _symbol) Governable() {}
+        ERC20 underlying_,
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) ERC4626(underlying_) Governable() {}
 
     /**
      * @notice Enable OUSD rebasing for this contract
      */
-    function initialize(string memory _name, string memory _symbol)
+    function initialize(string memory name_, string memory symbol_)
         external
         onlyGovernor
         initializer
     {
-        OUSD(address(asset)).rebaseOptIn();
-        name = _name;
-        symbol = _symbol;
+        OUSD(address(asset())).rebaseOptIn();
     }
 
-    /**
-     * @notice Show the total amount of OUSD held by the wrapper
-     */
-    function totalAssets() public view override returns (uint256) {
-        return ERC20(asset).balanceOf(address(this));
+    function name() public view override returns (string memory) {
+        return "Wrapped OUSD";
+    }
+
+    function symbol() public view override returns (string memory) {
+        return "WOUSD";
     }
 
     /**
      * @notice Transfer token to governor. Intended for recovering tokens stuck in
      *      contract, i.e. mistaken sends. Cannot transfer OUSD
-     * @param _asset Address for the asset
-     * @param _amount Amount of the asset to transfer
+     * @param asset_ Address for the asset
+     * @param amount_ Amount of the asset to transfer
      */
-    function transferToken(address _asset, uint256 _amount)
+    function transferToken(address asset_, uint256 amount_)
         external
         onlyGovernor
     {
-        require(_asset != address(asset), "Cannot collect OUSD");
-        IERC20(_asset).safeTransfer(governor(), _amount);
+        require(asset_ != address(asset()), "Cannot collect OUSD");
+        IERC20(asset_).safeTransfer(governor(), amount_);
     }
 }
