a773564a9ef674801db731f7295b8e8189c5c146
roland <62400812+rolandpo@users.noreply.github.com>
2022-04-08 10:13:27 +0100

    Approval process change (#958)
    * add approval step to swap
    
    * align side panel height
    
    * center arrow
    
    * delete modal components
    
    * prettier
    
    * remove buysellmodal
    
    * import downcaret
    
    * remove debug
    
    * remove allowancesloaded
    
    * prettier
    
    * fix approve button hiding
    
    * fbt change
    
    * prettier
    
    * fix button color change
    
    * replace coin with contract
    
    * fix approval button state
    
    * clean up
    
    * fix bug
    
    * fix bug
    
    * simplify things (#969)
    
    * simplify things
    
    * fix stuff
    
    * prettier
    
    * conditional loading delay
    
    * prettier
    
    * approval message changes
    
    * prettier
    
    * minor cleanup
    
    * defensive programming
    
    Co-authored-by: Domen Grabec <grabec@gmail.com>
    Co-authored-by: Roland Podlucki <roland@MacBook-Air.ht.home>

--- dapp/src/components/buySell/_PillArrow.js
+++ dapp/src/components/buySell/_PillArrow.js
@@ -14,9 +14,9 @@ const DownCaret = ({ swapMode, disableRotation, color = '#8293a4' }) => {
       <div className="image-holder-internal"></div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="12"
+        width="13"
         height="13"
-        viewBox="0 0 12 13"
+        viewBox="0 1 12 13"
       >
         <g fill="none" fillRule="evenodd">
           <g fill={color} fillRule="nonzero">
