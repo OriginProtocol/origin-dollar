0d6b21281afcc423fff4307c2c6069c7fa1b7c92
roland <62400812+rolandpo@users.noreply.github.com>
2022-04-15 09:45:21 +0100

    fix input flipping (#974)
    * fix input flipping
    
    * minor polish
    
    Co-authored-by: Domen Grabec <grabec@gmail.com>

--- dapp/src/components/buySell/SwapHomepage.js
+++ dapp/src/components/buySell/SwapHomepage.js
@@ -146,8 +146,8 @@ const SwapHomepage = ({
       if (selectedSwap) {
         const otherCoinAmount =
           Math.floor(selectedSwap.amountReceived * 1000000) / 1000000
-        setSelectedBuyCoinAmount(otherCoinAmount)
-        setSelectedRedeemCoinAmount(selectedBuyCoinAmount)
+        setSelectedBuyCoinAmount(Math.floor(otherCoinAmount * 100) / 100)
+        setSelectedRedeemCoinAmount(Math.floor(otherCoinAmount * 100) / 100)
       }
     }
   }, [swapMode])
