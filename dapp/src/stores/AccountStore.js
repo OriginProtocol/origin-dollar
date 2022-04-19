3ed75207e1b102f600397f943dde98fb3aee1e15
roland <62400812+rolandpo@users.noreply.github.com>
2022-04-19 09:50:05 +0200

    remove token prompt (#979)
    * remove token prompt
    
    * prettier

--- dapp/src/stores/AccountStore.js
+++ dapp/src/stores/AccountStore.js
@@ -12,10 +12,6 @@ const AccountStore = new Store({
   creditsBalanceOf: 0,
   // is user active / engaged with the dapp
   active: 'active', // active / idle
-  /* instead of showing an add OUSD modal right afer mint we set this state to 'waiting'. Wait for the
-   * OUSD balance animation to finish and then that animation sets this state to 'show'.
-   */
-  addOusdModalState: 'none',
   lifetimeYield: null,
 })
 
