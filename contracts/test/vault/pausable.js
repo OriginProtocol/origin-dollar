64d249416740b24211736063c34e1a0f6ae7361e
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-05-17 00:35:58 +0100

    Add pausable vault configuration tests

---
+++ contracts/test/vault/pausable.js
@@ -0,0 +1,29 @@
+const { compoundVaultFixture } = require("../_fixture");
+const { expect } = require("chai");
+
+const { loadFixture, isFork } = require("../helpers");
+
+describe("Pausable Vault Configuration", async () => {
+  if (isFork) {
+    this.timeout(0);
+  }
+
+  it("Governor can pause and unpause", async () => {
+    const { governor, vault } = await loadFixture(compoundVaultFixture);
+    await vault.connect(governor).pause();
+    expect(await vault.capitalPaused()).to.be.true;
+    expect(await vault.rebasePaused()).to.be.true;
+
+    await vault.connect(governor).unpause();
+    expect(await vault.capitalPaused()).to.be.false;
+    expect(await vault.rebasePaused()).to.be.false;
+  });
+
+  it("Governor can set pauser", async () => {
+    const { anna, governor, vault } = await loadFixture(compoundVaultFixture);
+    await expect(vault.connect(governor).setPauser(anna.address))
+      .to.emit(vault, "PauserChanged")
+      .withArgs(anna.address);
+    expect(await vault.pauser()).to.eq(anna.address);
+  });
+});
