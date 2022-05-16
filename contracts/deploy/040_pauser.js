017d1540b6b86ab9ec2d19ddfeb5f75eed4c2a66
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-05-16 18:29:13 +0100

    Implement pauser deployment script

--- contracts/deploy/040_pauser.js
+++ contracts/deploy/040_pauser.js
@@ -1,52 +1,59 @@
-const { deploymentWithProposal, withConfirmation } = require("../utils/deploy");
+const { deploymentWithProposal } = require("../utils/deploy");
 
 module.exports = deploymentWithProposal(
-  { deployName: "040_pauser", forceDeploy: false },
-  async ({ deployWithConfirmation, getTxOpts, ethers }) => {
-    const { deployerAddr, governorAddr } = await getNamedAccounts();
+  { deployName: "040_pauser", forceDeploy: true },
+  async ({ deployWithConfirmation, getTxOpts, ethers, withConfirmation }) => {
+    const { deployerAddr, governorAddr, strategistAddr } =
+      await getNamedAccounts();
     const sDeployer = await ethers.provider.getSigner(deployerAddr);
 
     // Current contracts
-    const cOUSDProxy = await ethers.getContract("OUSDProxy");
+    const cVaultProxy = await ethers.getContract("VaultProxy");
+    const cVault = await ethers.getContractAt("Vault", cVaultProxy.address);
 
     // Deployer Actions
     // ----------------
 
-    // 1. Deploy the new implementation.
-    const dWrappedOusdImpl = await deployWithConfirmation("WrappedOusd", [
-      cOUSDProxy.address,
-      "Wrapped OUSD",
-      "WOUSD",
-    ]);
+    // 1. Deploy the pauser implementation.
+    const dPauserImpl = await deployWithConfirmation("Pauser");
 
-    // 2. Deploy the new proxy
-    const dWrappedOUSDProxy = await deployWithConfirmation("WrappedOUSDProxy");
-    const cWrappedOUSDProxy = await ethers.getContract("WrappedOUSDProxy");
-    const cWrappedOUSD = await ethers.getContractAt(
-      "WrappedOusd",
-      cWrappedOUSDProxy.address
-    );
+    // 2. Deploy the pauser proxy
+    await deployWithConfirmation("PauserProxy");
+    const cPauserProxy = await ethers.getContract("PauserProxy");
+    const cPauser = await ethers.getContractAt("Pauser", cPauserProxy.address);
 
     // 3. Configure Proxy
     await withConfirmation(
-      cWrappedOUSDProxy
+      cPauserProxy
         .connect(sDeployer)
         ["initialize(address,address,bytes)"](
-          dWrappedOusdImpl.address,
+          dPauserImpl.address,
           deployerAddr,
           [],
           await getTxOpts()
         )
     );
 
-    // 3. Initialize Wrapped OUSD
+    // 4. Initialize Pauser with vault as pausable and 12 hours time
+    const expiryDuration = 12 * 60 * 60; // 12 hours in seconds
     await withConfirmation(
-      cWrappedOUSD.connect(sDeployer)["initialize()"](await getTxOpts())
+      cPauser
+        .connect(sDeployer)
+        ["initialize(address,uint256)"](
+          cVaultProxy.address,
+          expiryDuration,
+          await getTxOpts()
+        )
     );
 
-    // 4. Assign ownership
+    // 5. Set Strategist address.
     await withConfirmation(
-      cWrappedOUSD
+      cPauser.connect(sDeployer).setStrategistAddr(strategistAddr)
+    );
+
+    // 6. Assign ownership
+    await withConfirmation(
+      cPauser
         .connect(sDeployer)
         .transferGovernance(governorAddr, await getTxOpts())
     );
@@ -55,12 +62,19 @@ module.exports = deploymentWithProposal(
     // ----------------
 
     return {
-      name: "Claim WOUSD Governance",
+      name: "Claim Pauser governance and setup vault for pauser",
       actions: [
         // 1. Claim governance
         {
-          contract: cWrappedOUSD,
+          contract: cPauser,
           signature: "claimGovernance()",
+          args: [],
+        },
+        // 2. Setup vault for pauser
+        {
+          contract: cVault,
+          signature: "setPauser(address)",
+          args: [cPauser.address],
         },
       ],
     };
