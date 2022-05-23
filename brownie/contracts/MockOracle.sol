5878286b3392259ab4ba2d7bb540406c90afccd2
Daniel Von Fange <daniel@leancoder.com>
2022-05-23 08:49:39 -0400

    Prettier Mock Oracle

--- brownie/contracts/MockOracle.sol
+++ brownie/contracts/MockOracle.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.8.0;
 
 // For testing OUSD's repsonse to oracle price changes
 contract MockOracle {
-	mapping(address=>uint256) public price;
+    mapping(address => uint256) public price;
 
-	function setPrice(address asset, uint256 price_) external {
-		price[asset] = price_;
-	}
+    function setPrice(address asset, uint256 price_) external {
+        price[asset] = price_;
+    }
 }
