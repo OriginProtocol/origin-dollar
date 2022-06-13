64b871a2950828272a57c98d537097e0e5d5a663
Domen Grabec <grabec@gmail.com>
2022-06-13 12:31:09 +0200

    prettier and cleanup

--- contracts/deploy/041_stop_buyback_swaps.js
+++ contracts/deploy/041_stop_buyback_swaps.js
@@ -1,19 +1,10 @@
 const hre = require("hardhat");
 
 const addresses = require("../utils/addresses");
-const { log, deploymentWithProposal } = require("../utils/deploy");
-
+const { deploymentWithProposal } = require("../utils/deploy");
 module.exports = deploymentWithProposal(
   { deployName: "041_stop_buyback_swaps", forceDeploy: false },
-  async ({
-    oracleAddresses,
-    assetAddresses,
-    ethers,
-    deployWithConfirmation,
-    withConfirmation,
-  }) => {
-    const { deployerAddr } = await hre.getNamedAccounts();
-
+  async ({}) => {
     // Buyback contract
     const cBuyback = await ethers.getContract("Buyback");
 
@@ -26,7 +17,7 @@ module.exports = deploymentWithProposal(
           contract: cBuyback,
           signature: "setUniswapAddr(address)",
           args: [addresses.zero],
-        }
+        },
       ],
     };
   }
