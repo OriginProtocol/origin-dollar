39a1154a52036e94505911e833b956dae7fc7b8e
roland <62400812+rolandpo@users.noreply.github.com>
2022-07-12 18:55:41 +0200

    conditional staking banner (#1036)
    * add conditional staking banner
    
    * prettier
    
    * fix button height
    
    * fix mobile spacing
    
    * revert stakeui
    
    * update stakeui
    
    Co-authored-by: Micah Alcorn <mail@micahalcorn.com>

--- dapp/src/components/layout.js
+++ dapp/src/components/layout.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import classnames from 'classnames'
 import Head from 'next/head'
+import Link from 'next/link'
 import { useStoreState } from 'pullstate'
 import { useEffect, useRef } from 'react'
 import { useCookies } from 'react-cookie'
@@ -11,9 +12,11 @@ import { get } from 'lodash'
 import { useEagerConnect } from 'utils/hooks'
 import AccountStore from 'stores/AccountStore'
 import ContractStore from 'stores/ContractStore'
+import StakeStore from 'stores/StakeStore'
 import withRpcProvider from 'hoc/withRpcProvider'
 import AppFooter from './AppFooter'
 import MarketingFooter from './MarketingFooter'
+import { adjustLinkHref } from 'utils/utils'
 import { assetRootPath } from 'utils/image'
 
 const AIRDROP_URL = 'https://governance.ousd.com/claim'
@@ -41,6 +44,9 @@ const Layout = ({
   const rebaseOptedOut = useStoreState(AccountStore, (s) =>
     get(s, 'rebaseOptedOut')
   )
+
+  const stakes = useStoreState(StakeStore, (s) => s)
+  const showStakingBanner = (stakes.stakes || []).length !== 0
 
   const optIn = async () => {
     try {
@@ -148,20 +154,49 @@ const Layout = ({
         </div>
       </div>
       <div
-        className={classnames('notice text-white text-center p-3', {
-          dapp,
-        })}
+        className={classnames(
+          `notice ${
+            showStakingBanner ? 'staking pt-2' : 'pt-3'
+          } text-white text-center pb-3`,
+          {
+            dapp,
+          }
+        )}
       >
         <div className="container d-flex flex-column flex-md-row align-items-center">
-          {fbt('OGV airdrop is live!', 'Airdrop notice')}
-          <a
-            href={AIRDROP_URL}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-dark mt-3 mt-md-0 ml-md-auto"
-          >
-            Check eligibility
-          </a>
+          {showStakingBanner ? (
+            <>
+              <div className="d-flex flex-column mt-0 justify-content-center">
+                <div className="title-text">
+                  {fbt(
+                    'Changes are coming to OGN staking.',
+                    'Changes are coming to OGN staking.'
+                  )}
+                </div>
+                <div className="text">
+                  {fbt(
+                    'Your existing stakes will not be impacted. Claim your OGN at the end of your staking period.',
+                    'Your existing stakes will not be impacted. Claim your OGN at the end of your staking period.'
+                  )}
+                </div>
+              </div>
+              <div className="btn btn-dark mt-2 ml-md-auto">
+                <Link href={adjustLinkHref('/earn')}>Legacy staking</Link>
+              </div>
+            </>
+          ) : (
+            <>
+              {fbt('OGV airdrop is live!', 'Airdrop notice')}
+              <a
+                href={AIRDROP_URL}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-dark mt-3 mt-md-0 ml-md-auto"
+              >
+                Check eligibility
+              </a>
+            </>
+          )}
         </div>
       </div>
       <main className={classnames({ dapp, short, shorter, medium })}>
@@ -174,6 +209,25 @@ const Layout = ({
         .notice {
           background-color: black;
           margin-bottom: 35px;
+        }
+
+        .notice.staking {
+          background-color: #1a82ff;
+        }
+
+        .notice .title-text {
+          font-size: 18px;
+          font-weight: bold;
+          line-height: 1.75;
+          color: white;
+        }
+
+        .notice .text {
+          opacity: 0.8;
+          color: white;
+          line-height: normal;
+          font-size: 14px;
+          max-width: 1000px;
         }
 
         .notice.dapp {
