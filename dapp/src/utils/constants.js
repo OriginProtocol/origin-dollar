5275eedf2c89ed200f8c40f8812dce812b912bfd
roland <62400812+rolandpo@users.noreply.github.com>
2022-07-13 07:57:01 +0200

    selectable APY on homepage (#1033)
    * add selectable apy on homepage
    
    * prettier
    
    * move apy-select from nav to index
    
    * prettier
    
    * fix width
    
    * fix width

--- dapp/src/utils/constants.js
+++ dapp/src/utils/constants.js
@@ -15,6 +15,7 @@ const approveCoinGasLimits = {
   usdc: 60700,
 }
 const apyDayOptions = [7, 30, 60, 90, 365]
+const DEFAULT_SELECTED_APY = 30
 
 module.exports = {
   mintAbsoluteGasLimitBuffer,
@@ -26,4 +27,5 @@ module.exports = {
   curveGasLimitBuffer,
   approveCoinGasLimits,
   apyDayOptions,
+  DEFAULT_SELECTED_APY,
 }
