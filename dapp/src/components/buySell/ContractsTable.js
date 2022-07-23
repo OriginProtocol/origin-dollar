b20befb354b9afd55fa64cd4521cc5fc74e91d41
rolandpo <rolopod@gmail.com>
2022-07-23 14:02:04 +0200

    add info icon to unsupported routes

--- dapp/src/components/buySell/ContractsTable.js
+++ dapp/src/components/buySell/ContractsTable.js
@@ -5,9 +5,66 @@ import { find, sortBy } from 'lodash'
 import { useStoreState } from 'pullstate'
 import { formatCurrency } from 'utils/math'
 import analytics from 'utils/analytics'
+import { assetRootPath } from 'utils/image'
 
 import ContractStore from 'stores/ContractStore'
 import ConfirmationModal from 'components/buySell/ConfirmationModal'
+import Dropdown from 'components/Dropdown'
+
+const Info = ({contract}) => {
+  const [infoOpen, setInfoOpen] = useState(false)
+  const vault = contract === 'vault'
+
+  return (
+    <>
+      <div className="info-box">
+        <Dropdown
+          content={
+            <div className={`d-flex dropdown-menu text-wrap ${vault ? '' : 'short'}`}>
+              {vault ? fbt('The Origin Vault only allows redeeming OUSD into a mix of stablecoins, determined by the proportion of assets it governs', 'Unsupported-vault-message') : fbt('This route only supports redeeming OUSD into a single stablecoin', 'Unsupported-route-message')}
+            </div>
+          }
+          open={infoOpen}
+          onClose={() => setInfoOpen(false)}
+        >
+          <img
+            className="info-icon"
+            src={assetRootPath('/images/question-icon.svg')}
+            onClick={() => {
+              setInfoOpen(!infoOpen)
+            }}
+          />
+        </Dropdown>
+      </div>
+      <style jsx>{`
+        .info-box {
+          display: inline-block;
+          margin-left: 4px;
+        }
+
+        .dropdown-menu {
+          font-size: 14px;
+          top: 115%;
+          left: 0;
+          right: auto;
+          min-width: 230px;
+          padding: 18px 18px 18px 20px;
+        }
+
+        .dropdown-menu.short {
+          min-width: 190px;
+        }
+
+        .info-icon {
+          width: 16px;
+          height: 16px;
+          margin-bottom: 2px;
+          cursor: pointer;
+        }
+      `}</style>
+    </>
+  )
+}
 
 const ContractsTable = () => {
   const swapEstimations = useStoreState(ContractStore, (s) => s.swapEstimations)
@@ -140,6 +197,7 @@ const ContractsTable = () => {
     }
     setAlternateTxRouteConfirmed(isConfirmed)
   }
+
   return (
     <div className="contracts-table">
       {showAlternateRouteModal && (
@@ -357,6 +415,9 @@ const ContractsTable = () => {
                 }`}
               >
                 {empty ? '-' : status}
+                {errorReason === 'unsupported' && (
+                  <Info contract={contract}/>
+                )}
               </div>
             </div>
           )
