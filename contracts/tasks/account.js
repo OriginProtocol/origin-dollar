ea280315cd7d97e3c1f7ffbf4df989e6eb6b400b
Domen Grabec <grabec@gmail.com>
2022-09-13 09:29:24 +0200

    fix for minting on forked environment (#1067)

--- contracts/tasks/account.js
+++ contracts/tasks/account.js
@@ -180,22 +180,17 @@ async function mint(taskArguments, hre) {
   const addresses = require("../utils/addresses");
   const { usdtUnits, isFork, isLocalhost } = require("../test/helpers");
 
-  if (!isFork && !isLocalhost) {
-    throw new Error("Task can only be used on local or fork");
+  if (!isFork) {
+    throw new Error("Task can only be used on fork");
   }
 
-  const ousdProxy = await ethers.getContract("OUSDProxy");
-  const ousd = await ethers.getContractAt("OUSD", ousdProxy.address);
+  const ousd = await ethers.getContractAt("OUSD", addresses.mainnet.OUSDProxy);
+  const vault = await ethers.getContractAt(
+    "IVault",
+    addresses.mainnet.VaultProxy
+  );
 
-  const vaultProxy = await ethers.getContract("VaultProxy");
-  const vault = await ethers.getContractAt("IVault", vaultProxy.address);
-
-  let usdt;
-  if (isFork) {
-    usdt = await hre.ethers.getContractAt(usdtAbi, addresses.mainnet.USDT);
-  } else {
-    usdt = await hre.ethers.getContract("MockUSDT");
-  }
+  const usdt = await hre.ethers.getContractAt(usdtAbi, addresses.mainnet.USDT);
 
   const numAccounts = Number(taskArguments.num) || defaultNumAccounts;
   const accountIndex = Number(taskArguments.index) || defaultAccountIndex;
