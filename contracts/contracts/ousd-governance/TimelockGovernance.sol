e56636402a0e83352abfb488e0ad46edfe06a9f3
Ivan Zhelyazkov <ivan.y.zhelyazkov@gmail.com>
2022-10-31 12:28:51 -0400

    Rename Timelock and consolidate MockOGV contract

--- contracts/contracts/ousd-governance/TimelockGovernance.sol
+++ contracts/contracts/ousd-governance/TimelockGovernance.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "openzeppelin-4.6.0/governance/TimelockController.sol";
 
-contract Timelock is TimelockController {
+contract TimelockGovernance is TimelockController {
     constructor(address[] memory proposers, address[] memory executors)
         TimelockController(86400 * 2, proposers, executors)
     {}
