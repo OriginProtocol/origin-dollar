04cd9cf8bd70cc706a3f4587ab0411b676653bd2
Domen Grabec <grabec@gmail.com>
2022-11-11 23:54:46 +0100

    add morpho strategy address

--- contracts/test/vault/vault.fork-test.js
+++ contracts/test/vault/vault.fork-test.js
@@ -231,12 +231,12 @@ forkOnlyDescribe("ForkTest: Vault", function () {
         "0x5e3646A1Db86993f73E6b74A57D8640B69F7e259", // Aave
         "0xEA2Ef2e2E5A749D4A66b41Db9aD85a38Aa264cb3", // Convex
         "0x89Eb88fEdc50FC77ae8a18aAD1cA0ac27f777a90", // OUSD MetaStrategy
+        "0x5A4eEe58744D1430876d5cA93cAB5CcB763C037D", // Morpho MetaStrategy
         // TODO: Hardcode these after deploy
         "0x4258a6E32Eb33Eed64c3caefe6bcCf53141518dE", // alUSD MetaStrategy
         "0x617F960A19f0f4d560256E04B5F4B92E187C512D", // mUSD MetaStrategy
         "0xB262b69d1dB3dc092C73384B8553DA3d00e93682", // USDD MetaStrategy
         "0xD2001BaF4E726dAfd6589D5c7bf8C74d59E48438", // BUSD MetaStrategy
-        "0xdF58F78cebbb2A60740eD2f86cDf0545a485102F", // Morpho MetaStrategy
       ];
 
       for (const s of strategies) {
