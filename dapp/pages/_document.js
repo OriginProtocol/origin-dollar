bcdb8561d657874fc113f84ded76fbd4cea892d0
rolandpo <rolopod@gmail.com>
2022-12-29 15:27:59 +0100

    fix typo

--- dapp/pages/_document.js
+++ dapp/pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             href="/images/favicon.ico"
             type="image/x-icon"
           />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
           <link
             href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
