5e542c1942698bc53aca83823f8a711c18293f1a
rolandpo <rolopod@gmail.com>
2023-02-02 10:33:33 +0100

    add gtm fields

--- dapp/lib/gtm.js
+++ dapp/lib/gtm.js
@@ -1,8 +1,9 @@
 export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
 
-export const pageview = (url) => {
-  window.dataLayer.push({
+export const pageview = () => {
+  window?.dataLayer?.push({
     event: 'pageview',
-    page: url,
+    pageUrl: window.location.href,
+    pageTitle: document.title,
   })
 }
