36aba7ba786674593ce42055b228e7e77a5b7cf4
Domen Grabec <grabec@gmail.com>
2023-03-29 14:33:01 +0200

    add empty proxy contract

---
+++ contracts/deploy/049_oeth_proxy.js
@@ -0,0 +1,63 @@
+const { deploymentWithProposal } = require("../utils/deploy");
+
+module.exports = deploymentWithProposal(
+  { deployName: "049_oeth_proxy", forceDeploy: false},
+  async ({
+    deployWithConfirmation,
+    ethers,
+    getTxOpts,
+    withConfirmation,
+  }) => {
+    const { deployerAddr, governorAddr } = await getNamedAccounts();
+    const sDeployer = await ethers.provider.getSigner(deployerAddr);
+    
+    // Deployer Actions
+    // ----------------
+
+    // 1. Deploy new proxy
+    // New OETH proxy
+    const dOethProxy = await deployWithConfirmation(
+      "OETHProxy"
+    );
+    const cOethProxy = await ethers.getContractAt(
+      "OETHProxy",
+      dOethProxy.address
+    );
+
+    // 2. Deploy new implementation
+    const dOETHImpl = await deployWithConfirmation("OETH");
+
+    // 3. Init the proxy to point at the implementation
+    await withConfirmation(
+      cOethProxy
+        .connect(sDeployer)
+        ["initialize(address,address,bytes)"](
+          dOETHImpl.address,
+          deployerAddr,
+          [],
+          await getTxOpts()
+        )
+    );
+
+    // 5. Transfer governance
+    await withConfirmation(
+      cOethProxy
+        .connect(sDeployer)
+        .transferGovernance(governorAddr, await getTxOpts())
+    );
+
+    // Governance Actions
+    // ----------------
+    return {
+      name: "Deploy empty OETH proxy",
+      actions: [
+        // 1. Accept governance of OETH proxy
+        {
+          contract: cOethProxy,
+          signature: "claimGovernance()",
+          args: [],
+        }
+      ],
+    };
+  }
+);
