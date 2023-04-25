775cac2b058c33b50f79e9ea188e4040c51be8f4
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-04-25 22:39:16 +0400

    Add build script

---
+++ devops/build-frontend.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+buildContracts() {
+  if [[ $APP_ID == "ousd-dapp" || $APP_ID == "oeth-dapp" ]]
+  then
+    echo "Building contract for the DApp";
+    cd contracts;
+    NODE_ENV=development yarn install;
+    yarn build;
+    cd --;
+  fi
+}
+
+buildApp() {
+  buildContracts;
+  yarn install;
+  NODE_ENV=production nx build $APP_ID;
+}
