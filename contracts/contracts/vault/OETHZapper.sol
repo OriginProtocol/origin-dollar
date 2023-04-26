933998ce5d1a9798146c9ee07e593cb631d75382
Daniel Von Fange <daniel@leancoder.com>
2023-04-26 12:09:15 -0400

    Zapper cleanup

--- contracts/contracts/vault/OETHZapper.sol
+++ contracts/contracts/vault/OETHZapper.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.0;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IOUSD } from "../interfaces/IOUSD.sol";
 import { IVault } from "../interfaces/IVault.sol";
 import { IWETH9 } from "../interfaces/IWETH9.sol";
 import { ISfrxETH } from "../interfaces/ISfrxETH.sol";
 
 contract OETHZapper {
-    IOUSD immutable oeth;
+    IERC20 immutable oeth;
     IVault immutable vault;
+    
     IWETH9 constant weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 constant frxeth = IERC20(0x5E8422345238F34275888049021821E8E08CAa1f);
     ISfrxETH constant sfrxeth =
         ISfrxETH(0xac3E018457B222d93114458476f3E3416Abbe38F);
     address constant ETH_MARKER = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address constant FRXETH = 0x5E8422345238F34275888049021821E8E08CAa1f;
 
     event MintFrom(
         address indexed minter,
@@ -23,13 +23,13 @@ contract OETHZapper {
     );
 
     constructor(address _oeth, address _vault) {
-        oeth = IOUSD(_oeth);
+        oeth = IERC20(_oeth);
         vault = IVault(_vault);
 
         // slither-disable-next-line unused-return
         weth.approve(address(_vault), type(uint256).max);
         // slither-disable-next-line unused-return
-        IERC20(FRXETH).approve(address(_vault), type(uint256).max);
+        frxeth.approve(address(_vault), type(uint256).max);
     }
 
     receive() external payable {
@@ -49,11 +49,7 @@ contract OETHZapper {
         // slither-disable-next-line unused-return
         sfrxeth.redeem(amount, address(this), msg.sender);
         emit MintFrom(msg.sender, address(sfrxeth), amount);
-        return _mint(FRXETH, minOETH);
-    }
-
-    function rebaseOptIn() external {
-        oeth.rebaseOptIn(); // Gas savings for every zap
+        return _mint(address(frxeth), minOETH);
     }
 
     function _mint(address asset, uint256 minOETH) internal returns (uint256) {
