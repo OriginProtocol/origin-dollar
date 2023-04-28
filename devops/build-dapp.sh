bfbc1cfec2d51d1f3df807e536d47dca9285126f
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-04-28 19:50:40 +0400

    Use bash instead

--- devops/build-dapp.sh
+++ devops/build-dapp.sh
@@ -4,9 +4,11 @@ buildContracts() {
   echo "Building contract for the DApp";
   cd contracts;
   NODE_ENV=development yarn install;
-  if [[ $APP_ID == 'oeth-dapp' ]]; then
+  if [ "$APP_ID" = "oeth-dapp" ]; then
+  echo "Generating ABI for the OETH DApp";
     yarn run deploy:oeth
   else
+    echo "Generating ABI for the OUSD DApp";
     yarn run deploy
   fi
   cd --;
@@ -15,7 +17,7 @@ buildContracts() {
 
 buildApp() {
   buildContracts;
-  if [[ $APP_ID == 'oeth-dapp' ]]; then
+  if [ "$APP_ID" = "oeth-dapp" ]; then
     cd "dapp-oeth";
   else
     cd "dapp";
--- devops/start-dapp.sh
+++ devops/start-dapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 startApp() {
-  if [[$APP_ID == 'oeth-dapp']]; then
+  if [ "$APP_ID" = "oeth-dapp" ]; then
     cd "dapp-oeth";
   else
     cd "dapp";
