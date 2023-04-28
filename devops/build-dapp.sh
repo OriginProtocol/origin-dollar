dde9e9fa10ff22984ff9027478b572da899284c6
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-04-28 19:24:17 +0400

    Update scripts

--- devops/build-dapp.sh
+++ devops/build-dapp.sh
@@ -1,21 +1,19 @@
 buildContracts() {
-  if [[ $APP_ID == "ousd-dapp" || $APP_ID == "oeth-dapp" ]]; then
-    echo "Building contract for the DApp";
-    cd contracts;
-    NODE_ENV=development yarn install;
-    if [[ $APP_ID == "oeth-dapp" ]]; then
-      yarn run deploy:oeth
-    else
-      yarn run deploy
-    fi
-    cd --;
-    rm -rf contracts/node_modules # Purge
+  echo "Building contract for the DApp";
+  cd contracts;
+  NODE_ENV=development yarn install;
+  if [[ $APP_ID == "oeth-dapp" ]]; then
+    yarn run deploy:oeth
+  else
+    yarn run deploy
   fi
+  cd --;
+  rm -rf contracts/node_modules # Purge
 }
 
 buildApp() {
   buildContracts;
-  if [[ $APP_ID == "oeth-dapp" ]]; then
+  if $APP_ID == "oeth-dapp"; then
     cd "dapp-oeth";
   else
     cd "dapp";
