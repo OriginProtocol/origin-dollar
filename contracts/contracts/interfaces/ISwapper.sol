f5963c45dc47c7768f1990701ce5e3e8f6b8ea08
Nicholas Addison <nick@addisonbrown.com.au>
2023-05-26 01:45:33 +1000

    collateral swap on vault now calls Swapper

---
+++ contracts/contracts/interfaces/ISwapper.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+interface ISwapper {
+    /**
+     * @param fromAsset The token address of the asset being sold.
+     * @param toAsset The token address of the asset being purchased.
+     * @param fromAssetAmount The amount of assets being sold.
+     * @param minToAssetAmmount The minimum amount of assets to be purchased.
+     * @param data tx.data returned from 1Inch's /v5.0/1/swap API
+     */
+    function swap(
+        address fromAsset,
+        address toAsset,
+        uint256 fromAssetAmount,
+        uint256 minToAssetAmmount,
+        bytes calldata data
+    ) external returns (uint256 toAssetAmount);
+}
