397dae7814a582aa2c4b88fc27e092b54476875a
Nicholas Addison <nick@addisonbrown.com.au>
2023-05-26 22:13:14 +1000

    Fixed vault upgrade for collateral swaps

--- contracts/contracts/interfaces/IVault.sol
+++ contracts/contracts/interfaces/IVault.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import { VaultStorage } from "../vault/VaultStorage.sol";
+
 interface IVault {
     event AssetSupported(address _asset);
     event AssetDefaultStrategyUpdated(address _asset, address _strategy);
@@ -144,6 +146,14 @@ interface IVault {
 
     function rebase() external;
 
+    function swapCollateral(
+        address fromAsset,
+        address toAsset,
+        uint256 fromAssetAmount,
+        uint256 minToAssetAmount,
+        bytes calldata data
+    ) external returns (uint256 toAssetAmount);
+
     function totalValue() external view returns (uint256 value);
 
     function checkBalance(address _asset) external view returns (uint256);
@@ -154,6 +164,11 @@ interface IVault {
         returns (uint256[] memory);
 
     function getAssetCount() external view returns (uint256);
+
+    function getAssetConfig(address _asset)
+        external
+        view
+        returns (VaultStorage.Asset memory config);
 
     function getAllAssets() external view returns (address[] memory);
 
