f47c0cfa07d7d4657dbbe06d7a2b90bd59d57d3d
Nicholas Addison <nick@addisonbrown.com.au>
2023-05-30 17:22:40 +1000

    Added getIInchSwapData to dynamically get 1Inch swap tx data Added axios to dev deps Upgraded waffle testing deps

--- contracts/utils/addresses.js
+++ contracts/utils/addresses.js
@@ -155,8 +155,12 @@ addresses.mainnet.WOETHProxy = "0xDcEe70654261AF21C44c093C300eD3Bb97b78192";
 
 // Tokens
 addresses.mainnet.sfrxETH = "0xac3E018457B222d93114458476f3E3416Abbe38F";
-addresses.mainnet.frxETH = "0x5e8422345238f34275888049021821e8e08caa1f";
+addresses.mainnet.frxETH = "0x5E8422345238F34275888049021821E8E08CAa1f";
 addresses.mainnet.rETH = "0xae78736Cd615f374D3085123A210448E74Fc6393";
 addresses.mainnet.stETH = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84";
+
+// 1Inch
+addresses.mainnet.oneInchRouterV5 =
+  "0x1111111254EEB25477B68fb85Ed929f73A960582";
 
 module.exports = addresses;
