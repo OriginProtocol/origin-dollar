1d2854b3aa358438b3a4199baafa14836d3d18b0
Nicholas Addison <nick@addisonbrown.com.au>
2023-05-30 17:22:40 +1000

    Added getIInchSwapData to dynamically get 1Inch swap tx data Added axios to dev deps Upgraded waffle testing deps

--- contracts/utils/1Inch.js
+++ contracts/utils/1Inch.js
@@ -1,4 +1,9 @@
+const axios = require("axios");
 const { defaultAbiCoder } = require("ethers/lib/utils");
+
+const addresses = require("./addresses");
+
+const ONE_INCH_API = "https://api.1inch.io/v5.0/1/swap";
 
 /**
  * Re-encodes the 1Inch swap data to be used by the vault's swapper
@@ -8,7 +13,7 @@ const { defaultAbiCoder } = require("ethers/lib/utils");
 const recodeSwapData = async (apiEncodedData) => {
   const c1InchRouter = await ethers.getContractAt(
     "IOneInchRouter",
-    "0x1111111254EEB25477B68fb85Ed929f73A960582"
+    addresses.mainnet.oneInchRouterV5
   );
   const apiDecodedData = await c1InchRouter.interface.decodeFunctionData(
     "swap",
@@ -21,6 +26,44 @@ const recodeSwapData = async (apiEncodedData) => {
   );
 };
 
+const getIInchSwapData = async ({
+  vault,
+  fromAsset,
+  toAsset,
+  fromAmount,
+  slippage,
+}) => {
+  const swapper = await ethers.getContract("Swapper1InchV5");
+
+  const params = {
+    fromTokenAddress: fromAsset.address,
+    toTokenAddress: toAsset.address,
+    amount: fromAmount.toString(),
+    fromAddress: swapper.address,
+    destReceiver: vault.address,
+    slippage: slippage ?? 1,
+    disableEstimate: true,
+    allowPartialFill: false,
+  };
+
+  try {
+    const response = await axios.get(ONE_INCH_API, { params });
+
+    if (!response.data.tx || !response.data.tx.data) {
+      throw Error("response is missing tx.data");
+    }
+
+    return response.data.tx.data;
+  } catch (err) {
+    if (err.response) {
+      console.error("Response data  : ", err.response.data);
+      console.error("Response status: ", err.response.status);
+    }
+    throw Error(`Call to 1Inch swap API failed: ${err.message}`);
+  }
+};
+
 module.exports = {
   recodeSwapData,
+  getIInchSwapData,
 };
