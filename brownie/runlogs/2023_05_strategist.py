2a86b68e85b4ca6f19f52f46f8e1e887368c6611
Domen Grabec <grabec@gmail.com>
2023-05-31 16:40:00 +0200

    add withdrawal test

--- brownie/runlogs/2023_05_strategist.py
+++ brownie/runlogs/2023_05_strategist.py
@@ -360,3 +360,28 @@ for idx, item in enumerate(txs):
   print("To: ", item.receiver)
   print("Data (Hex encoded): ", item.input, "\n")
 
+
+# --------------------------------
+# May 31, 2023 - OETH Morpho Aave deposit - test that withdrawal works
+# --------------------------------
+txs = []
+with TemporaryFork():
+  # Before
+  txs.append(vault_oeth_core.rebase({'from':STRATEGIST}))
+  txs.append(oeth_vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+  # Strategist
+  txs.append(vault_oeth_admin.withdrawAllFromStrategy(OETH_MORPHO_AAVE_STRAT, {'from': STRATEGIST}))
+
+  #After
+  vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+  supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+  profit = vault_change - supply_change
+  txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (0.1 * 10**18), {'from': STRATEGIST}))
+
+  morpho_balance = oeth_morpho_aave_strat.checkBalance(WETH)
+  print("-----")
+  print("Profit", "{:.6f}".format(profit / 10**18), profit)
+  print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+  print("Morpho Aave Balance", "{:.6f}".format(morpho_balance / 10**18))
+  print("-----")
