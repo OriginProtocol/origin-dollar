5e00784bb0878fa930750c1b00ca6e4f3ca90a93
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-06-01 06:21:20 -0700

    fix get

--- dapp-oeth/src/hooks/useCurrencySwapper.js
+++ dapp-oeth/src/hooks/useCurrencySwapper.js
@@ -140,7 +140,7 @@ const useCurrencySwapper = ({
       }
 
       const allowance = parseFloat(
-        get(allowances, 'coinNeedingApproval[allowanceCheckKey]')
+        get(allowances, `${coinNeedingApproval}.${allowanceCheckKey}`)
       )
 
       setNeedsApproval(
