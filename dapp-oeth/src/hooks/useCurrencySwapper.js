94332e1547514ac3131e31bba117609cb6242f13
HrikB <hrik.bhowal@gmail.com>
2023-06-01 10:43:32 -0400

    Undefined dapp crash (#1573)
    * fix dapp crash from undefined
    
    * fix get

--- dapp-oeth/src/hooks/useCurrencySwapper.js
+++ dapp-oeth/src/hooks/useCurrencySwapper.js
@@ -140,7 +140,8 @@ const useCurrencySwapper = ({
       }
 
       const allowance = parseFloat(
-        get(allowances, 'coinNeedingApproval[allowanceCheckKey]')
+        get(allowances, `${coinNeedingApproval}.${allowanceCheckKey}`)
+
       )
 
       setNeedsApproval(
