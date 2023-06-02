ec49b58e0b68c005de6dc7fb5e1bb6969c43a44c
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-06-02 22:48:30 +0400

    Fix funding script

--- contracts/utils/funding.js
+++ contracts/utils/funding.js
@@ -135,7 +135,7 @@ const fundAccounts = async () => {
           .transfer(
             address,
             utils.parseUnits(
-              tokenContract in ousdCoins ? "1000000" : "200",
+              ousdCoins.find(x => x.address == tokenContract.address) ? "1000000" : "200",
               await tokenContract.decimals()
             )
           );
