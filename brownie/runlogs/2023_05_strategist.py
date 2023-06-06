590447c35a37249cc19ce68c95eb0b62ce83f890
Domen Grabec <grabec@gmail.com>
2023-06-06 15:08:50 +0200

    ETH Morpho Aave allocation (#1563)
    * add ETH Morpho Aave allocation
    
    * minor corrections
    
    * add withdrawal test
    
    * Oeth morpho runlog 2 (#1580)
    
    * add Convex AMO runlog
    
    * withdrawAll from Morpho ETH runlog
    
    * remove all funds from aave runlog

--- brownie/runlogs/2023_05_strategist.py
+++ brownie/runlogs/2023_05_strategist.py
@@ -321,3 +321,67 @@ safe = ApeSafe('0xF14BBdf064E3F67f51cd9BD646aE3716aD938FDC')
 safe_tx = safe.multisend_from_receipts(txs)
 safe.sign_with_frame(safe_tx)
 r = safe.post_transaction(safe_tx)
+
+
+
+
+# --------------------------------
+# May 31, 2023 - OETH Morpho Aave deposit
+# --------------------------------
+
+from world import *
+
+txs = []
+with TemporaryFork():
+  # Before
+  txs.append(vault_oeth_core.rebase({'from':STRATEGIST}))
+  txs.append(oeth_vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+  # Strategist
+  txs.append(vault_oeth_admin.depositToStrategy(OETH_MORPHO_AAVE_STRAT, [weth], [57.2*10**18], {'from': STRATEGIST}))
+
+  #After
+  vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+  supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+  profit = vault_change - supply_change
+  txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (0.1 * 10**18), {'from': STRATEGIST}))
+
+  morpho_balance = oeth_morpho_aave_strat.checkBalance(WETH)
+  print("-----")
+  print("Profit", "{:.6f}".format(profit / 10**18), profit)
+  print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+  print("Morpho Aave Balance", "{:.6f}".format(morpho_balance / 10**18))
+  print("-----")
+print("Est Gas Max: {:,}".format(1.10*sum([x.gas_used for x in txs])))
+
+print("Schedule the following transactions on Gnosis Safe")
+for idx, item in enumerate(txs):
+  print("Transaction ", idx)
+  print("To: ", item.receiver)
+  print("Data (Hex encoded): ", item.input, "\n")
+
+
+# --------------------------------
+# May 31, 2023 - OETH Morpho Aave deposit - test that withdrawal works
+# --------------------------------
+txs = []
+with TemporaryFork():
+  # Before
+  txs.append(vault_oeth_core.rebase({'from':STRATEGIST}))
+  txs.append(oeth_vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+  # Strategist
+  txs.append(vault_oeth_admin.withdrawAllFromStrategy(OETH_MORPHO_AAVE_STRAT, {'from': STRATEGIST}))
+
+  #After
+  vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+  supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+  profit = vault_change - supply_change
+  txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (0.1 * 10**18), {'from': STRATEGIST}))
+
+  morpho_balance = oeth_morpho_aave_strat.checkBalance(WETH)
+  print("-----")
+  print("Profit", "{:.6f}".format(profit / 10**18), profit)
+  print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+  print("Morpho Aave Balance", "{:.6f}".format(morpho_balance / 10**18))
+  print("-----")
