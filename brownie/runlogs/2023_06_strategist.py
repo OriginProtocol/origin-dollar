cf9305273fb6454629aac54afc99f83d1612bae9
Domen Grabec <grabec@gmail.com>
2023-06-15 13:37:14 +0200

    OETH June allocation

--- brownie/runlogs/2023_06_strategist.py
+++ brownie/runlogs/2023_06_strategist.py
@@ -202,3 +202,42 @@ for idx, item in enumerate(txs):
   print("To: ", item.receiver)
   print("Data (Hex encoded): ", item.input, "\n")
 
+
+# --------------------------------
+# June 2, 2023 - OETH Morpho Aave withdrawAll
+# --------------------------------
+
+from world import *
+
+txs = []
+with TemporaryFork():
+  # Before
+  # Before
+  txs.append(vault_oeth_core.rebase({'from':STRATEGIST}))
+  txs.append(oeth_vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+  # Strategist
+  txs.append(vault_oeth_admin.depositToStrategy(OETH_CONVEX_OETH_ETH_STRAT, [weth], [877.39264*1e18], {'from': STRATEGIST}))
+  txs.append(vault_oeth_admin.depositToStrategy(OETH_MORPHO_AAVE_STRAT, [weth], [103.28868*1e18], {'from': STRATEGIST}))
+
+  #After
+  vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+  supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+  profit = vault_change - supply_change
+  txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (10 * 10**18), {'from': STRATEGIST}))
+
+  morpho_balance = oeth_morpho_aave_strat.checkBalance(WETH)
+  weth_balance = weth.balanceOf(vault_oeth_admin)
+  print("-----")
+  print("Profit", "{:.6f}".format(profit / 10**18), profit)
+  print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+  print("Morpho Aave Balance", "{:.6f}".format(morpho_balance / 10**18))
+  print("WETH Balance", "{:.6f}".format(weth_balance / 10**18))
+  print("-----")
+print("Est Gas Max: {:,}".format(1.10*sum([x.gas_used for x in txs])))
+
+print("Schedule the following transactions on Gnosis Safe")
+for idx, item in enumerate(txs):
+  print("Transaction ", idx)
+  print("To: ", item.receiver)
+  print("Data (Hex encoded): ", item.input, "\n")
