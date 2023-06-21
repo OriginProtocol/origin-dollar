192c1c5008361460d59e7fce01ada9985ecb502d
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-06-21 12:33:12 +0530

    Revert log statements

--- contracts/contracts/strategies/FraxETHStrategy.sol
+++ contracts/contracts/strategies/FraxETHStrategy.sol
@@ -13,7 +13,7 @@ import { IWETH9 } from "../interfaces/IWETH9.sol";
 import { IFraxETHMinter } from "../interfaces/IFraxETHMinter.sol";
 import { Generalized4626Strategy } from "./Generalized4626Strategy.sol";
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 contract FraxETHStrategy is Generalized4626Strategy {
     using SafeERC20 for IERC20;
@@ -34,10 +34,10 @@ contract FraxETHStrategy is Generalized4626Strategy {
             // slither-disable-next-line unused-return
             fraxETHMinter.submitAndDeposit{ value: _amount }(address(this));
         } else if (_asset == address(assetToken)) {
-            console.log("Platform: %s", platformAddress);
-            console.log("frxETH balance: %s", IERC20(assetToken).balanceOf(address(this)));
-            console.log("frxETH allowance: %s", IERC20(assetToken).allowance(address(this), address(shareToken)));
-            console.log("amount: %s", _amount);
+            // console.log("Platform: %s", platformAddress);
+            // console.log("frxETH balance: %s", IERC20(assetToken).balanceOf(address(this)));
+            // console.log("frxETH allowance: %s", IERC20(assetToken).allowance(address(this), address(shareToken)));
+            // console.log("amount: %s", _amount);
 
             // Stake frxETH
             // slither-disable-next-line unused-return
