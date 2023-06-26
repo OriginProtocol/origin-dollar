16843543f1562cc4fd4e5e2acdb0bbea6125ccbb
Domen Grabec <grabec@gmail.com>
2023-06-26 17:50:44 +0200

    prettier

--- contracts/deploy/068_oeth_to_ogv_governance_p1.js
+++ contracts/deploy/068_oeth_to_ogv_governance_p1.js
@@ -4,19 +4,23 @@ const addresses = require("../utils/addresses");
 module.exports = deploymentWithProposal(
   {
     deployName: "068_oeth_to_ogv_governance_p1",
-    forceDeploy: false
+    forceDeploy: false,
   },
-  async ({
-    ethers,
-  }) => {
+  async ({ ethers }) => {
     const cOETHProxy = await ethers.getContract("OETHProxy");
     const cOETHVaultProxy = await ethers.getContract("OETHVaultProxy");
     const cWOETHProxy = await ethers.getContract("WOETHProxy");
     const cOETHDripperProxy = await ethers.getContract("OETHDripperProxy");
-    const cConvexEthMetaStrategyProxy = await ethers.getContract("ConvexEthMetaStrategyProxy");
+    const cConvexEthMetaStrategyProxy = await ethers.getContract(
+      "ConvexEthMetaStrategyProxy"
+    );
     const cOETHHarvesterProxy = await ethers.getContract("OETHHarvesterProxy");
-    const cOETHMorphoAaveStrategyProxy = await ethers.getContract("OETHMorphoAaveStrategyProxy");
-    const cFraxETHStrategyProxy = await ethers.getContract("FraxETHStrategyProxy");
+    const cOETHMorphoAaveStrategyProxy = await ethers.getContract(
+      "OETHMorphoAaveStrategyProxy"
+    );
+    const cFraxETHStrategyProxy = await ethers.getContract(
+      "FraxETHStrategyProxy"
+    );
 
     // Governance Actions
     // ----------------
