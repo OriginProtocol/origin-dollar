1c1cd047d1469456fd5a69f7e8d8fdb65b4be250
Nicholas Addison <nick@addisonbrown.com.au>
2023-06-27 17:59:21 +1000

    Ignore echidna testing contracts from coverage

--- contracts/.solcover.js
+++ contracts/.solcover.js
@@ -2,6 +2,7 @@ module.exports = {
   skipFiles: [
     "mocks", // Testing
     "crytic", // Testing
+    "echidna", // Testing
     "token/OUSDResolutionUpgrade.sol", // No longer used
     "oracle/MixOracle.sol", // No longer used
     "vault/VaultInitializer.sol", // No longer used
