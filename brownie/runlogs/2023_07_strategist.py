93f23aff50406976d337f57668210c1e64b51acc
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-07-06 11:39:18 +0530

    Add collateral swap runlog (#1682)
    * Add collateral swap runlog

--- brownie/runlogs/2023_07_strategist.py
+++ brownie/runlogs/2023_07_strategist.py
@@ -67,3 +67,42 @@ def main():
       print("Transaction ", idx)
       print("To: ", item.receiver)
       print("Data (Hex encoded): ", item.input, "\n")
+
+# -----------------------------------
+# July 6, 2023 - Collateral Swap Test
+# -----------------------------------
+from collateralSwap import *
+
+txs = []
+
+def main():
+  with TemporaryFork():
+    # Before
+    txs.append(oeth_vault_core.rebase({'from':STRATEGIST}))
+    txs.append(oeth_vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+    # Swap 50 stETH for frxETH with 0.1% tolerance
+    _, swap_data = build_swap_tx(STETH, FRXETH, 50 * 10**18, 0.1, False)
+    decoded_input = vault_core_w_swap_collateral.swapCollateral.decode_input(swap_data)
+    txs.append(
+      vault_core_w_swap_collateral.swapCollateral(*decoded_input, {'from':STRATEGIST})
+    )
+    txs.append(
+      oeth_vault_core.allocate({'from':STRATEGIST})
+    )
+
+    # After
+    vault_change = oeth_vault_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (0.1 * 10**18), {'from': STRATEGIST}))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
+    print("Schedule the following transactions on Gnosis Safe")
+    for idx, item in enumerate(txs):
+      print("Transaction ", idx)
+      print("To: ", item.receiver)
+      print("Data (Hex encoded): ", item.input, "\n")
