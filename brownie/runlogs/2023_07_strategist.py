e9ef1bcf9eb7c9e124c44e70a4687316201663d1
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-07-15 09:08:06 +0400

    Add July 13 runlog (#1700)
    * Add July 13 runlog
    
    * Update code owners
    
    * Code change
    
    * Update code
    
    * Update script
    
    * Cleanup

--- brownie/runlogs/2023_07_strategist.py
+++ brownie/runlogs/2023_07_strategist.py
@@ -148,3 +148,110 @@ def main():
       print("Transaction ", idx)
       print("To: ", item.receiver)
       print("Data (Hex encoded): ", item.input, "\n")
+# -------------------------------
+# July 13, 2023 - OUSD Allocation
+# -------------------------------
+from world import *
+from allocations import *
+
+txs = []
+
+votes = """
+Morpho Aave USDT  79.46%
+Convex OUSD+3Crv  9.93%
+Morpho Compound DAI 7.06%
+Morpho Compound USDC 2.99%
+Morpho Compound USDT 0.57%
+Morpho Aave DAI 0%
+Morpho Aave USDC  0%
+Convex DAI+USDC+USDT  0%
+Aave DAI  0%
+Convex LUSD+3Crv  0%
+Existing Allocation 0%
+Aave USDC 0%
+Aave USDT 0%
+Compound DAI  0%
+Compound USDC 0%
+Compound USDT 0%
+"""
+
+with TemporaryForkWithVaultStats(votes=votes):
+  # Before
+  txs.append(vault_core.rebase({'from': STRATEGIST}))
+  txs.append(vault_value_checker.takeSnapshot({'from': STRATEGIST}))
+
+  # Withdraw all from Aave
+  txs.append(
+    vault_admin.withdrawAllFromStrategy(AAVE_STRAT, {'from': STRATEGIST})
+  )
+
+  # Deposit 610k USDC and 116k USDT to Morpho Compound
+  txs.append(
+    to_strat(
+      MORPHO_COMP_STRAT,
+      [
+        [610_430.4, usdc],
+        [116_194.6, usdt],
+      ]
+    )
+  )
+
+  # Withdraw all from Morpho Aave
+  txs.append(
+    vault_admin.withdrawAllFromStrategy(MORPHO_AAVE_STRAT, {'from': STRATEGIST})
+  )
+
+  # Deposit 3.61M USDT and USDC to Curve AMO
+  txs.append(
+    to_strat(
+      OUSD_METASTRAT,
+      [
+        [1_335_700, usdc],
+        [2_274_300, usdt],
+      ]
+    )
+  )
+
+  # Withdraw 20% in USDT and rest in DAI from Curve AMO
+  txs.append(
+    from_strat(
+      OUSD_METASTRAT,
+      [
+        [200_000, usdt],
+        [800_000, dai],
+      ]
+    )
+  )
+
+  # Deposit all DAI to Morpho Compound
+  txs.append(
+    to_strat(MORPHO_COMP_STRAT, [[944_382, dai]])
+  )
+
+  # Deposit all USDT to Morpho Aave
+  txs.append(
+    to_strat(MORPHO_AAVE_STRAT, [[16_094_394, usdt]])
+  )
+
+  # Finish it off with a rebase
+  txs.append(vault_core.rebase({'from': STRATEGIST}))
+
+  # Set default strategies
+  txs.append(vault_admin.setAssetDefaultStrategy(usdc, MORPHO_COMP_STRAT,{'from' :STRATEGIST}))
+  txs.append(vault_admin.setAssetDefaultStrategy(dai, MORPHO_COMP_STRAT,{'from': STRATEGIST}))
+
+  # After
+  vault_change = vault_core.totalValue() - vault_value_checker.snapshots(STRATEGIST)[0]
+  supply_change = ousd.totalSupply() - vault_value_checker.snapshots(STRATEGIST)[1]
+  profit = vault_change - supply_change
+
+  txs.append(vault_value_checker.checkDelta(profit, (500 * 10**18), vault_change, (500 * 10**18), {'from': STRATEGIST}))
+  print("-----")
+  print("Profit", "{:.6f}".format(profit / 10**18), profit)
+  print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
+  print("Schedule the following transactions on Gnosis Safe")
+  for idx, item in enumerate(txs):
+    print("Transaction ", idx)
+    print("To: ", item.receiver)
+    print("Data (Hex encoded): ", item.input, "\n")
