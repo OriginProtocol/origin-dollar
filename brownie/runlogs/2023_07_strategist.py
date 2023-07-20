d1cc4654d3145ec0ac50189baf77046f0c9f5fb0
Nicholas Addison <nick@addisonbrown.com.au>
2023-07-20 16:49:44 +1000

    Added run log for 2,500 rETH to WETH swap

--- brownie/runlogs/2023_07_strategist.py
+++ brownie/runlogs/2023_07_strategist.py
@@ -255,3 +255,43 @@ with TemporaryForkWithVaultStats(votes=votes):
     print("Transaction ", idx)
     print("To: ", item.receiver)
     print("Data (Hex encoded): ", item.input, "\n")
+
+
+# -----------------------------------
+# July 20, 2023 - Collateral Swap of 2,500 rETH for WETH
+# -----------------------------------
+from collateralSwap import *
+
+txs = []
+
+def main():
+  with TemporaryFork():
+    # Before
+    txs.append(oeth_vault_core.rebase({'from':STRATEGIST}))
+    txs.append(oeth_vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+    # Swap 2,500 rETH for WETH with 0.1% tolerance
+    _, swap_data = build_swap_tx(RETH, WETH, 2500 * 10**18, 0.1, False)
+    decoded_input = vault_core_w_swap_collateral.swapCollateral.decode_input(swap_data)
+    txs.append(
+      vault_core_w_swap_collateral.swapCollateral(*decoded_input, {'from':STRATEGIST})
+    )
+
+    # Deposit 2500 WETH to Convex OETH-ETH strategy
+    txs.append(vault_oeth_admin.depositToStrategy(OETH_CONVEX_OETH_ETH_STRAT, [WETH], [2500*1e18], {'from': STRATEGIST}))
+
+    # After
+    vault_change = oeth_vault_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (0.1 * 10**18), {'from': STRATEGIST}))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
+    print("Schedule the following transactions on Gnosis Safe")
+    for idx, item in enumerate(txs):
+      print("Transaction ", idx)
+      print("To: ", item.receiver)
+      print("Data (Hex encoded): ", item.input, "\n")
