16a45a620020a02feb2fedc4a5035f338ad3fdc4
Nick Addison <nick@addisonbrown.com.au>
2023-07-26 07:57:07 +1000

    Refactor of Hardhat tasks (#1716)
    * Refactor of Hardhat tasks

---
+++ contracts/utils/txLogger.js
@@ -0,0 +1,32 @@
+const { formatUnits } = require("ethers/lib/utils");
+
+const log = require("./logger")("utils:txLogger");
+
+/**
+ * Log transaction details after the tx has been sent and mined.
+ * @param {ContractTransaction} tx transaction sent to the network
+ * @param {string} method description of the tx. eg method name
+ * @returns {ContractReceipt} transaction receipt
+ */
+async function logTxDetails(tx, method) {
+  log(
+    `Sent ${method} transaction with hash ${tx.hash} from ${
+      tx.from
+    } with gas price ${tx.gasPrice?.toNumber() / 1e9} Gwei`
+  );
+  const receipt = await tx.wait();
+
+  // Calculate tx cost in Wei
+  const txCost = receipt.gasUsed.mul(tx.gasPrice ?? 0);
+  log(
+    `Processed ${method} tx in block ${receipt.blockNumber}, using ${
+      receipt.gasUsed
+    } gas costing ${formatUnits(txCost)} ETH`
+  );
+
+  return receipt;
+}
+
+module.exports = {
+  logTxDetails,
+};
