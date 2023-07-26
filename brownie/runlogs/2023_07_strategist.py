83d9365cdd3211786fc527009046990dc22c603b
Nick Addison <nick@addisonbrown.com.au>
2023-07-26 22:08:59 +1000

    Run log for 260 WETH AMO Deposit (#1715)

--- brownie/runlogs/2023_07_strategist.py
+++ brownie/runlogs/2023_07_strategist.py
@@ -410,3 +410,36 @@ with TemporaryFork():
     print("Transaction ", idx)
     print("To: ", item.receiver)
     print("Data (Hex encoded): ", item.input, "\n")
+
+# --------------------------------
+# July 25, 2023 - OETH AMO Deposit
+# --------------------------------
+
+from world import *
+
+txs = []
+with TemporaryFork():
+  # Before
+  txs.append(vault_oeth_core.rebase({'from':STRATEGIST}))
+  txs.append(oeth_vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+  # Strategist
+  txs.append(vault_oeth_admin.depositToStrategy(OETH_CONVEX_OETH_ETH_STRAT, [weth], [260 * 1e18], {'from': STRATEGIST}))
+
+  #After
+  vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+  supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+  profit = vault_change - supply_change
+  txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), {'from': STRATEGIST}))
+  print("-----")
+  print("Profit", "{:.6f}".format(profit / 10**18), profit)
+  print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+  print("Variance", 10**17)
+  print("-----")
+  print("Est Gas Max: {:,}".format(1.10*sum([x.gas_used for x in txs])))
+
+  print("Schedule the following transactions on Gnosis Safe")
+  for idx, item in enumerate(txs):
+    print("Transaction ", idx)
+    print("To: ", item.receiver)
+    print("Data (Hex encoded): ", item.input, "\n")
