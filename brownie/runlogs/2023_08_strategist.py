a627b408a2b455d04acf5c3598b821169405f283
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-08-03 17:47:11 +0400

    Add buyback runlogs

--- brownie/runlogs/2023_08_strategist.py
+++ brownie/runlogs/2023_08_strategist.py
@@ -19,3 +19,69 @@ with TemporaryForkForReallocations() as txs:
   print("Profit", "{:.6f}".format(profit / 10**18), profit)
   print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
   print("-----")
+
+# -----------------------------------
+# August 03, 2023 - OGV Buyback
+# -----------------------------------
+from buyback import *
+
+def main():
+  build_buyback_tx(max_dollars=2250, max_slippage=1.5, with_fork=True)
+
+# -----------------------------------
+# August 03, 2023 - CVX Buyback
+# -----------------------------------
+from world import *
+from oneinch import get_1inch_swap_data
+
+txs = []
+def main():
+  with TemporaryFork():
+    slippage = 1
+
+    oeth_balance = oeth.balanceOf(STRATEGIST)
+    ousd_balance = ousd.balanceOf(STRATEGIST)
+
+    # Approve 1-inch to move OETH and OUSD
+    txs.append(
+      oeth.approve(ROUTER_1INCH_V5, oeth_balance, {'from': STRATEGIST})
+    )
+    txs.append(
+      ousd.approve(ROUTER_1INCH_V5, ousd_balance, {'from': STRATEGIST})
+    )
+
+    # Swap OETH for CVX
+    txs.append(
+      get_1inch_swap_data(
+        OETH,
+        CVX,
+        oeth_balance,
+        slippage,
+      )
+    )
+
+    # Swap OUSD for CVX
+    txs.append(
+      get_1inch_swap_data(
+        OUSD,
+        CVX,
+        ousd_balance,
+        slippage,
+      )
+    )
+
+    print("Schedule the following transactions on Gnosis Safe")
+    for idx, item in enumerate(txs):
+      print("Transaction ", idx)
+      print("To: ", item.receiver)
+      print("Data (Hex encoded): ", item.input, "\n")
+
+
+# -----------------------------------
+# August 03, 2023 - OGV Buyback 2
+# -----------------------------------
+from buyback import *
+
+txs = []
+def main():
+  build_buyback_tx(max_slippage=1.5, with_fork=True)
