9041b84ceb0e4137e0b2b771901445d73fc05e48
Nicholas Addison <nick@addisonbrown.com.au>
2023-08-09 20:07:25 +1000

    Fixed check of Harvester on Flux Strategy

--- contracts/test/strategies/flux.fork-test.js
+++ contracts/test/strategies/flux.fork-test.js
@@ -55,7 +55,7 @@ forkOnlyDescribe("Flux strategy", function () {
       expect(await fluxStrategy.vaultAddress()).to.equal(
         addresses.mainnet.VaultProxy
       );
-      expect(await fluxStrategy.harvesterAddress()).to.equal(addresses.zero);
+      expect(await fluxStrategy.harvesterAddress()).to.equal(addresses.mainnet.HarvesterProxy);
     });
   });
   describe("Mint", function () {
