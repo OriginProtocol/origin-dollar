d1aa67df6bba24d74654ba058bf4b2c2b5ce559d
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-08-09 12:41:33 +0400

    Fix Morpho compound

--- contracts/test/strategies/morpho-comp.fork-test.js
+++ contracts/test/strategies/morpho-comp.fork-test.js
@@ -108,25 +108,24 @@ forkOnlyDescribe("ForkTest: Morpho Compound Strategy", function () {
       const vaultSigner = await impersonateAndFundContract(vault.address);
       const amount = "110000";
 
-      const removeFundsFromVault = async () => {
-        await usdc
-          .connect(vaultSigner)
-          .transfer(matt.address, usdc.balanceOf(vault.address));
-        await usdt
-          .connect(vaultSigner)
-          .transfer(matt.address, usdt.balanceOf(vault.address));
-      };
-
-      // remove funds so no residual funds get allocated
-      await removeFundsFromVault();
-
-      await mintTest(fixture, matt, usdc, amount);
-      await mintTest(fixture, matt, usdt, amount);
-
       const usdcUnits = await units(amount, usdc);
       const usdtUnits = await units(amount, usdt);
+
+      await vault.connect(matt).mint(usdt.address, usdtUnits, 0);
+      await vault.connect(matt).mint(usdc.address, usdcUnits, 0);
+
+      await vault.connect(matt).rebase();
+      await vault.connect(matt).allocate();
+
       const vaultUsdtBefore = await usdt.balanceOf(vault.address);
       const vaultUsdcBefore = await usdc.balanceOf(vault.address);
+
+      const stratBalUsdc = await morphoCompoundStrategy.checkBalance(
+        usdc.address
+      );
+      const stratBalUsdt = await morphoCompoundStrategy.checkBalance(
+        usdt.address
+      );
 
       await morphoCompoundStrategy.connect(vaultSigner).withdrawAll();
 
@@ -135,8 +134,11 @@ forkOnlyDescribe("ForkTest: Morpho Compound Strategy", function () {
       const vaultUsdcDiff =
         (await usdc.balanceOf(vault.address)) - vaultUsdcBefore;
 
-      expect(vaultUsdcDiff).to.approxEqualTolerance(usdcUnits, 2);
-      expect(vaultUsdtDiff).to.approxEqualTolerance(usdtUnits, 1);
+      expect(vaultUsdcDiff).to.approxEqualTolerance(stratBalUsdc, 1);
+      expect(vaultUsdtDiff).to.approxEqualTolerance(stratBalUsdt, 1);
+
+      expect(await morphoCompoundStrategy.checkBalance(usdc.address)).to.equal("0");
+      expect(await morphoCompoundStrategy.checkBalance(usdt.address)).to.equal("0");
     });
   });
 
