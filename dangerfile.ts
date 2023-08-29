008064a547640e1ec7a3c70c81ddac86998e16d8
Rafael Ugolini <rafael.ugolini@gmail.com>
2023-08-29 13:57:13 +0200

    fix: check if review is approved

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,4 @@
-import { warn, danger } from "danger";
+import { warn, danger, message } from "danger";
 
 const MINIMUM_REVIEWERS = 2;
 
@@ -10,8 +10,13 @@ const files = [
 const hasContractChanges = files.some((file) => file.endsWith(".sol"));
 
 if (hasContractChanges) {
-  const reviewers = danger.github.reviews.length;
+  const reviewers = danger.github.reviews.filter(
+    (review) => review.state === "APPROVED"
+  ).length;
+
   if (reviewers < MINIMUM_REVIEWERS) {
     warn(`:eyes: This PR needs at least ${MINIMUM_REVIEWERS} reviewers`);
+  } else {
+    message(`:tada: This PR has ${reviewers} approved reviews`);
   }
 }
