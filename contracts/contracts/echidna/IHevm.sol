0690946823d28c840e9d237ba07d2e509d387cce
Rappie <jasper@rappie.com>
2023-09-13 01:37:19 +0200

    Add new Echidna fuzzing campaign (#1787)
    * Remove `readline.emitKeypressEvents` to fix problems with `npx hardhat console`
    
    * Add Echidna
    
    * Remove old echidna properties
    
    * Update Node script
    
    * Minor updates to Echidna
    
    ---------
    
    Co-authored-by: Daniel Von Fange <daniel@leancoder.com>

---
+++ contracts/contracts/echidna/IHevm.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// https://github.com/ethereum/hevm/blob/main/doc/src/controlling-the-unit-testing-environment.md#cheat-codes
+
+interface IHevm {
+    function warp(uint256 x) external;
+
+    function roll(uint256 x) external;
+
+    function store(
+        address c,
+        bytes32 loc,
+        bytes32 val
+    ) external;
+
+    function load(address c, bytes32 loc) external returns (bytes32 val);
+
+    function sign(uint256 sk, bytes32 digest)
+        external
+        returns (
+            uint8 v,
+            bytes32 r,
+            bytes32 s
+        );
+
+    function addr(uint256 sk) external returns (address addr);
+
+    function ffi(string[] calldata) external returns (bytes memory);
+
+    function prank(address sender) external;
+}
