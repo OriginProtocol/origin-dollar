b6095351841c9e3793d2a56d4a776caff13b16ae
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-09-21 22:03:51 +0530

    Add September Runlogs (#1816)

---
+++ brownie/runlogs/2023_09_strategist.py
@@ -0,0 +1,24 @@
+# -------------------------------------
+# Sep 13, 2023 - OGV Buyback
+# ------------------------------------
+from buyback import *
+
+def main():
+  build_buyback_tx(max_dollars=14000, max_slippage=9)
+
+# -------------------------------------
+# Sep 13, 2023 - Buy CVX
+# ------------------------------------
+from convex import *
+
+def main():
+  build_cvx_buyback_tx(slippage=1)
+  
+# -------------------------------------
+# Sep 13, 2023 - Buy CVX
+# ------------------------------------
+from convex import *
+
+def main():
+  lock_cvx()
+
