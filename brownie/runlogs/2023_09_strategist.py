e0f3cfbe3ce34bcd90a4629b239690931f141861
Domen Grabec <grabec@gmail.com>
2023-09-22 22:24:27 +0200

    add allocation script

--- brownie/runlogs/2023_09_strategist.py
+++ brownie/runlogs/2023_09_strategist.py
@@ -22,3 +22,31 @@ from convex import *
 def main():
   lock_cvx()
 
+# -------------------------------------
+# Sep 22, 2023 - Allocation
+# ------------------------------------
+
+from world import *
+
+with TemporaryForkForReallocations() as txs:
+  # Before
+  txs.append(vault_oeth_core.rebase({'from':STRATEGIST}))
+  txs.append(oeth_vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+  # Strategist
+  txs.append(vault_oeth_admin.withdrawFromStrategy(OETH_CONVEX_OETH_ETH_STRAT, [weth], [1_432 * 10**18], {'from': STRATEGIST}))
+  txs.append(vault_oeth_admin.depositToStrategy(FRAX_ETH_STRATEGY, [weth], [1_664*10**18], {'from': STRATEGIST}))
+
+
+  #After
+  vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+  supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+  profit = vault_change - supply_change
+  txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (1000 * 10**18), {'from': STRATEGIST}))
+  print("-----")
+  print("Profit", "{:.6f}".format(profit / 10**18), profit)
+  print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+  print("-----")
+
+  
+
