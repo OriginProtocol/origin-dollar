f854a38f89ecb756c492939f1c8d6f380f5b988f
Domen Grabec <grabec@gmail.com>
2023-09-26 21:11:05 +0200

    create a deploy file

---
+++ contracts/deploy/076_balancer_rETH_WETH_2.js
@@ -0,0 +1,65 @@
+const { deploymentWithGovernanceProposal } = require("../utils/deploy");
+const addresses = require("../utils/addresses");
+const { balancer_rETH_WETH_PID } = require("../utils/constants");
+
+const platformAddress = addresses.mainnet.rETH_WETH_BPT;
+
+module.exports = deploymentWithGovernanceProposal(
+  {
+    deployName: "076_balancer_rETH_WETH_2",
+    forceDeploy: false,
+    //forceSkip: true,
+    deployerIsProposer: false,
+    //proposalId: ,
+  },
+  async ({ deployWithConfirmation, ethers, getTxOpts, withConfirmation }) => {
+    const { deployerAddr } = await getNamedAccounts();
+    const sDeployer = await ethers.provider.getSigner(deployerAddr);
+
+    // Current contracts
+    const cOETHVaultProxy = await ethers.getContract("OETHVaultProxy");
+
+    // Deployer Actions
+    // ----------------
+    const cOETHBalancerMetaPoolStrategyProxy = await ethers.getContract(
+      "OETHBalancerMetaPoolrEthStrategyProxy"
+    );
+
+    // 1. Deploy new Balancer strategy implementation
+    const dOETHBalancerMetaPoolStrategyImpl = await deployWithConfirmation(
+      "BalancerMetaPoolStrategy",
+      [
+        [platformAddress, cOETHVaultProxy.address],
+        [
+          addresses.mainnet.rETH,
+          addresses.mainnet.stETH,
+          addresses.mainnet.wstETH,
+          addresses.mainnet.frxETH,
+          addresses.mainnet.sfrxETH,
+          addresses.mainnet.balancerVault, // Address of the Balancer vault
+          balancer_rETH_WETH_PID, // Pool ID of the Balancer pool
+        ],
+        addresses.mainnet.rETH_WETH_AuraRewards, // Address of the Aura rewards contract
+      ]
+    );
+
+    console.log(
+      "Balancer strategy address:",
+      cOETHBalancerMetaPoolStrategyProxy.address
+    );
+
+    // Governance Actions
+    // ----------------
+    return {
+      name: "Deploy new Balancer MetaPool strategy",
+      actions: [
+        // 1. Upgrade strategy implementation
+        {
+          contract: cOETHBalancerMetaPoolStrategyProxy,
+          signature: "upgradeTo(address)",
+          args: [dOETHBalancerMetaPoolStrategyImpl.address],
+        },
+      ],
+    };
+  }
+);
