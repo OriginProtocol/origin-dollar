352a3158235f6fc1a70a77c2f25232c0fd64af4f
Domen Grabec <grabec@gmail.com>
2023-09-29 11:23:16 +0200

    slither

--- contracts/contracts/strategies/balancer/BaseBalancerStrategy.sol
+++ contracts/contracts/strategies/balancer/BaseBalancerStrategy.sol
@@ -503,7 +503,9 @@ abstract contract BaseBalancerStrategy is InitializableAbstractStrategy {
         IERC20 pToken = IERC20(platformAddress);
 
         // Balancer vault for BPT token (required for removing liquidity)
+        // slither-disable-next-line unused-return
         pToken.approve(address(balancerVault), 0);
+        // slither-disable-next-line unused-return
         pToken.approve(address(balancerVault), type(uint256).max);
     }
 
