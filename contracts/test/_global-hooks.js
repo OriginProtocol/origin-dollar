9622df8a6c6c3604cadc9e66c749d1c91634cdc3
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-10-17 18:14:25 +0530

    Try weighted split

--- contracts/test/_global-hooks.js
+++ contracts/test/_global-hooks.js
@@ -3,19 +3,65 @@ const mocha = require("mocha");
 const _chunkId = Number(process.env.CHUNK_ID);
 const _maxChunks = Number(process.env.MAX_CHUNKS);
 
-let _testCaseCounter = 0;
-let _batchCounter = 0;
-let _batchSize = 5; // Group every 5 case into same batch
+const currentBatchTestCases = [];
+
 mocha.beforeEach(function () {
   if (!_maxChunks) {
     // Not parallel tests
     return;
   }
 
-  _testCaseCounter++;
-  _batchCounter = Math.ceil(_testCaseCounter / _batchSize);
+  let root = this.runnable().parent
+  while (root.parent) {
+    root = root.parent
+  }
 
-  if (_batchCounter % _maxChunks != _chunkId) {
-    this.currentTest.skip();
+  if (!currentBatchTestCases.includes(
+    `${root.file}||||||${root.fullTitle()}`
+  )) {
+    this.currentTest?.skip()
   }
 });
+
+mocha.before(function () {
+  if (!_maxChunks) {
+    // Not parallel tests
+    return;
+  }
+
+  let root = this.runnable().parent
+  while (root.parent) {
+    root = root.parent
+  }
+
+  const findTestCaseCount = (suite) => {
+    if (!suite.suites?.length) {
+      return suite.tests?.length || 0
+    }
+
+    return suite.suites.reduce((count, currSuite) => {
+      return count + findTestCaseCount(currSuite)
+    }, suite.tests?.length || 0)
+  }
+
+  // const batches = new Array(_maxChunks)
+  const batchWeights = new Array(_maxChunks).fill(0)
+
+  for (let suite of root.suites) {
+    const testCaseCount = findTestCaseCount(suite)
+
+    const targetBatchIndex = Object.keys(batchWeights).slice(1).reduce((lastIndex, currIndex) => {
+      return (batchWeights[lastIndex] == 0 || batchWeights[lastIndex] < batchWeights[currIndex]) ? lastIndex : currIndex
+    }, 0)
+
+    // if (!batches[targetBatchIndex]) {
+    //   batches[targetBatchIndex] = []
+    // }
+    // batches[targetBatchIndex].push(suite.fullTitle())
+    batchWeights[targetBatchIndex] += testCaseCount
+
+    if (targetBatchIndex == _chunkId) {
+      currentBatchTestCases.push(`${suite.file}||||||${suite.fullTitle()}`)
+    }
+  }
+})
