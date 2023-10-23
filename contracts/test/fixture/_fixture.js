933009b6c0fa66221b9cf95c0d3c6d10309704d7
Domen Grabec <grabec@gmail.com>
2023-10-23 16:45:05 +0200

    add comment

--- contracts/test/fixture/_fixture.js
+++ contracts/test/fixture/_fixture.js
@@ -1069,6 +1069,8 @@ async function balancerFrxETHwstETHeETHFixture(
     60 // use 60% of the account balances
   );
 
+  // set the price to an acceptable level so that fork tests don't fail
+  // even when it de-pegs. 
   await setFraxOraclePrice(parseUnits("0.999", 18));
   return fixture;
 }
