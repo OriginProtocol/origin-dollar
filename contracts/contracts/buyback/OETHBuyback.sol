7dc4740bb40a67386ae9209e3bbeecc85567fef4
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-10-23 18:30:56 +0530

    Slither and linter

--- contracts/contracts/buyback/OETHBuyback.sol
+++ contracts/contracts/buyback/OETHBuyback.sol
@@ -6,10 +6,12 @@ import { BaseBuyback } from "./BaseBuyback.sol";
 contract OETHBuyback is BaseBuyback {
     // abi.encodePacked(oeth, uint24(500), weth9, uint24(3000), ogv);
     bytes public constant ogvPath =
+    // solhint-disable-next-line max-line-length
         hex"856c4efb76c1d1ae02e20ceb03a2a6a08b0b8dc30001f4c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000bb89c354503c38481a7a7a51629142963f98ecc12d0";
 
     // abi.encodePacked(oeth, uint24(500), weth9, uint24(10000), cvx);
     bytes public constant cvxPath =
+    // solhint-disable-next-line max-line-length
         hex"856c4efb76c1d1ae02e20ceb03a2a6a08b0b8dc30001f4c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20027104e3fbd56cd56c3e72c1403e103b45db9da5b9d2b";
 
     constructor(
