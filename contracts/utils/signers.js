b9047822196935bae5cf967e2b4bb21ae6fcb631
Nicholas Addison <nick@addisonbrown.com.au>
2023-10-27 16:12:40 +1100

    Added signer support for Defender Relayer

--- contracts/utils/signers.js
+++ contracts/utils/signers.js
@@ -1,4 +1,6 @@
 const { Wallet } = require("ethers").utils;
+const { Defender } = require("@openzeppelin/defender-sdk");
+
 const { ethereumAddress, privateKey } = require("./regex");
 const { hardhatSetBalance } = require("../test/_fund");
 const hhHelpers = require("@nomicfoundation/hardhat-network-helpers");
@@ -44,12 +46,34 @@ async function getSigner(address) {
     return await impersonateAndFund(address);
   }
 
+  // If using Defender Relayer
+  if (process.env.DEFENDER_API_KEY && process.env.DEFENDER_API_SECRET) {
+    return await getDefenderSigner();
+  }
+
   const signers = await hre.ethers.getSigners();
   const signer = signers[0];
   log(`Using signer ${await signer.getAddress()}`);
 
   return signer;
 }
+
+const getDefenderSigner = async () => {
+  const speed = process.env.SPEED || "fast";
+  if (!["safeLow", "average", "fast", "fastest"].includes(speed)) {
+    console.error(
+      `Defender Relay Speed param must be either 'safeLow', 'average', 'fast' or 'fastest'. Not "${speed}"`
+    );
+    process.exit(2);
+  }
+  const credentials = {
+    relayerApiKey: process.env.DEFENDER_API_KEY,
+    relayerApiSecret: process.env.DEFENDER_API_SECRET,
+  };
+  const client = new Defender(credentials);
+  const provider = client.relaySigner.getProvider();
+  return client.relaySigner.getSigner(provider, { speed });
+};
 
 /**
  * Impersonate an account when connecting to a forked node.
