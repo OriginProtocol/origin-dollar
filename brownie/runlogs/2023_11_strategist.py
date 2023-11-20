cc9ab4bc1daf33aa4f519ea06c32160053d82e2a
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-11-20 11:52:32 +0530

    Update runlog (#1937)

--- brownie/runlogs/2023_11_strategist.py
+++ brownie/runlogs/2023_11_strategist.py
@@ -42,3 +42,38 @@ def main():
     )
 
     print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Nov 17, 2023 - OUSD Reallocation
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    txs.append(vault_core.rebase({'from':STRATEGIST}))
+    txs.append(vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+    txs.append(
+      vault_admin.withdrawFromStrategy(
+        MORPHO_COMP_STRAT, 
+        [USDC], 
+        [morpho_comp_strat.checkBalance(USDC)], 
+        {'from': STRATEGIST}
+      )
+    )
+
+    txs.append(
+      vault_admin.depositToStrategy(
+        MORPHO_AAVE_STRAT, 
+        [USDC], 
+        [usdc.balanceOf(VAULT_PROXY_ADDRESS)], 
+        {'from': STRATEGIST}
+      )
+    )
+
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = ousd.totalSupply() - vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(vault_value_checker.checkDelta(profit, (500 * 10**18), vault_change, (500 * 10**18), {'from': STRATEGIST}))
+
+    txs.append(vault_core.allocate({'from': STRATEGIST}))
