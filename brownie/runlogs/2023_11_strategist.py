f254fc29f0c5f02317315a6972623f396d088858
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-11-22 20:32:35 +0530

    Update runlog

--- brownie/runlogs/2023_11_strategist.py
+++ brownie/runlogs/2023_11_strategist.py
@@ -177,3 +177,33 @@ def main():
     )
 
     print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Nov 22, 2023 - Withdraw from Morpho
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+    txs.append(
+      oeth_vault_admin.withdrawFromStrategy(
+        OETH_MORPHO_AAVE_STRAT, 
+        [WETH], 
+        [60 * 10**18], 
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (0.1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
