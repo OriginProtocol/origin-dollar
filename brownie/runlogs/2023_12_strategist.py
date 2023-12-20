6990d8e17b4101ae9d1238991b0511890cac9e09
Domen Grabec <grabec@gmail.com>
2023-12-20 15:41:55 +0100

    update the runlog

--- brownie/runlogs/2023_12_strategist.py
+++ brownie/runlogs/2023_12_strategist.py
@@ -108,7 +108,7 @@ def main():
 # Dec 20, 2023 - OETH Reallocation
 # -------------------------------------
 from world import *
-from collateralSwap import *
+#from collateralSwap import *
 
 def main():
   with TemporaryForkForReallocations() as txs:
@@ -116,47 +116,25 @@ def main():
     txs.append(vault_oeth_core.rebase(std))
     txs.append(oeth_vault_value_checker.takeSnapshot(std))
 
-    
-    # Withdraw 1,217.146 WETH and 1,064.393 rETH
-    txs.append(
-      vault_oeth_admin.withdrawFromStrategy(
-        BALANCER_RETH_STRATEGY, 
-        [reth, weth], 
-        [1_064.393 * 10**18, 1_217.146 * 10**18], 
-        {'from': STRATEGIST}
-      )
-    )
-
-    #print("Balance of RETH: ", reth.balanceOf(oeth_vault_core) / 1e18)
-
-    # Swap 1_064.393 rETH for 1163.270 WETH with 0.6% tolerance
-    _, swap_data = build_swap_tx(RETH, WETH, 1064393 * 10**15, 0.6, False)
-    decoded_input = oeth_vault_admin.swapCollateral.decode_input(swap_data)
-    txs.append(
-      oeth_vault_admin.swapCollateral(*decoded_input, {'from':STRATEGIST})
-    )
-
-    print("Balance of AMO before: ", oeth_meta_strat.checkBalance(WETH) / 1e18)
-    print("oeth supply before", oeth.totalSupply())
-    # Deposit 2,380.416 ETH to Convex AMO
+    # Deposit 2,026..416 ETH to Convex AMO
     txs.append(
       vault_oeth_admin.depositToStrategy(
         OETH_CONVEX_OETH_ETH_STRAT, 
         [WETH], 
-        [2_380.416 * 10**18], 
+        [2_026.30298717216 * 10**18], 
         {'from': STRATEGIST}
       )
     )
-    print(txs[-1].call_trace(True))
-    print("oeth supply after", oeth.totalSupply())
-    print("Balance of AMO after: ", oeth_meta_strat.checkBalance(WETH) / 1e18)
+
+    # below line works on hardhat's default node (brownie console --network mainnet-fork)
+    # but not on our default node running in a separate terminal and attaching
+    # to it: (brownie console --network hardhat)
+    #print(txs[-1].call_trace(True))
 
     # After
     vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
     supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
     profit = vault_change - supply_change
-    print("vault_change", vault_change)
-    print("supply_change", supply_change)
     print("profit", profit)
     print("snapshot totalSupply", oeth_vault_value_checker.snapshots(STRATEGIST)[1])
     txs.append(oeth_vault_value_checker.checkDelta(profit, (0.2 * 10**18), vault_change, (0.2 * 10**18), std))
