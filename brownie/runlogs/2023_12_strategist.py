3591e4235f44d4db3d270923b741909b43e7f894
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-12-20 18:19:21 +0400

    Add runlog (#1962)

--- brownie/runlogs/2023_12_strategist.py
+++ brownie/runlogs/2023_12_strategist.py
@@ -62,3 +62,44 @@ def main():
     print("Profit", "{:.6f}".format(profit / 10**18), profit)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
     print("-----")
+
+# -------------------------------------
+# Dec 20, 2023 - OETH Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      build_buyback_tx(
+        OETH,
+        oeth.balanceOf(OETH_BUYBACK),
+        max_ogv_slippage=1.25,
+        max_cvx_slippage=3
+      )
+    )
+
+    print(to_gnosis_json(txs))
+
+
+# -------------------------------------
+# Dec 20, 2023 - OUSD Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      build_buyback_tx(
+        OUSD,
+        ousd.balanceOf(OUSD_BUYBACK),
+        max_ogv_slippage=1.25,
+        max_cvx_slippage=3
+      )
+    )
+
+    print(to_gnosis_json(txs))
