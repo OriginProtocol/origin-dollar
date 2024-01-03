b968c08ab2f5f7960ad65c6e49af6b4c9160965c
Domen Grabec <grabec@gmail.com>
2024-01-03 17:56:08 +0100

    split up runs

--- brownie/runlogs/2024_01_strategist.py
+++ brownie/runlogs/2024_01_strategist.py
@@ -28,16 +28,28 @@ def main():
 
   with TemporaryFork():
     txs.append(
+      cvx_locker.processExpiredLocks(True, std)
+    )
+
+    print(to_gnosis_json(txs))
+
+
+# -------------------------------------
+# Jan 04, 2023 - OUSD Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
       build_buyback_tx(
         OUSD,
         ousd.balanceOf(OUSD_BUYBACK),
         max_ogv_slippage=1.25,
         max_cvx_slippage=3
       )
-    )
-
-    txs.append(
-      cvx_locker.processExpiredLocks(True, std)
     )
 
     print(to_gnosis_json(txs))
