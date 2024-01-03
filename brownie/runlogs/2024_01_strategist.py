ea7b2561fe9bf465dbbb095e2b4233a4afe1d30f
Domen Grabec <grabec@gmail.com>
2024-01-03 18:06:59 +0100

    January strategist runs (#1969)
    * Add script
    
    * split up runs
    
    ---------
    
    Co-authored-by: Shahul Hameed <10547529+shahthepro@users.noreply.github.com>

---
+++ brownie/runlogs/2024_01_strategist.py
@@ -0,0 +1,55 @@
+# -------------------------------------
+# Jan 03, 2023 - OETH Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      build_buyback_tx(
+        OETH,
+        oeth.balanceOf(OETH_BUYBACK),
+        max_ogv_slippage=1.25,
+        max_cvx_slippage=2.5
+      )
+    )
+
+    print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Jan 03, 2023 - OUSD Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      cvx_locker.processExpiredLocks(True, std)
+    )
+
+    print(to_gnosis_json(txs))
+
+
+# -------------------------------------
+# Jan 04, 2023 - OUSD Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      build_buyback_tx(
+        OUSD,
+        ousd.balanceOf(OUSD_BUYBACK),
+        max_ogv_slippage=1.25,
+        max_cvx_slippage=3
+      )
+    )
+
+    print(to_gnosis_json(txs))
