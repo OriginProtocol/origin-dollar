c59e42bd5dfbff28aa69922d7ab491ac27515e3e
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-01-03 20:33:34 +0400

    Add script

---
+++ brownie/runlogs/2024_01_strategist.py
@@ -0,0 +1,43 @@
+# -------------------------------------
+# Jan 03, 2023 - OETH Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      build_buyback_tx(
+        OETH,
+        oeth.balanceOf(OETH_BUYBACK),
+        max_ogv_slippage=1.25,
+        max_cvx_slippage=2.5
+      )
+    )
+
+    print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Jan 03, 2023 - OUSD Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      build_buyback_tx(
+        OUSD,
+        ousd.balanceOf(OUSD_BUYBACK),
+        max_ogv_slippage=1.25,
+        max_cvx_slippage=3
+      )
+    )
+
+    txs.append(
+      cvx_locker.processExpiredLocks(True, std)
+    )
+
+    print(to_gnosis_json(txs))
