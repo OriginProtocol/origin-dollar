a0564158870b25a94e60ec7b9ecd87000e6aa1c7
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-01-16 18:16:06 +0400

    Fix balanced metapool fixture

--- contracts/test/_metastrategies-fixtures.js
+++ contracts/test/_metastrategies-fixtures.js
@@ -39,8 +39,9 @@ async function withDefaultOUSDMetapoolStrategiesSet() {
   );
 
   // Also, mint some OUSD so that there's some in the pool
-  const amount = "20000";
+  const amount = "500000";
   for (const asset of [usdt, usdc, dai]) {
+    await setERC20TokenBalance(daniel.address, asset, amount);
     await vault
       .connect(daniel)
       .mint(asset.address, await units(amount, asset), 0);
@@ -187,35 +188,6 @@ async function tiltTo3CRV_Metapool_automatic(fixture) {
   }
 }
 
-/* Tilt towards OUSD by checking liquidity
- */
-async function tiltToOUSD_Metapool_automatic(fixture) {
-  const { metapool, threePoolToken } = fixture;
-
-  const metapoolSigner = await impersonateAndFund(metapool.address);
-  await resetAllowance(threePoolToken, metapoolSigner, metapool.address);
-
-  // 90% of main coin pool liquidity
-  const shareOfThreePoolCoinBalance = (
-    await threePoolToken.balanceOf(metapool.address)
-  )
-    .mul(ousdUnits("0.9"))
-    .div(ousdUnits("1"));
-
-  let acc = ethers.BigNumber.from("0");
-  /* self deploy 90% of threepool coin liquidity until pool has at least five times
-   * the 3crvLP liquidity comparing to main coin.
-   */
-  while (acc.lt((await metapool.balances(0)).mul(5))) {
-    // Tilt to main token
-    await metapool.connect(metapoolSigner)[
-      // eslint-disable-next-line
-      "add_liquidity(uint256[2],uint256)"
-    ]([shareOfThreePoolCoinBalance, 0], 0);
-    acc = acc.add(shareOfThreePoolCoinBalance);
-  }
-}
-
 /* Just triple the main token liquidity in a flaky manner where the pool
  * re-deploys its own liquidity
  */
@@ -269,7 +241,7 @@ async function withOUSDTitledMetapool() {
   const fixture = await withDefaultOUSDMetapoolStrategiesSet();
 
   fixture.metapool = fixture.ousdMetaPool;
-  await tiltToOUSD_Metapool_automatic(fixture);
+  await tiltToOUSD_OUSDMetapool(fixture);
 
   return fixture;
 }
