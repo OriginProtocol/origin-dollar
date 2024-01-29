3d51434fb828a462abf7a9e28d598ba210a3f726
Domen Grabec <grabec@gmail.com>
2024-01-30 00:39:29 +0100

    add burn OETH strategist runlog (#1983)

--- brownie/runlogs/2024_01_strategist.py
+++ brownie/runlogs/2024_01_strategist.py
@@ -181,3 +181,34 @@ with TemporaryForkForReallocations() as txs:
   print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
   print("-----")
 
+
+# -------------------------------------
+# Jan 29, 2023 - OETH Burn OETH
+# -------------------------------------
+from world import *
+
+with TemporaryForkForReallocations() as txs:
+  # Before
+  txs.append(vault_oeth_core.rebase(std))
+  txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+  metapool_virtual_price = 1001209756036959115
+
+  # burn 2957.922 OETH from the LP position
+  txs.append(
+    oeth_meta_strat.removeAndBurnOTokens(
+      2957.922 * 10**18 * 10**18 / metapool_virtual_price, 
+      std
+    )
+  )
+
+  # After
+  vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+  supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+  profit = vault_change - supply_change
+  txs.append(oeth_vault_value_checker.checkDelta(profit, (0.25 * 10**18), vault_change, (0.25 * 10**18), std))
+  print("-----")
+  print("Profit", "{:.6f}".format(profit / 10**18), profit)
+  print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+  print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+  print("-----")
