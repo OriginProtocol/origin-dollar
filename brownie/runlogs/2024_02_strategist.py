4a6a940efd12a5c1e18fe1afca7d8442376b5389
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-02-27 19:35:19 +0530

    Update Feb Runlog (#1992)
    * Update runlog
    
    * Update Feb runlog

--- brownie/runlogs/2024_02_strategist.py
+++ brownie/runlogs/2024_02_strategist.py
@@ -114,3 +114,43 @@ def main():
     )
 
     print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Feb 27, 2024 - OETH Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      build_buyback_tx(
+        OETH,
+        oeth.balanceOf(OETH_BUYBACK),
+        max_ogv_slippage=3,
+        max_cvx_slippage=6
+      )
+    )
+
+    print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Feb 27, 2024 - OUSD Buyback
+# -------------------------------------
+from buyback import *
+
+def main():
+  txs = []
+
+  with TemporaryFork():
+    txs.append(
+      build_buyback_tx(
+        OUSD,
+        ousd.balanceOf(OUSD_BUYBACK),
+        max_ogv_slippage=5,
+        max_cvx_slippage=5
+      )
+    )
+
+    print(to_gnosis_json(txs))
