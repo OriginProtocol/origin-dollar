faebaaead1df77292bf3b28da499eba32a11f169
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-03-14 17:54:27 +0530

    Update runlog

--- brownie/runlogs/2024_03_strategist.py
+++ brownie/runlogs/2024_03_strategist.py
@@ -31,3 +31,35 @@ def main():
     print("-----")
     print("Profit", "{:.6f}".format(profit / 10**18), profit)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
+# ---------------------------------
+# Mar 14, 2024 - Move out from Flux
+# ---------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_core.rebase({'from':STRATEGIST}))
+    txs.append(vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+    # Withdraw all from Flux (including dust)
+    txs.append(
+      vault_admin.withdrawAllFromStrategy(
+        FLUX_STRAT,
+        {'from': STRATEGIST}
+      )
+    )
+
+    # Put everything in Morpho Aave (default strategy for USDC and USDT)
+    txs.append(vault_core.allocate({'from': STRATEGIST}))
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = ousd.totalSupply() - vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (500 * 10**18), vault_change, (500 * 10**18), {'from': STRATEGIST}))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
