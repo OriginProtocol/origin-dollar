81f6eb9d3e137097096d9eac7d1a68d81a35a4a2
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-03-27 11:05:46 +0530

    Add test redeem runlog (#2008)

--- brownie/runlogs/2024_03_strategist.py
+++ brownie/runlogs/2024_03_strategist.py
@@ -227,3 +227,45 @@ def main():
 
     print("Schedule the following transactions on Gnosis Safe")
     print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Mar 26, 2024 - frxETH Test Redemption
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+    # Withdraw frxETH from FrxETHStrategy
+    txs.append(
+      vault_oeth_admin.withdrawFromStrategy(
+        OETH_FRAX_ETH_STRAT, 
+        [FRXETH], 
+        [10 * 10**18],
+        std
+      )
+    )
+
+    # Deposit to redemption strategy
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_FRAX_ETH_REDEEM_STRAT, 
+        [FRXETH], 
+        [10 * 10**18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
