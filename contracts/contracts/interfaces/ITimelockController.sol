10199e455edac5f6a4379c718b5c2b14bc2f0c8d
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-04-17 16:46:42 +0400

    Add proposal to add Guardian to Timelock

---
+++ contracts/contracts/interfaces/ITimelockController.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+interface ITimelockController {
+    function grantRole(bytes32 role, address account) external;
+
+    function revokeRole(bytes32 role, address account) external;
+
+    function renounceRole(bytes32 role, address account) external;
+}
---
+++ contracts/deploy/092_add_multisig_to_timelock.js
@@ -0,0 +1,35 @@
+const addresses = require("../utils/addresses");
+const { deploymentWithGovernanceProposal } = require("../utils/deploy");
+
+module.exports = deploymentWithGovernanceProposal(
+  {
+    deployName: "092_add_multisig_to_timelock",
+    forceDeploy: false,
+    // forceSkip: true,
+    // onlyOnFork: true, // this is only executed in forked environment
+    reduceQueueTime: true, // just to solve the issue of later active proposals failing
+    proposalId: "",
+  },
+  async ({ ethers }) => {
+    const TIMELOCK_ADMIN_ROLE =
+      "0x5f58e3a2316349923ce3780f8d587db2d72378aed66a8261c916544fa6846ca5"; // keccak256"TIMELOCK_ADMIN_ROLE");
+    const cTimelock = await ethers.getContractAt(
+      "ITimelockController",
+      addresses.mainnet.Timelock
+    );
+
+    return {
+      name: "Add Guardian Multisig to Timelock\n\
+    \n\
+    Part of the OGN/OGV merger. This proposal adds the Origin's 5 of 8 Multisig to the Timelock to make things easier during migration/merger. This permission will be revoked once the merger is complete. \
+    ",
+      actions: [
+        {
+          contract: cTimelock,
+          signature: "grantRole(bytes32,address)",
+          args: [TIMELOCK_ADMIN_ROLE, addresses.mainnet.Guardian],
+        },
+      ],
+    };
+  }
+);
