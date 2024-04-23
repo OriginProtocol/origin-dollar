2ad53ada4ab9eb49bd1a1cfbc476111fd2c6729d
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-04-23 19:23:19 +0530

    Update runlog

--- brownie/runlogs/2024_04_strategist.py
+++ brownie/runlogs/2024_04_strategist.py
@@ -224,3 +224,52 @@ def main():
     print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
     print("-----")
+
+# -------------------------------------
+# Apr 23, 2024 - FraxETH Redeem
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+    outstanding_redeems = frxeth_redeem_strat.outstandingRedeems();
+    
+    print("Outstanding redeems before:", outstanding_redeems)
+
+    txs.append(
+      frxeth_redeem_strat.redeemTickets(
+        [
+          350, 351, 352, 353, 354, 355, 356, 357, 358,
+          359, 360, 361, 362, 363, 364, 365, 366
+        ],
+        outstanding_redeems,
+        std
+      )
+    )
+
+    print("Outstanding redeems after:", frxeth_redeem_strat.outstandingRedeems())
+
+    # Deposit WETH to Morpho Aave
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_MORPHO_AAVE_STRAT, 
+        [WETH], 
+        [outstanding_redeems],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
