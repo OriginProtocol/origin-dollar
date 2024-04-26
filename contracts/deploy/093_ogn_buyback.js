ff320a2867aa5e1124487041ba075dd279367099
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-04-26 15:16:28 +0530

    OGN Buyback

---
+++ contracts/deploy/093_ogn_buyback.js
@@ -0,0 +1,67 @@
+const addresses = require("../utils/addresses");
+const {
+  deploymentWithGovernanceProposal,
+  deployWithConfirmation,
+} = require("../utils/deploy");
+
+module.exports = deploymentWithGovernanceProposal(
+  {
+    deployName: "093_ogn_buyback",
+    // forceSkip: true,
+    // onlyOnFork: true, // this is only executed in forked environment
+    reduceQueueTime: true, // just to solve the issue of later active proposals failing
+    proposalId: "",
+  },
+  async ({ ethers }) => {
+    const cOETHBuybackProxy = await ethers.getContract("OETHBuybackProxy");
+    const cOUSDBuybackProxy = await ethers.getContract("BuybackProxy");
+
+    const cSwapper = await ethers.getContract("Swapper1InchV5");
+
+    // Deploy new OETHBuyback implementation
+    const dOETHBuybackImpl = await deployWithConfirmation(
+      "OETHBuyback",
+      [
+        addresses.mainnet.OETHProxy,
+        addresses.mainnet.OGN,
+        addresses.mainnet.CVX,
+        addresses.mainnet.CVXLocker,
+      ],
+      undefined,
+      true
+    );
+
+    // Deploy new OUSDBuyback implementation
+    const dOUSDBuybackImpl = await deployWithConfirmation(
+      "OUSDBuyback",
+      [
+        addresses.mainnet.OUSDProxy,
+        addresses.mainnet.OGN,
+        addresses.mainnet.CVX,
+        addresses.mainnet.CVXLocker,
+      ],
+      undefined,
+      true
+    );
+
+    await cSwapper.approveAssets([addresses.mainnet.OGN]);
+
+    return {
+      name: "Upgrade contracts for OGN Buyback",
+      actions: [
+        // 1. Upgrade OUSD Buyback to new implementation
+        {
+          contract: cOUSDBuybackProxy,
+          signature: "upgradeTo(address)",
+          args: [dOUSDBuybackImpl.address],
+        },
+        // 2. Upgrade OETH Buyback to new implementation
+        {
+          contract: cOETHBuybackProxy,
+          signature: "upgradeTo(address)",
+          args: [dOETHBuybackImpl.address],
+        },
+      ],
+    };
+  }
+);
