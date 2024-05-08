b898f7a56bc006b7477e705c0adf24021a90c882
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-05-08 18:15:51 +0530

    Add one more test

--- brownie/scripts/test_multisig_as_governor.py
+++ brownie/scripts/test_multisig_as_governor.py
@@ -73,5 +73,50 @@ def main():
     
     print("All Good!")
 
+  print("-------------------------------------------")
+  with TemporaryFork():
+    print("\n\nMaking sure that it's possible to grant role...")
+    calldata = timelock_contract.grantRole.encode_input(
+      # keccak256("TIMELOCK_ADMIN_ROLE")
+      "0xb09aa5aeb3702cfd50b6b62bc4532604938f21248a27a1d5ca736082b6819cc1",
+      "0x0000000000000000000000000000000000000011"
+    )
+
+    print("\n\nCreating another test proposal on Timelock...")
+    tx = timelock_contract.schedule(
+      TIMELOCK, # Target
+      0, # Value
+      calldata,
+      "",
+      "",
+      24 * 60 * 60, # delay
+      {'from': GOV_MULTISIG}
+    )
+    tx.info()
+
+    print("Waiting until it's ready for execution...")
+    # Fast forward to execute
+    chain.sleep(24 * 60 * 60 + 10)
+
+    print("Executing...")
+    # Test execution
+    tx = timelock_contract.execute(
+      TIMELOCK, # Target
+      0, # Value
+      calldata,
+      "",
+      "",
+      {'from': GOV_MULTISIG}
+    )
+    tx.info()
+
+    if not timelock_contract.hasRole(
+      "0xb09aa5aeb3702cfd50b6b62bc4532604938f21248a27a1d5ca736082b6819cc1",
+      "0x0000000000000000000000000000000000000011"
+    ):
+      raise Exception("Failed to grant roles")
+    
+    print("All Good!")
+
 
     
--- contracts/deploy/094_add_multisig_to_timelock.js
+++ contracts/deploy/094_add_multisig_to_timelock.js
@@ -11,8 +11,6 @@ module.exports = deploymentWithGovernanceProposal(
     proposalId: "",
   },
   async ({ ethers }) => {
-    const TIMELOCK_ADMIN_ROLE =
-      "0x5f58e3a2316349923ce3780f8d587db2d72378aed66a8261c916544fa6846ca5"; // keccak256("TIMELOCK_ADMIN_ROLE");
     const PROPOSER_ROLE =
       "0xb09aa5aeb3702cfd50b6b62bc4532604938f21248a27a1d5ca736082b6819cc1"; // keccak256("PROPOSER_ROLE");
     const EXECUTOR_ROLE =
@@ -30,11 +28,6 @@ module.exports = deploymentWithGovernanceProposal(
     Part of the OGN/OGV merger. This proposal adds the Origin's 5 of 8 Multisig to the Timelock to provide a backup governance during the transition between governance systems during the token merger. This permission will be revoked once the merger is complete. \
     ",
       actions: [
-        {
-          contract: cTimelock,
-          signature: "grantRole(bytes32,address)",
-          args: [TIMELOCK_ADMIN_ROLE, addresses.mainnet.Guardian],
-        },
         {
           contract: cTimelock,
           signature: "grantRole(bytes32,address)",
