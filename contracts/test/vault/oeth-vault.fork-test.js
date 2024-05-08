9d9e09caa083d809050a7c57c7db39a3ea4fad0f
Nicholas Addison <nick@addisonbrown.com.au>
2024-05-08 21:44:42 +1000

    Fixed harvester behaviour tests

--- contracts/test/vault/oeth-vault.fork-test.js
+++ contracts/test/vault/oeth-vault.fork-test.js
@@ -219,6 +219,7 @@ describe("ForkTest: OETH Vault", function () {
   shouldHaveRewardTokensConfigured(() => ({
     vault: fixture.oethVault,
     harvester: fixture.oethHarvester,
+    ignoreTokens: [fixture.weth.address.toLowerCase()],
     expectedConfigs: {
       [fixture.cvx.address]: {
         allowedSlippageBps: 300,
