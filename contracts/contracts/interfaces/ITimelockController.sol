b0f69ce6abf4c61143d711913ecee95c255b6d91
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-05-29 13:09:47 +0530

    Use transition governance when needed

--- contracts/contracts/interfaces/ITimelockController.sol
+++ contracts/contracts/interfaces/ITimelockController.sol
@@ -6,4 +6,34 @@ interface ITimelockController {
     function revokeRole(bytes32 role, address account) external;
 
     function renounceRole(bytes32 role, address account) external;
+
+    function hasRole(bytes32 role, address account)
+        external
+        view
+        returns (bool);
+
+    function executeBatch(
+        address[] calldata targets,
+        uint256[] calldata values,
+        bytes[] calldata payloads,
+        bytes32 predecessor,
+        bytes32 salt
+    ) external payable;
+
+    function scheduleBatch(
+        address[] calldata targets,
+        uint256[] calldata values,
+        bytes[] calldata payloads,
+        bytes32 predecessor,
+        bytes32 salt,
+        uint256 delay
+    ) external;
+
+    function hashOperationBatch(
+        address[] calldata targets,
+        uint256[] calldata values,
+        bytes[] calldata payloads,
+        bytes32 predecessor,
+        bytes32 salt
+    ) external;
 }
