aed1661a5c8bf4551faf56477b2016d481ab2bf5
Nicholas Addison <nick@addisonbrown.com.au>
2024-06-04 08:19:49 +1000

    fixed Holesky fork tests

--- contracts/deploy/deployActions.js
+++ contracts/deploy/deployActions.js
@@ -854,6 +854,7 @@ const upgradeNativeStakingSSVStrategy = async () => {
       assetAddresses.WETH, // wethAddress
       assetAddresses.SSV, // ssvToken
       assetAddresses.SSVNetwork, // ssvNetwork
+      600, // maxValidators
       cFeeAccumulatorProxy.address, // feeAccumulator
       assetAddresses.beaconChainDepositContract, // depositContractMock
     ]
