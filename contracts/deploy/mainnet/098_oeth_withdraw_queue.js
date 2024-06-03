20819190dd019f98e38c39e508e4ce43ca68f42a
Nicholas Addison <nick@addisonbrown.com.au>
2024-06-03 21:14:18 +1000

    bumped the deploy script number

--- contracts/deploy/mainnet/098_oeth_withdraw_queue.js
+++ contracts/deploy/mainnet/098_oeth_withdraw_queue.js
@@ -3,7 +3,7 @@ const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
 
 module.exports = deploymentWithGovernanceProposal(
   {
-    deployName: "097_withdraw_queue",
+    deployName: "098_oeth_withdraw_queue",
     forceDeploy: false,
     //forceSkip: true,
     reduceQueueTime: true,
