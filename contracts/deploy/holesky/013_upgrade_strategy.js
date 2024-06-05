e6af23acdaaf5b3cb783c534e3b2e02ae2fefe33
Nicholas Addison <nick@addisonbrown.com.au>
2024-06-05 15:32:00 +1000

    Added new Holesky deploy script

---
+++ contracts/deploy/holesky/013_upgrade_strategy.js
@@ -0,0 +1,18 @@
+const { upgradeNativeStakingSSVStrategy } = require("../deployActions");
+
+const mainExport = async () => {
+  console.log("Running 013 deployment on Holesky...");
+
+  console.log("Upgrading native staking strategy");
+  await upgradeNativeStakingSSVStrategy();
+
+  console.log("Running 012 deployment done");
+  return true;
+};
+
+mainExport.id = "013_upgrade_strategy";
+mainExport.tags = [];
+mainExport.dependencies = [];
+mainExport.skip = () => false;
+
+module.exports = mainExport;
