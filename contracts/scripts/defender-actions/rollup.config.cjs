2ba9a03caac255dab4e88956107ba5797973691f
Domen Grabec <grabec@gmail.com>
2024-06-21 13:57:40 +0200

    add rollup visualizer

--- contracts/scripts/defender-actions/rollup.config.cjs
+++ contracts/scripts/defender-actions/rollup.config.cjs
@@ -2,12 +2,14 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const json = require("@rollup/plugin-json");
 const builtins = require("builtin-modules");
+const { visualizer } = require("rollup-plugin-visualizer");
 
 const commonConfig = {
   plugins: [
     resolve({ preferBuiltins: true, exportConditions: ["node"] }),
     commonjs(),
     json({ compact: true }),
+    visualizer()
   ],
   // Do not bundle these packages.
   // ethers is required to be bundled even though its an Autotask package.
