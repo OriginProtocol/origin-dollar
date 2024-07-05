15359762c3229ead797a6eb1fac88af04ac47747
clement-ux <clemmoller@gmail.com>
2024-07-05 14:54:24 +0200

    test: add fork test for deposit/mint/withdraw/redeem.

--- contracts/test/token/woeth.mainnet.fork-test.js
+++ contracts/test/token/woeth.mainnet.fork-test.js
@@ -100,4 +100,120 @@ describe("ForkTest: wOETH", function () {
     // Ensure balance is right
     await expect(domen).to.have.approxBalanceOf("10000", oeth);
   });
+
+  describe("Funds in, Funds out", async () => {
+    it("should deposit at the correct ratio", async () => {
+      const { oeth, woeth, domen } = fixture;
+
+      const totalSupply = await woeth.totalSupply();
+      const balanceBefore = await oeth.balanceOf(domen.address);
+
+      // Wrap some OETH
+      const txResponse = await woeth
+        .connect(domen)
+        .deposit(oethUnits("50"), domen.address);
+      const txReceipt = await txResponse.wait();
+      const mintedShares = txReceipt.events[2].args.shares; // 0. transfer oeth, 1. transfer woeth, 2. deposit
+      const assetTransfered = txReceipt.events[2].args.assets; // 0. transfer oeth, 1. transfer woeth, 2. mint
+
+      await expect(assetTransfered).to.be.equal(oethUnits("50"));
+      await expect(
+        await woeth.convertToShares(assetTransfered)
+      ).to.be.approxEqual(mintedShares);
+      await expect(woeth).to.have.a.totalSupply(totalSupply.add(mintedShares));
+      await expect(await woeth.balanceOf(domen.address)).to.be.equal(
+        mintedShares
+      );
+      await expect(await oeth.balanceOf(domen.address)).to.be.equal(
+        balanceBefore.sub(assetTransfered)
+      );
+    });
+    it("should withdraw at the correct ratio", async () => {
+      const { oeth, woeth, domen } = fixture;
+      // First wrap some OETH
+      await woeth.connect(domen).deposit(oethUnits("50"), domen.address);
+
+      const totalSupply = await woeth.totalSupply();
+      const balanceBefore = await oeth.balanceOf(domen.address);
+
+      // Then unwrap some WOETH
+      const txResponse = await woeth
+        .connect(domen)
+        .withdraw(
+          await woeth.maxWithdraw(domen.address),
+          domen.address,
+          domen.address
+        );
+      const txReceipt = await txResponse.wait();
+      const burnedShares = txReceipt.events[2].args.shares; // 0. transfer oeth, 1. transfer woeth, 2. withdraw
+      const assetTransfered = txReceipt.events[2].args.assets; // 0. transfer oeth, 1. transfer woeth, 2. mint
+
+      await expect(assetTransfered).to.be.approxEqual(oethUnits("50"));
+      await expect(
+        await woeth.convertToShares(assetTransfered)
+      ).to.be.approxEqual(burnedShares);
+      await expect(woeth).to.have.a.totalSupply(totalSupply.sub(burnedShares));
+      await expect(await woeth.balanceOf(domen.address)).to.be.equal(0);
+      await expect(await oeth.balanceOf(domen.address)).to.be.approxEqual(
+        balanceBefore.add(assetTransfered)
+      );
+    });
+    it("should mint at the correct ratio", async () => {
+      const { oeth, woeth, domen } = fixture;
+
+      const totalSupply = await woeth.totalSupply();
+      const balanceBefore = await oeth.balanceOf(domen.address);
+
+      // Mint some WOETH
+      const txResponse = await woeth
+        .connect(domen)
+        .mint(oethUnits("25"), domen.address);
+      const txReceipt = await txResponse.wait();
+      const mintedShares = txReceipt.events[2].args.shares; // 0. transfer oeth, 1. transfer woeth, 2. mint
+      const assetTransfered = txReceipt.events[2].args.assets; // 0. transfer oeth, 1. transfer woeth, 2. mint
+
+      await expect(mintedShares).to.be.equal(oethUnits("25"));
+      await expect(await woeth.convertToAssets(mintedShares)).to.be.approxEqual(
+        assetTransfered
+      );
+      await expect(woeth).to.have.a.totalSupply(totalSupply.add(mintedShares));
+      await expect(await woeth.balanceOf(domen.address)).to.be.equal(
+        mintedShares
+      );
+      await expect(await oeth.balanceOf(domen.address)).to.be.equal(
+        balanceBefore.sub(assetTransfered)
+      );
+    });
+    it("should redeem at the correct ratio", async () => {
+      const { oeth, woeth, domen } = fixture;
+
+      // Mint some WOETH
+      await woeth.connect(domen).mint(oethUnits("25"), domen.address);
+
+      const totalSupply = await woeth.totalSupply();
+      const balanceBefore = await oeth.balanceOf(domen.address);
+
+      // Redeem some WOETH
+      const txResponse = await woeth
+        .connect(domen)
+        .redeem(
+          await woeth.maxRedeem(domen.address),
+          domen.address,
+          domen.address
+        );
+      const txReceipt = await txResponse.wait();
+      const burnedShares = txReceipt.events[2].args.shares; // 0. transfer oeth, 1. transfer woeth, 2. redeem
+      const assetTransfered = txReceipt.events[2].args.assets; // 0. transfer oeth, 1. transfer woeth, 2. redeem
+
+      await expect(burnedShares).to.be.equal(oethUnits("25"));
+      await expect(await woeth.convertToAssets(burnedShares)).to.be.approxEqual(
+        assetTransfered
+      );
+      await expect(woeth).to.have.a.totalSupply(totalSupply.sub(burnedShares));
+      await expect(await woeth.balanceOf(domen.address)).to.be.equal(0);
+      await expect(await oeth.balanceOf(domen.address)).to.be.approxEqual(
+        balanceBefore.add(assetTransfered)
+      );
+    });
+  });
 });
