e5d8432246951b2b531b894dcbf0add5ea345c19
Nicholas Addison <nick@addisonbrown.com.au>
2024-07-11 09:32:25 +1000

    Run log claim remaining stETH

--- brownie/runlogs/2024_07_strategist.py
+++ brownie/runlogs/2024_07_strategist.py
@@ -224,7 +224,6 @@ def main():
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
     print("-----")
 
-
 # -------------------------------------
 # Jul 10, 2024 - Claim second batch of withdrawals from Lido Withdrawal Strategy
 # -------------------------------------
@@ -276,7 +275,7 @@ def main():
         std
       )
     )
-    
+
     steth_remaining = steth.balanceOf(OETH_VAULT)
 
     # deposit of the remaining stETH
@@ -295,6 +294,38 @@ def main():
     profit = vault_change - supply_change
     txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
     print("stETH remaining", "{:.6f}".format(steth_remaining / 10**18), steth_remaining)
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+# -------------------------------------
+# Jul 10, 2024 - Claim withdrawals from Lido Withdrawal Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+    
+    # Claim the withdrawal requests from the Lido Withdrawal Queue
+    txs.append(
+      lido_withdrawal_strat.claimWithdrawals(
+        # withdrawal NFT IDs in tx 0x3d43421cc03b8c6580eba42e4e3f85b1e9b7c82d94c5d28a9f62777775ed2dc9
+        [45345, 45346, 45347, 45348, 45349, 45350, 45351, 45352, 45353, 45354, 45355, 45356, 45357],
+        4992 * 10**18,
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
     print("-----")
     print("Profit", "{:.6f}".format(profit / 10**18), profit)
     print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
