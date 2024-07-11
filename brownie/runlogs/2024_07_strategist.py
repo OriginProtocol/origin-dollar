d8b471b36ce803f1577013355b87def0ab391590
Nick Addison <nick@addisonbrown.com.au>
2024-07-11 23:46:51 +1000

    Runlog for deposit of stETH to Lido Withdrawal Strategy (#2124)
    * Runlog added deposit of 1 stETH to LidoWithdrawal Strategy
    
    * Runlog for remaining stETH to Lido Withdrawal Strategy
    
    * Run log for claiming ETH form Lido Withdrawal Queue
    
    * Run log deposit to Native Staking Strategy
    
    * Added claim of 4992 ETH from Lido to run log
    
    * Split last Lido withdrawal into two depositToStrategy txs
    
    * no more stETH so skip Lido withdrawal fork tests
    
    * Run log claim remaining stETH
    
    * Updated claim of remaining stETH from Lido
    
    * Added deposit to the Native Staking Strategy

--- brownie/runlogs/2024_07_strategist.py
+++ brownie/runlogs/2024_07_strategist.py
@@ -94,3 +94,253 @@ def main():
     print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
     print("-----")
+
+# -------------------------------------
+# Jul 9, 2024 - First deposit to Lido Withdrawal Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+    
+    # Deposit 1 stETH to Lido Withdrawal Strategy
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_LIDO_WITHDRAWAL_STRAT, 
+        [STETH], 
+        [1 * 10**18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+# -------------------------------------
+# Jul 9, 2024 - Claim first withdrawal from Lido Withdrawal Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+    
+    # Claim the 1 ETH from the Lido Withdrawal Queue
+    txs.append(
+      lido_withdrawal_strat.claimWithdrawals(
+        [45153], # withdrawal NFT id
+        10**18 - 2,
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+
+# -------------------------------------
+# Jul 10, 2024 - deposit to Native Staking Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+    
+    # Deposit WETH to Native Staking Strategy
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_NATIVE_STAKING_STRAT, 
+        [WETH], 
+        # 43 validators * 32 ETH = 1376 ETH
+        [1376 * 10**18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+# -------------------------------------
+# Jul 10, 2024 - Deposit 4992 stETH to Lido Withdrawal Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+    # Deposit 156 validators * 32 ETH = 4992 WETH
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_LIDO_WITHDRAWAL_STRAT, 
+        [STETH], 
+        [4992 * 10 **18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+# -------------------------------------
+# Jul 10, 2024 - Claim second batch of withdrawals from Lido Withdrawal Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+    
+    # Claim the 5 withdrawal requests from the Lido Withdrawal Queue
+    txs.append(
+      lido_withdrawal_strat.claimWithdrawals(
+        [45270, 45271, 45272, 45273, 45274], # withdrawal NFT ids
+        4992 * 10**18,
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+# -------------------------------------
+# Jul 10, 2024 - Deposit all stETH to Lido Withdrawal Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+    
+    # Deposit 156 validators * 32 ETH = 4992 WETH
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_LIDO_WITHDRAWAL_STRAT, 
+        [STETH], 
+        [4992 * 10 **18],
+        std
+      )
+    )
+
+    steth_remaining = steth.balanceOf(OETH_VAULT)
+
+    # deposit of the remaining stETH
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_LIDO_WITHDRAWAL_STRAT, 
+        [STETH], 
+        [steth_remaining],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("stETH remaining", "{:.6f}".format(steth_remaining / 10**18), steth_remaining)
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+# -------------------------------------
+# Jul 11, 2024 - Claim withdrawals from Lido Withdrawal Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+    
+    # Claim the withdrawal requests from the Lido Withdrawal Queue
+    expected_amount = (4992 + 7000) * 10**18 + 914635193689940225665 + 5
+    txs.append(
+      lido_withdrawal_strat.claimWithdrawals(
+        # withdrawal NFT IDs in tx 0x3d43421cc03b8c6580eba42e4e3f85b1e9b7c82d94c5d28a9f62777775ed2dc9
+        [45345, 45346, 45347, 45348, 45349, 45350, 45351, 45352, 45353, 45354, 45355, 45356, 45357],
+        expected_amount,
+        std
+      )
+    )
+
+    # Deposit WETH to Native Staking Strategy
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_NATIVE_STAKING_STRAT, 
+        [WETH], 
+        # (500 max validators - 243 existing validators) * 32 = 8,224 ETH
+        [8224 * 10**18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Expected amount", "{:.6f}".format(expected_amount / 10**18), expected_amount)
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
