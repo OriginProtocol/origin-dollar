a6c6fd7bbd7965faacc21c50315a30991d641a17
Nick Addison <nick@addisonbrown.com.au>
2024-07-12 18:44:18 +1000

    Run log deposits to Native Staking Strategy (#2129)
    * Run log deposit 96 WETH to Native Staking Strategy
    
    * Run log deposit of 32 WETH to Native Staking Strategy
    
    * Skip Native Staking Strategy validator fork tests if the strategy is full

--- brownie/runlogs/2024_07_strategist.py
+++ brownie/runlogs/2024_07_strategist.py
@@ -344,3 +344,70 @@ def main():
     print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
     print("-----")
+
+# -------------------------------------
+# Jul 12, 2024 - Deposit 96 WETH to Native Staking Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+    # Deposit WETH to Native Staking Strategy
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_NATIVE_STAKING_STRAT, 
+        [WETH], 
+        # 3 existing validators * 32 = 96 ETH
+        [96 * 10**18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+
+# -------------------------------------
+# Jul 12, 2024 - Deposit 32 WETH to Native Staking Strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+    # Deposit WETH to Native Staking Strategy
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_NATIVE_STAKING_STRAT, 
+        [WETH], 
+        # 1 more validator
+        [32 * 10**18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
