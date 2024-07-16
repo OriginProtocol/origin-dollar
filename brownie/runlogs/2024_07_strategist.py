2721de136342d10269b56f1e17c41cec3f0abe79
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-07-16 11:25:38 +0530

    Add buyback runlog

--- brownie/runlogs/2024_07_strategist.py
+++ brownie/runlogs/2024_07_strategist.py
@@ -444,3 +444,68 @@ def main():
       print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
     except:
       brownie.chain.revert()
+
+# -------------------------------------
+# Jul 16, 2024 - OETH Buyback
+# -------------------------------------
+from buyback import *
+def main():
+  txs = []
+
+  oeth_for_ogn, oeth_for_cvx = get_balance_splits(OETH)
+
+  with TemporaryFork():
+    txs.append(
+      build_1inch_buyback_tx(
+        OETH,
+        OGN,
+        oeth_for_ogn,
+        3.5
+      )
+    )
+
+    txs.append(
+      build_1inch_buyback_tx(
+        OETH,
+        CVX,
+        oeth_for_cvx,
+        2
+      )
+    )
+
+    txs.append(
+      cvx_locker.processExpiredLocks(True, std)
+    )
+
+    print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Jul 16, 2024 - OUSD Buyback
+# -------------------------------------
+from buyback import *
+def main():
+  txs = []
+
+  ousd_for_ogn, ousd_for_cvx = get_balance_splits(OUSD)
+
+  with TemporaryFork():
+    txs.append(
+      build_1inch_buyback_tx(
+        OUSD,
+        OGN,
+        ousd_for_ogn,
+        3
+      )
+    )
+
+    txs.append(
+      build_1inch_buyback_tx(
+        OUSD,
+        CVX,
+        ousd_for_cvx,
+        2
+      )
+    )
+
+    print(to_gnosis_json(txs))
+
