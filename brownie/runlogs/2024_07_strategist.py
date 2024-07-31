b08dbf9e6221c4955932b5ca27147d31d36b916c
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-07-31 17:32:22 +0530

    Add realloc runlog

--- brownie/runlogs/2024_07_strategist.py
+++ brownie/runlogs/2024_07_strategist.py
@@ -640,3 +640,44 @@ def main():
     )
 
     print(to_gnosis_json(txs))
+  
+# -------------------------------
+# Jul 30, 2024 - OUSD Allocation
+# -------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_core.rebase({'from':STRATEGIST}))
+    txs.append(vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+    # Withdraw 100k from Morpho Aave
+    txs.append(
+      vault_admin.withdrawFromStrategy(
+        MORPHO_AAVE_STRAT, 
+        [usdt], 
+        [100_000 * 10**6], 
+        {'from': STRATEGIST}
+      )
+    )
+
+    # Put everything in Aave
+    txs.append(
+      vault_admin.depositToStrategy(
+        AAVE_STRAT, 
+        [usdt], 
+        [100_000*10**6], 
+        {'from': STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = ousd.totalSupply() - vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (500 * 10**18), vault_change, (500 * 10**18), {'from': STRATEGIST}))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
