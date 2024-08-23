47133bf34a4a1c2b9b0ea58409c3da78802895f1
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-08-23 18:38:40 +0400

    Add OETHb Zapper contract

---
+++ contracts/contracts/vault/OETHBaseZapper.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IVault } from "../interfaces/IVault.sol";
+import { IWETH9 } from "../interfaces/IWETH9.sol";
+
+contract OETHBaseZapper {
+    IERC20 public immutable oethb;
+    IVault public immutable vault;
+
+    IWETH9 public constant weth =
+        IWETH9(0x4200000000000000000000000000000000000006);
+    address private constant ETH_MARKER =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    event Zap(address indexed minter, address indexed asset, uint256 amount);
+
+    constructor(address _oethb, address _vault) {
+        oethb = IERC20(_oethb);
+        vault = IVault(_vault);
+
+        weth.approve(address(_vault), type(uint256).max);
+    }
+
+    /**
+     * @dev Deposit ETH and receive OETH in return.
+     * Will verify that the user is sent 1:1 for ETH.
+     */
+    receive() external payable {
+        deposit();
+    }
+
+    /**
+     * @dev Deposit ETH and receive superOETHb in return
+     * Will verify that the user is sent 1:1 for ETH.
+     * @return Amount of OETH sent to user
+     */
+    function deposit() public payable returns (uint256) {
+        uint256 balance = address(this).balance;
+
+        emit Zap(msg.sender, ETH_MARKER, balance);
+
+        // Wrap ETH
+        weth.deposit{ value: balance }();
+
+        // Mint OETHb
+        uint256 toMint = weth.balanceOf(address(this));
+        vault.mint(address(weth), toMint, balance);
+
+        // State check
+        uint256 mintedAmount = oethb.balanceOf(address(this));
+        require(mintedAmount >= balance, "Zapper: not enough minted");
+        require(oethb.transfer(msg.sender, mintedAmount));
+
+        return mintedAmount;
+    }
+}
