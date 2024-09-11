94f42f297c938d8874b667594f4bd58de1f5a157
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-09-11 09:25:54 +0400

    Update runlog

--- brownie/runlogs/2024_09_strategist.py
+++ brownie/runlogs/2024_09_strategist.py
@@ -239,3 +239,67 @@ def main():
     )
 
     print(to_gnosis_json(txs, OETHB_STRATEGIST, "8453"))
+
+# -------------------------------------
+# Sep 11, 2024 - OETH Buyback
+# -------------------------------------
+from buyback import *
+def main():
+  txs = []
+
+  oeth_for_ogn, oeth_for_cvx = get_balance_splits(OETH)
+
+  with TemporaryFork():
+    txs.append(
+      build_1inch_buyback_tx(
+        OETH,
+        OGN,
+        oeth_for_ogn,
+        3.5
+      )
+    )
+
+    txs.append(
+      build_1inch_buyback_tx(
+        OETH,
+        CVX,
+        oeth_for_cvx,
+        2
+      )
+    )
+
+    txs.append(
+      cvx_locker.processExpiredLocks(True, std)
+    )
+
+    print(to_gnosis_json(txs))
+
+# -------------------------------------
+# Sep 11, 2024 - OUSD Buyback
+# -------------------------------------
+from buyback import *
+def main():
+  txs = []
+
+  ousd_for_ogn, ousd_for_cvx = get_balance_splits(OUSD)
+
+  with TemporaryFork():
+    txs.append(
+      build_1inch_buyback_tx(
+        OUSD,
+        OGN,
+        ousd_for_ogn,
+        3
+      )
+    )
+
+    txs.append(
+      build_1inch_buyback_tx(
+        OUSD,
+        CVX,
+        ousd_for_cvx,
+        2
+      )
+    )
+
+    print(to_gnosis_json(txs))
