50b8ca2a3c073171ada6b74b6ff0706c370bb76b
Nicholas Addison <nick@addisonbrown.com.au>
2024-09-12 13:21:56 +1000

    Rebalance of Base AMO

--- brownie/runlogs/2024_09_strategist.py
+++ brownie/runlogs/2024_09_strategist.py
@@ -345,3 +345,47 @@ def main():
     print("-----")
     print("Profit", "{:.6f}".format(profit / 10**18), profit)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
+# -------------------------------------------
+# Sept 12 2024 - OETHb allocation & rebalance
+# -------------------------------------------
+from world_base import *
+
+def main():
+  with TemporaryForkForOETHbReallocations() as txs:
+    # Before
+    txs.append(vault_core.rebase({ 'from': OETHB_STRATEGIST }))
+    txs.append(vault_value_checker.takeSnapshot({ 'from': OETHB_STRATEGIST }))
+
+    # Deposit all WETH
+    wethAmount = weth.balanceOf(OETHB_VAULT_PROXY_ADDRESS)
+    txs.append(
+      vault_admin.depositToStrategy(
+        OETHB_AERODROME_AMO_STRATEGY, 
+        [weth], 
+        [wethAmount], 
+        {'from': OETHB_STRATEGIST}
+      )
+    )
+
+    # deposit funds into the underlying strategy
+    txs.append(
+      amo_strat.rebalance(
+        0, 
+        True,
+        0,
+        {'from': OETHB_STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(OETHB_STRATEGIST)[0]
+    supply_change = oethb.totalSupply() - vault_value_checker.snapshots(OETHB_STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), {'from': OETHB_STRATEGIST}))
+
+    print("-----")
+    print("WETH", "{:.6f}".format(wethAmount / 10**18), wethAmount)
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
