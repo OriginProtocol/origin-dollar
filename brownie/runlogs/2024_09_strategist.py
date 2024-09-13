14bdfe306bb5520fb66ca1c478fea1dced6b2b64
Nick Addison <nick@addisonbrown.com.au>
2024-09-13 16:01:49 +1000

    OETHb deposit and rebalance (#2230)

--- brownie/runlogs/2024_09_strategist.py
+++ brownie/runlogs/2024_09_strategist.py
@@ -389,3 +389,60 @@ def main():
     print("WETH", "{:.6f}".format(wethAmount / 10**18), wethAmount)
     print("Profit", "{:.6f}".format(profit / 10**18), profit)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
+# -------------------------------------------
+# Sept 13 2024 - OETHb allocation & rebalance
+# -------------------------------------------
+from world_base import *
+
+def main():
+  with TemporaryForkForOETHbReallocations() as txs:
+    # Before
+    txs.append(vault_core.rebase({ 'from': OETHB_STRATEGIST }))
+    txs.append(vault_value_checker.takeSnapshot({ 'from': OETHB_STRATEGIST }))
+
+    # Deposit all WETH
+    wethDepositAmount = weth.balanceOf(OETHB_VAULT_PROXY_ADDRESS)
+    txs.append(
+      vault_admin.depositToStrategy(
+        OETHB_AERODROME_AMO_STRATEGY, 
+        [weth], 
+        [wethDepositAmount], 
+        {'from': OETHB_STRATEGIST}
+      )
+    )
+
+    # Rebalance the AMO pool
+    wethPoolBalance = weth.balanceOf(AERODROME_WETH_OETHB_POOL_BASE)
+    superOETHbPoolBalance = oethb.balanceOf(AERODROME_WETH_OETHB_POOL_BASE)
+    total = wethPoolBalance + superOETHbPoolBalance
+    swapAmount = total * 0.01
+    minAmount = swapAmount * 0.98
+
+    txs.append(
+      amo_strat.rebalance(
+        swapAmount,
+        False,
+        minAmount,
+        {'from': OETHB_STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(OETHB_STRATEGIST)[0]
+    supply_change = oethb.totalSupply() - vault_value_checker.snapshots(OETHB_STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), {'from': OETHB_STRATEGIST}))
+
+    print("Pool WETH      ", "{:.6f}".format(wethPoolBalance / 10**18), wethPoolBalance * 100 / total)
+    print("Pool superOETH ", "{:.6f}".format(superOETHbPoolBalance / 10**18), superOETHbPoolBalance * 100 / total)
+    print("Pool Total     ", "{:.6f}".format(total / 10**18), total)
+    print("-----")
+    print("WETH Deposit", "{:.6f}".format(wethDepositAmount / 10**18), wethDepositAmount)
+    print("-----")
+    print("Swap amount", "{:.6f}".format(swapAmount / 10**18), swapAmount)
+    print("Min  amount", "{:.6f}".format(minAmount / 10**18), minAmount)
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
