56d9e60e7727716f7f530ae71cc19c90ab1788a3
Nicholas Addison <nick@addisonbrown.com.au>
2024-09-13 17:36:51 +1000

    Added withdrawal queue functions to Brownie Vault Core ABI Added OETH ARM to Brownie Added withdrawal from OETH AMO

--- brownie/runlogs/2024_09_strategist.py
+++ brownie/runlogs/2024_09_strategist.py
@@ -446,3 +446,44 @@ def main():
     print("-----")
     print("Profit", "{:.6f}".format(profit / 10**18), profit)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
+
+# -------------------------------------------
+# Sept 13 2024 - Withdraw from OETH AMO Strategy
+# -------------------------------------------
+
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase({'from': STRATEGIST}))
+    txs.append(oeth_vault_value_checker.takeSnapshot({'from': STRATEGIST}))
+
+    # Remove 50 WETH from strategy and burn equivalent OETH
+    txs.append(
+      vault_oeth_admin.withdrawFromStrategy(
+        OETH_CONVEX_OETH_ETH_STRAT, 
+        [weth], 
+        [50 * 10**18],
+        {'from': STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (0.1 * 10**18), {'from': STRATEGIST}))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+    # Test the OETH ARM can claim its withdrawals
+    txs.append(
+      vault_oeth_core.claimWithdrawals(
+        [47, 48],
+        {'from': OETH_ARM}
+      )
+    )
