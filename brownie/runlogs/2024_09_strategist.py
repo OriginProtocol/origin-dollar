ed142b61e3731d0f2e336415dd559ccd597dc81a
Domen Grabec <grabec@gmail.com>
2024-09-18 00:47:20 +0200

    rebalance

--- brownie/runlogs/2024_09_strategist.py
+++ brownie/runlogs/2024_09_strategist.py
@@ -686,3 +686,58 @@ def main():
     print("--------------------")
     print("Profit       ", c18(profit), profit)
     print("Vault Change ", c18(vault_change), vault_change)
+
+# -----------------------------------------------------
+# Sept 18 2024 - OETHb allocation & rebalance 22:50 CET
+# -----------------------------------------------------
+
+from world_base import *
+
+def main():
+  with TemporaryForkForOETHbReallocations() as txs:
+    # Before
+    txs.append(vault_core.rebase({ 'from': OETHB_STRATEGIST }))
+    txs.append(vault_value_checker.takeSnapshot({ 'from': OETHB_STRATEGIST }))
+
+    # Deposit all WETH
+    wethDepositAmount = weth.balanceOf(OETHB_VAULT_PROXY_ADDRESS)
+    txs.append(
+      vault_admin.depositToStrategy(
+        OETHB_AERODROME_AMO_STRATEGY, 
+        [weth], 
+        [wethDepositAmount], 
+        {'from': OETHB_STRATEGIST}
+      )
+    )
+
+    amo_snapsnot()
+    swapWeth = True
+    swapAmount = 0
+    minAmount = swapAmount * 0.98
+    print("--------------------")
+    print("WETH Deposit ", c18(wethDepositAmount))
+    print("-----")
+    print("Swap amount  ", c18(swapAmount))
+    print("Min  amount  ", c18(minAmount))
+    print("-----")
+
+    txs.append(
+      amo_strat.rebalance(
+        swapAmount,
+        swapWeth,
+        minAmount,
+        {'from': OETHB_STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(OETHB_STRATEGIST)[0]
+    supply_change = oethb.totalSupply() - vault_value_checker.snapshots(OETHB_STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), {'from': OETHB_STRATEGIST}))
+
+    amo_snapsnot()
+    print("--------------------")
+    print("Profit       ", c18(profit), profit)
+    print("Vault Change ", c18(vault_change), vault_change)
