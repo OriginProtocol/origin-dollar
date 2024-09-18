94ab9775e9eb1298ce753ad145d977a241489015
Shah <10547529+shahthepro@users.noreply.github.com>
2024-09-18 20:51:41 +0400

    Add script to deposit to wOETH strategy (#2242)
    * Add script to deposit to wOETH strategy
    
    * Update code

--- brownie/world_base.py
+++ brownie/world_base.py
@@ -3,6 +3,7 @@ from world_abstract import *
 weth = load_contract('ERC20', WETH_BASE)
 aero = load_contract('ERC20', AERO_BASE)
 oethb = load_contract('ousd', OETHB)
+woeth = load_contract('ERC20', BRIDGED_WOETH_BASE)
 
 base_strategist = brownie.accounts.at(OETHB_STRATEGIST, force=True)
 
@@ -16,6 +17,8 @@ amo_strat = load_contract('aerodrome_amo_strategy', OETHB_AERODROME_AMO_STRATEGY
 vault_admin = load_contract('vault_admin', OETHB_VAULT_PROXY_ADDRESS)
 vault_core = load_contract('vault_core', OETHB_VAULT_PROXY_ADDRESS)
 vault_value_checker = load_contract('vault_value_checker', OETHB_VAULT_VALUE_CHECKER)
+
+woeth_strat = load_contract('woeth_strategy', OETHB_WOETH_STRATEGY)
 
 dripper = load_contract('dripper', OETHB_DRIPPER)
 
