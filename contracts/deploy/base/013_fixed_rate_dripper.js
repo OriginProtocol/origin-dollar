7585394a1cfeb8b97daa19fb9d3aa60d56e10825
Shah <10547529+shahthepro@users.noreply.github.com>
2024-09-27 07:54:20 +0400

    Add FixedRateDripper (#2248)
    * Add FixedRateDripper
    
    * Trim down code
    
    * Add some tests
    
    * Update test
    
    * Add comment

---
+++ contracts/deploy/base/013_fixed_rate_dripper.js
@@ -0,0 +1,30 @@
+const { deployOnBaseWithGuardian } = require("../../utils/deploy-l2");
+const { deployWithConfirmation } = require("../../utils/deploy");
+const addresses = require("../../utils/addresses");
+
+module.exports = deployOnBaseWithGuardian(
+  {
+    deployName: "013_fixed_rate_dripper",
+  },
+  async ({ ethers }) => {
+    const cOETHbDripperProxy = await ethers.getContract("OETHBaseDripperProxy");
+    const cOETHbVaultProxy = await ethers.getContract("OETHBaseVaultProxy");
+
+    // Deploy new implementation
+    const dOETHbDripper = await deployWithConfirmation("FixedRateDripper", [
+      cOETHbVaultProxy.address,
+      addresses.base.WETH,
+    ]);
+
+    return {
+      actions: [
+        {
+          // 1. Upgrade Dripper
+          contract: cOETHbDripperProxy,
+          signature: "upgradeTo(address)",
+          args: [dOETHbDripper.address],
+        },
+      ],
+    };
+  }
+);
