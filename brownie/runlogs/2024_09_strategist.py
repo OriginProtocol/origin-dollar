f31be884a554de57fef0f7dd43fe45b7fb8b6af0
Nicholas Addison <nick@addisonbrown.com.au>
2024-09-27 12:09:10 +1000

    Run log - withdraw 16 WETH from 2nd staking strategy

--- brownie/runlogs/2024_09_strategist.py
+++ brownie/runlogs/2024_09_strategist.py
@@ -1292,3 +1292,36 @@ def main():
     print("-----")
     print("Profit", "{:.6f}".format(profit / 10**18), profit)
     print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
+
+# -------------------------------
+# Sep 27, 2024 - Withdraw from 2nd Native Staking Strategy
+# -------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(oeth_dripper.collectAndRebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+    # Withdraw 16 WETH from the Second Native Staking Strategy
+    txs.append(
+      vault_oeth_admin.withdrawFromStrategy(
+        OETH_NATIVE_STAKING_2_STRAT, 
+        [WETH], 
+        [16 * 10**18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**17), vault_change, (1 * 10**17), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+
