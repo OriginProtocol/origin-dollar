592a2e415bd2547d8dca6e8f8cf82c5727e6bdac
Clément <55331875+clement-ux@users.noreply.github.com>
2024-10-04 01:48:45 +0200

    Run log OUSD reallocate 200k USDC to new MetaMorpho strat (#2268)
    * Run log OUSD reallocate 200k USDC to new MetaMorpho strat
    
    * Updated run log comment
    
    ---------
    
    Co-authored-by: Nicholas Addison <nick@addisonbrown.com.au>

--- brownie/runlogs/2024_10_strategist.py
+++ brownie/runlogs/2024_10_strategist.py
@@ -52,3 +52,44 @@ def main():
     print("--------------------")
     print("Profit       ", c18(profit), profit)
     print("Vault Change ", c18(vault_change), vault_change)
+
+# -----------------------------------------------------
+# Oct 3rd 2024 - OUSD Reallocation 200k USDC from Morpho Aave to new MetaMorpho
+# -----------------------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_core.rebase({'from':STRATEGIST}))
+    txs.append(vault_value_checker.takeSnapshot({'from':STRATEGIST}))
+
+    # Withdraw 200k from Morpho Aave
+    txs.append(
+      vault_admin.withdrawFromStrategy(
+        MORPHO_AAVE_STRAT, 
+        [usdc], 
+        [200_000 * 10**6], 
+        {'from': STRATEGIST}
+      )
+    )
+
+    # Put everything in new MetaMorpho
+    txs.append(
+      vault_admin.depositToStrategy(
+        MORPHO_META_USDC_STRAT, 
+        [usdc], 
+        [200_000*10**6], 
+        {'from': STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = ousd.totalSupply() - vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (500 * 10**18), vault_change, (500 * 10**18), {'from': STRATEGIST}))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
