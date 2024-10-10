6abd45bf1dfe88c9c8b6d25a4fdcb0e53107235d
Domen Grabec <grabec@gmail.com>
2024-10-11 00:23:43 +0200

    AMO automatic deposits (#2266)
    * add contract code and fix tests for the AMO to automatically rebalance
    
    * intermediary work
    
    * add tests for automatic deposits to AMO strategy
    
    * disable automatic WETH allocation
    
    * minor fix
    
    * minor fix
    
    * test name fix
    
    * auto allocation threshold set to 0.1
    
    * refactor some rebalance functionality into a separate function. Expose WETH share as a public function
    
    * use approve instead of safeApprove for WETH
    
    * remove safe approve from OETHb
    
    * correct comment

--- contracts/test/_fixture-base.js
+++ contracts/test/_fixture-base.js
@@ -156,6 +156,9 @@ const defaultBaseFixture = deployments.createFixture(async () => {
 
     await impersonateAndFund(governor.address);
     await impersonateAndFund(timelock.address);
+
+    // configure Vault to not automatically deposit to strategy
+    await oethbVault.connect(governor).setVaultBuffer(oethUnits("1"));
   }
 
   // Make sure we can print bridged WOETH for tests
