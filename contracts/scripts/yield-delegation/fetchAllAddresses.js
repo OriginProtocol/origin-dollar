7ae4fbcb95b8a0963fa3e54d142d43da82818a7c
Domen Grabec <grabec@gmail.com>
2024-11-28 01:21:21 +0100

    finish the balance extraction script
    (cherry picked from commit 5f7a208fae60f03858d64a63c0c178c99b528a55)

--- contracts/scripts/yield-delegation/fetchAllAddresses.js
+++ contracts/scripts/yield-delegation/fetchAllAddresses.js
@@ -7,25 +7,38 @@ const sql = postgres(process.env.SQUID_DB_URL);
 
 const OETH_ADDRESS = "0x856c4efb76c1d1ae02e20ceb03a2a6a08b0b8dc3";
 const OUSD_ADDRESS = "0x2a8e1e676ec238d8a992307b495b45b3feaa5e86";
+const SUPER_OETH_ADDRESS = "0xdbfefd2e8460a6ee4955a68582f85708baea60a3";
 
-async function getAddresses(oTokenAddress) {
+async function getAddressesAndBalances(oTokenAddress, maxBlockNumber, csvFileName) {
   const batchSize = 5000;
   let offset = 0;
   const allAddresses = [];
 
+  const blockSmallerOrEq = blockNumber => sql` AND block_number <= ${ blockNumber }`
   while (true) {
+    console.log(`Running query batchSize: ${batchSize}, offset: ${offset}`);
     const batch = await sql`
-      SELECT DISTINCT account 
-      FROM erc20_balance 
-      WHERE address = ${oTokenAddress}
-      ORDER BY account
-      LIMIT ${batchSize} 
-      OFFSET ${offset}
+      SELECT b.account, b.balance, b.block_number FROM (
+        SELECT DISTINCT ON (account) account, balance, block_number
+        FROM erc20_balance
+        WHERE address = ${oTokenAddress}
+        ${
+          maxBlockNumber > 0 ?
+            blockSmallerOrEq(maxBlockNumber) :
+            sql``
+        }
+        ORDER BY account, block_number DESC
+        LIMIT ${batchSize} 
+        OFFSET ${offset}
+      ) b WHERE balance > 0
     `;
 
     if (batch.length === 0) break;
 
-    batch.forEach(row => allAddresses.push(row.account));
+    batch.forEach((row) => {
+      allAddresses.push([row.account, row.balance, row.block_number].join(','))
+      //console.log(`account: ${row.account} balance: ${row.balance} block_number: ${row.block_number}`)
+    });
     offset += batchSize;
     
     console.log(`Processed ${allAddresses.length} unique addresses so far...`);
@@ -34,31 +47,17 @@ async function getAddresses(oTokenAddress) {
   console.log(`Total ${oTokenAddress == OETH_ADDRESS ? "OETH" : "OUSD"} unique addresses found: ${allAddresses.length}`);
 
   // // Write addresses to CSV
-  // const csvContent = allAddresses.join('\n');
-  // fs.writeFileSync('unique_addresses.csv', csvContent);
-  // console.log('Addresses written to unique_addresses.csv');
+  const csvContent = allAddresses.join('\n');
+  fs.writeFileSync(csvFileName, csvContent);
+  console.log(`Addresses and balances written to ${csvFileName}`);
 
   return allAddresses;
 }
 
-async function getCredits(oToken, userAddress, blockNumber) {}
-
-async function getBalance(oToken, userAddress, blockNumber) {}
-
-async function processOTokenAddresses(oTokenAddress) {
-  const addresses = await getAddresses(oTokenAddress);
-
-
-}
-
 async function main() {
-  const oethAddresses = await getAddresses(OETH_ADDRESS);
-  const ousdAddresses = await getAddresses(OUSD_ADDRESS);
-
-  // const allAddresses = [...oethAddresses, ...ousdAddresses];
-
-  // console.log(allAddresses.length);
-
+  const oethAddresses = await getAddressesAndBalances(OETH_ADDRESS, 0, 'oethBalances.csv');
+  const ousdAddresses = await getAddressesAndBalances(OUSD_ADDRESS, 0, 'ousdBalances.csv');
+  const soethAddresses = await getAddressesAndBalances(SUPER_OETH_ADDRESS, 0, 'soethBalances.csv');
 }
 
 main()
