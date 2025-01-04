9eefde240b257df22ab56b3c74d24efcc2ee8fb6
Clément <clemmoller@gmail.com>
2025-01-04 22:58:47 +0100

    test: add more tests.

--- contracts/test/harvest/simple-harvester.mainnet.fork-test.js
+++ contracts/test/harvest/simple-harvester.mainnet.fork-test.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 
 const addresses = require("../../utils/addresses");
 const { isCI } = require("../helpers");
+const { setERC20TokenBalance } = require("../_fund");
 
 const { loadDefaultFixture } = require("../_fixture");
 
@@ -28,7 +29,7 @@ describe("ForkTest: CurvePoolBooster", function () {
     );
   });
 
-  it("Should support Strategy", async () => {
+  it("Should support Strategy as governor", async () => {
     const { simpleOETHHarvester } = fixture;
     const timelock = await ethers.provider.getSigner(
       addresses.mainnet.Timelock
@@ -49,6 +50,49 @@ describe("ForkTest: CurvePoolBooster", function () {
     ).to.be.equal(true);
   });
 
+  it("Should support Strategy as strategist", async () => {
+    const { simpleOETHHarvester } = fixture;
+    const strategist = await ethers.provider.getSigner(
+      await simpleOETHHarvester.strategistAddr()
+    );
+
+    expect(
+      await simpleOETHHarvester.supportedStrategies(
+        addresses.mainnet.ConvexOETHAMOStrategy
+      )
+    ).to.be.equal(false);
+    await simpleOETHHarvester
+      .connect(strategist)
+      .setSupportedStrategy(addresses.mainnet.ConvexOETHAMOStrategy, true);
+    expect(
+      await simpleOETHHarvester.supportedStrategies(
+        addresses.mainnet.ConvexOETHAMOStrategy
+      )
+    ).to.be.equal(true);
+  });
+
+  it("Should revert if support strategy is not governor or strategist", async () => {
+    const { simpleOETHHarvester, josh } = fixture;
+
+    await expect(
+      // prettier-ignore
+      simpleOETHHarvester
+        .connect(josh)
+        .setSupportedStrategy(addresses.mainnet.ConvexOETHAMOStrategy, true)
+    ).to.be.revertedWith("Caller is not the Strategist or Governor");
+  });
+
+  it("Should revert if when setting strategist is not governor", async () => {
+    const { simpleOETHHarvester, josh } = fixture;
+
+    await expect(
+      // prettier-ignore
+      simpleOETHHarvester
+        .connect(josh)
+        .setStrategistAddr(josh.address)
+    ).to.be.revertedWith("Caller is not the Governor");
+  });
+
   it("Should Set strategist", async () => {
     const { simpleOETHHarvester, josh } = fixture;
     const timelock = await ethers.provider.getSigner(
@@ -62,7 +106,24 @@ describe("ForkTest: CurvePoolBooster", function () {
     expect(await simpleOETHHarvester.strategistAddr()).to.equal(josh.address);
   });
 
-  it("Should Harvest and transfer rewards", async () => {
+  it("Should Harvest and transfer rewards as strategist", async () => {
+    const { simpleOETHHarvester, convexEthMetaStrategy, crv } = fixture;
+    const strategistAddress = await simpleOETHHarvester.strategistAddr();
+    const strategist = await ethers.provider.getSigner(strategistAddress);
+
+    const balanceBeforeCRV = await crv.balanceOf(strategistAddress);
+    await simpleOETHHarvester
+      .connect(strategist)
+      .setSupportedStrategy(convexEthMetaStrategy.address, true);
+    // prettier-ignore
+    await simpleOETHHarvester
+      .connect(strategist)["harvestAndTransfer(address)"](convexEthMetaStrategy.address);
+
+    const balanceAfterCRV = await crv.balanceOf(strategistAddress);
+    expect(balanceAfterCRV).to.be.gt(balanceBeforeCRV);
+  });
+
+  it("Should Harvest and transfer rewards as governor", async () => {
     const { simpleOETHHarvester, convexEthMetaStrategy, crv } = fixture;
     const timelock = await ethers.provider.getSigner(
       addresses.mainnet.Timelock
@@ -105,5 +166,34 @@ describe("ForkTest: CurvePoolBooster", function () {
       simpleOETHHarvester
         .connect(timelock).setSupportedStrategy(addresses.zero, true)
     ).to.be.revertedWith("Invalid strategy");
+  });
+
+  it("Should test to rescue tokens as governor", async () => {
+    const { simpleOETHHarvester, crv } = fixture;
+    const timelock = await ethers.provider.getSigner(
+      addresses.mainnet.Timelock
+    );
+
+    await setERC20TokenBalance(simpleOETHHarvester.address, crv, "1000");
+    const balanceBeforeCRV = await crv.balanceOf(simpleOETHHarvester.address);
+    await simpleOETHHarvester
+      .connect(timelock)
+      .transferToken(crv.address, "1000");
+    const balanceAfterCRV = await crv.balanceOf(simpleOETHHarvester.address);
+    expect(balanceAfterCRV).to.be.lt(balanceBeforeCRV);
+  });
+
+  it("Should test to rescue tokens as strategist", async () => {
+    const { simpleOETHHarvester, crv } = fixture;
+    const strategistAddress = await simpleOETHHarvester.strategistAddr();
+    const strategist = await ethers.provider.getSigner(strategistAddress);
+
+    await setERC20TokenBalance(simpleOETHHarvester.address, crv, "1000");
+    const balanceBeforeCRV = await crv.balanceOf(simpleOETHHarvester.address);
+    await simpleOETHHarvester
+      .connect(strategist)
+      .transferToken(crv.address, "1000");
+    const balanceAfterCRV = await crv.balanceOf(simpleOETHHarvester.address);
+    expect(balanceAfterCRV).to.be.lt(balanceBeforeCRV);
   });
 });
