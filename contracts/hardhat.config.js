9a5cb567a4bee2505fb094a309ae822c412d98d1
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-01-06 12:40:16 +0400

    Update fixtures

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -50,6 +50,8 @@ const BASE_DEPLOYER = MAINNET_DEPLOYER;
 const BASE_GOVERNOR = "0x92A19381444A001d62cE67BaFF066fA1111d7202";
 const BASE_STRATEGIST = "0x28bce2eE5775B652D92bB7c2891A89F036619703";
 
+const MULTICHAIN_STRATEGIST = "0x4FF1b9D9ba8558F5EAfCec096318eA0d8b541971";
+
 const mnemonic =
   "replace hover unaware super where filter stone fine garlic address matrix basic";
 
@@ -317,6 +319,9 @@ module.exports = {
       mainnet: MAINNET_STRATEGIST,
       holesky: HOLESKY_DEPLOYER, // on Holesky the deployer is also the strategist
       base: BASE_STRATEGIST,
+    },
+    multichainStrategistAddr: {
+      default: MULTICHAIN_STRATEGIST,
     },
   },
   contractSizer: {
