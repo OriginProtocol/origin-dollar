1b488bc8ce1c79cb4c6acaa5fb8f195e1b241920
Clément <clemmoller@gmail.com>
2025-01-13 19:46:11 +0100

    fix: allow to transferERC20 to custom receiver.

--- contracts/contracts/harvest/Dripper.sol
+++ contracts/contracts/harvest/Dripper.sol
@@ -140,9 +140,9 @@ contract Dripper is Governable {
 
     /// @dev Transfer out all ERC20 held by the contract. Governor only.
     /// @param _asset ERC20 token address
-    function transferAllToken(address _asset) external onlyGovernor {
+    function transferAllToken(address _asset, address _receiver) external onlyGovernor {
         IERC20(_asset).safeTransfer(
-            governor(),
+            _receiver,
             IERC20(_asset).balanceOf(address(this))
         );
     }
