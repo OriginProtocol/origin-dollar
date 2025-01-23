5d19df67fdfcc010d043e6045ec84eea95f2e70b
Clément <clemmoller@gmail.com>
2025-01-23 22:10:47 +0100

    feat: add extra test for Curve Pool Booster.

--- contracts/test/strategies/curvePoolBooster.mainnet.fork-test.js
+++ contracts/test/strategies/curvePoolBooster.mainnet.fork-test.js
@@ -52,6 +52,7 @@ describe("ForkTest: CurvePoolBooster", function () {
       );
   }
 
+  // --- Initialization ---
   it("Should have correct params", async () => {
     const { curvePoolBooster } = fixture;
     const { multichainStrategistAddr } = await getNamedAccounts();
@@ -76,6 +77,7 @@ describe("ForkTest: CurvePoolBooster", function () {
     );
   });
 
+  // --- Campaign Management ---
   it("Should Create a campaign", async () => {
     const { curvePoolBooster, ousd } = fixture;
 
@@ -101,32 +103,6 @@ describe("ForkTest: CurvePoolBooster", function () {
 
     // Ensure fee is collected
     expect(await ousd.balanceOf(josh.address)).to.equal(parseUnits("1"));
-  });
-
-  it("Should set campaign id", async () => {
-    const { curvePoolBooster } = fixture;
-    const { multichainStrategistAddr } = await getNamedAccounts();
-    const sStrategist = await ethers.provider.getSigner(
-      multichainStrategistAddr
-    );
-    expect(await curvePoolBooster.campaignId()).to.equal(0);
-
-    await curvePoolBooster.connect(sStrategist).setCampaignId(12);
-    expect(await curvePoolBooster.campaignId()).to.equal(12);
-  });
-
-  it("Should set fee and fee collector", async () => {
-    const { curvePoolBooster, josh } = fixture;
-    const gov = await curvePoolBooster.governor();
-    const sGov = await ethers.provider.getSigner(gov);
-    expect(await curvePoolBooster.fee()).to.equal(0);
-
-    await curvePoolBooster.connect(sGov).setFee(100);
-    expect(await curvePoolBooster.fee()).to.equal(100);
-
-    expect(await curvePoolBooster.feeCollector()).not.to.equal(josh.address);
-    await curvePoolBooster.connect(sGov).setFeeCollector(josh.address);
-    expect(await curvePoolBooster.feeCollector()).to.equal(josh.address);
   });
 
   it("Should manage total rewards", async () => {
@@ -172,8 +148,6 @@ describe("ForkTest: CurvePoolBooster", function () {
       .manageNumberOfPeriods(2, parseUnits("0.1"), 0);
   });
 
-  // J'en suis ici!
-
   it("Should manage reward per voter", async () => {
     const { curvePoolBooster, ousd, wousd } = fixture;
     const { multichainStrategistAddr } = await getNamedAccounts();
@@ -198,30 +172,17 @@ describe("ForkTest: CurvePoolBooster", function () {
       .manageRewardPerVote(100, parseUnits("0.1"), 0);
   });
 
-  it("Should rescue ETH", async () => {
+  it("Should close a campaing", async () => {
     const { curvePoolBooster } = fixture;
     const { multichainStrategistAddr } = await getNamedAccounts();
     const sStrategist = await ethers.provider.getSigner(
       multichainStrategistAddr
     );
+    await dealOETHAndCreateCampaign();
 
-    // Deal ETH to pool booster
-    await sStrategist.sendTransaction({
-      to: curvePoolBooster.address,
-      value: parseUnits("1"),
-    });
-
-    const balanceBefore = await ethers.provider.getBalance(
-      curvePoolBooster.address
-    );
     await curvePoolBooster
       .connect(sStrategist)
-      .rescueETH(multichainStrategistAddr);
-    const balanceAfter = await ethers.provider.getBalance(
-      curvePoolBooster.address
-    );
-    expect(balanceBefore).to.be.eq(parseUnits("1"));
-    expect(balanceAfter).to.be.eq(parseUnits("0"));
+      .closeCampaign(12, parseUnits("0.1"), 0);
   });
 
   it("Should revert if not called by operator", async () => {
@@ -376,6 +337,95 @@ describe("ForkTest: CurvePoolBooster", function () {
     ).to.be.revertedWith("No reward to manage");
   });
 
+  // --- Rescue ETH and ERC20 ---
+  it("Should rescue ETH", async () => {
+    const { curvePoolBooster } = fixture;
+    const { multichainStrategistAddr } = await getNamedAccounts();
+    const sStrategist = await ethers.provider.getSigner(
+      multichainStrategistAddr
+    );
+
+    // Deal ETH to pool booster
+    await sStrategist.sendTransaction({
+      to: curvePoolBooster.address,
+      value: parseUnits("1"),
+    });
+
+    const balanceBefore = await ethers.provider.getBalance(
+      curvePoolBooster.address
+    );
+    await curvePoolBooster
+      .connect(sStrategist)
+      .rescueETH(multichainStrategistAddr);
+    const balanceAfter = await ethers.provider.getBalance(
+      curvePoolBooster.address
+    );
+    expect(balanceBefore).to.be.eq(parseUnits("1"));
+    expect(balanceAfter).to.be.eq(parseUnits("0"));
+  });
+
+  it("Should rescue ERC20", async () => {
+    const { curvePoolBooster, ousd, wousd } = fixture;
+    const { multichainStrategistAddr } = await getNamedAccounts();
+    const woethSigner = await impersonateAndFund(wousd.address);
+    const gov = await curvePoolBooster.governor();
+    const sGov = await ethers.provider.getSigner(gov);
+
+    // Deal OETH to pool booster
+    await ousd
+      .connect(woethSigner)
+      .transfer(curvePoolBooster.address, parseUnits("10"));
+    expect(await ousd.balanceOf(curvePoolBooster.address)).to.equal(
+      parseUnits("10")
+    );
+
+    await curvePoolBooster
+      .connect(sGov)
+      .rescueToken(ousd.address, multichainStrategistAddr);
+    expect(await ousd.balanceOf(curvePoolBooster.address)).to.equal(0);
+  });
+
+  it("Should revert if receiver is invalid", async () => {
+    const { curvePoolBooster, ousd } = fixture;
+    const gov = await curvePoolBooster.governor();
+    const sGov = await ethers.provider.getSigner(gov);
+
+    await expect(
+      curvePoolBooster.connect(sGov).rescueToken(ousd.address, addresses.zero)
+    ).to.be.revertedWith("Invalid receiver");
+
+    await expect(
+      curvePoolBooster.connect(sGov).rescueETH(addresses.zero)
+    ).to.be.revertedWith("Invalid receiver");
+  });
+
+  // --- Setters ---
+  it("Should set campaign id", async () => {
+    const { curvePoolBooster } = fixture;
+    const { multichainStrategistAddr } = await getNamedAccounts();
+    const sStrategist = await ethers.provider.getSigner(
+      multichainStrategistAddr
+    );
+    expect(await curvePoolBooster.campaignId()).to.equal(0);
+
+    await curvePoolBooster.connect(sStrategist).setCampaignId(12);
+    expect(await curvePoolBooster.campaignId()).to.equal(12);
+  });
+
+  it("Should set fee and fee collector", async () => {
+    const { curvePoolBooster, josh } = fixture;
+    const gov = await curvePoolBooster.governor();
+    const sGov = await ethers.provider.getSigner(gov);
+    expect(await curvePoolBooster.fee()).to.equal(0);
+
+    await curvePoolBooster.connect(sGov).setFee(100);
+    expect(await curvePoolBooster.fee()).to.equal(100);
+
+    expect(await curvePoolBooster.feeCollector()).not.to.equal(josh.address);
+    await curvePoolBooster.connect(sGov).setFeeCollector(josh.address);
+    expect(await curvePoolBooster.feeCollector()).to.equal(josh.address);
+  });
+
   it("Should revert if fee too high", async () => {
     const { curvePoolBooster } = fixture;
     const gov = await curvePoolBooster.governor();
@@ -384,5 +434,51 @@ describe("ForkTest: CurvePoolBooster", function () {
     await expect(
       curvePoolBooster.connect(sGov).setFee(10000)
     ).to.be.revertedWith("Fee too high");
+  });
+
+  it("Should set Campaign Remote Manager", async () => {
+    const { curvePoolBooster, josh } = fixture;
+    const gov = await curvePoolBooster.governor();
+    const sGov = await ethers.provider.getSigner(gov);
+    expect(await curvePoolBooster.campaignRemoteManager()).to.equal(
+      addresses.mainnet.CampaignRemoteManager
+    );
+
+    await curvePoolBooster.connect(sGov).setCampaignRemoteManager(josh.address);
+
+    expect(await curvePoolBooster.campaignRemoteManager()).to.equal(
+      josh.address
+    );
+  });
+
+  it("Should revert if campaign remote manager is invalid", async () => {
+    const { curvePoolBooster } = fixture;
+    const gov = await curvePoolBooster.governor();
+    const sGov = await ethers.provider.getSigner(gov);
+
+    await expect(
+      curvePoolBooster.connect(sGov).setCampaignRemoteManager(addresses.zero)
+    ).to.be.revertedWith("Invalid campaignRemoteManager");
+  });
+
+  it("Should set Votemarket address", async () => {
+    const { curvePoolBooster, josh } = fixture;
+    const gov = await curvePoolBooster.governor();
+    const sGov = await ethers.provider.getSigner(gov);
+    expect(await curvePoolBooster.votemarket()).to.equal(addresses.votemarket);
+
+    await curvePoolBooster.connect(sGov).setVotemarket(josh.address);
+
+    expect(await curvePoolBooster.votemarket()).to.equal(josh.address);
+  });
+
+  it("Should revert if votemarket is invalid", async () => {
+    const { curvePoolBooster } = fixture;
+    const gov = await curvePoolBooster.governor();
+    const sGov = await ethers.provider.getSigner(gov);
+
+    await expect(
+      curvePoolBooster.connect(sGov).setVotemarket(addresses.zero)
+    ).to.be.revertedWith("Invalid votemarket");
   });
 });
