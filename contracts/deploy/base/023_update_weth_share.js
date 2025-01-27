6bdb59ee4acb30d9c810a937f7ad5c22d9c7a076
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-01-27 14:48:55 +0530

    Fix script

--- contracts/deploy/base/023_update_weth_share.js
+++ contracts/deploy/base/023_update_weth_share.js
@@ -20,7 +20,7 @@ module.exports = deployOnBaseWithGuardian(
           // 1. Set WETH share to be 1% to 15%
           contract: cAMOStrategy,
           signature: "setAllowedPoolWethShareInterval(uint256,uint256)",
-          args: [utils.parseUnits("1", 18), utils.parseUnits("15", 18)],
+          args: [utils.parseUnits("0.01", 18), utils.parseUnits("0.15", 18)],
         },
       ],
     };
