46ba29975735c97da89021223cb90ccf75a6e223
Shah <10547529+shahthepro@users.noreply.github.com>
2025-02-06 22:30:07 +0530

    Fix L2 deploy tooling network name on fork (#2385)
    * Fix L2 deploy tooling network name on fork
    
    * Remove unused variables
    
    * Also account for non-standalone node mode

--- contracts/utils/deploy-l2.js
+++ contracts/utils/deploy-l2.js
@@ -6,12 +6,10 @@ const {
   advanceTime,
   advanceBlocks,
 } = require("../test/helpers");
-const addresses = require("./addresses");
 const {
   deployWithConfirmation,
   withConfirmation,
   impersonateGuardian,
-  handleTransitionGovernance,
   buildGnosisSafeJson,
   constructContractMethod,
 } = require("./deploy");
@@ -36,13 +34,23 @@ function log(msg, deployResult = null) {
   }
 }
 
+function getNetworkName() {
+  if (isForkTest) {
+    return "hardhat";
+  } else if (isFork) {
+    return "localhost";
+  } else {
+    return process.env.NETWORK_NAME || "mainnet";
+  }
+}
+
 async function buildAndSimulateTimelockOperations(
   deployName,
   propDesc,
   propArgs
 ) {
   console.log("Building and simulating timelock operations for", deployName);
-  const networkName = process.env.FORK_NETWORK_NAME;
+  const networkName = getNetworkName();
   const { guardianAddr, timelockAddr } = await getNamedAccounts();
 
   const timelock = await ethers.getContractAt(
@@ -125,7 +133,7 @@ async function simulateTimelockOperations(deployName) {
     return false;
   }
 
-  const networkName = process.env.FORK_NETWORK_NAME;
+  const networkName = getNetworkName();
 
   const scheduleFilePath = path.resolve(
     __dirname,
