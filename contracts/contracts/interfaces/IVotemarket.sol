6eb522a5bc4771034d4a6fca06ce23be209d3d8f
Clément <clemmoller@gmail.com>
2025-02-10 22:19:43 +0100

    prettier

--- contracts/contracts/interfaces/IVotemarket.sol
+++ contracts/contracts/interfaces/IVotemarket.sol
@@ -59,16 +59,33 @@ interface IVotemarket {
     );
     event CampaignUpgradeQueued(uint256 campaignId, uint256 epoch);
     event CampaignUpgraded(uint256 campaignId, uint256 epoch);
-    event Claim(uint256 indexed campaignId, address indexed account, uint256 amount, uint256 fee, uint256 epoch);
+    event Claim(
+        uint256 indexed campaignId,
+        address indexed account,
+        uint256 amount,
+        uint256 fee,
+        uint256 epoch
+    );
 
     function CLAIM_WINDOW_LENGTH() external view returns (uint256);
+
     function CLOSE_WINDOW_LENGTH() external view returns (uint256);
+
     function EPOCH_LENGTH() external view returns (uint256);
+
     function MAX_ADDRESSES_PER_CAMPAIGN() external view returns (uint256);
+
     function MINIMUM_PERIODS() external view returns (uint8);
+
     function ORACLE() external view returns (address);
+
     function acceptGovernance() external;
-    function addressesByCampaignId(uint256) external view returns (uint256 _spacer);
+
+    function addressesByCampaignId(uint256)
+        external
+        view
+        returns (uint256 _spacer);
+
     function campaignById(uint256)
         external
         view
@@ -85,18 +102,35 @@ interface IVotemarket {
             uint256 endTimestamp,
             address hook
         );
+
     function campaignCount() external view returns (uint256);
+
     function campaignUpgradeById(uint256, uint256)
         external
         view
-        returns (uint8 numberOfPeriods, uint256 totalRewardAmount, uint256 maxRewardPerVote, uint256 endTimestamp);
-    function claim(uint256 campaignId, uint256 epoch, bytes memory hookData, address receiver)
-        external
-        returns (uint256 claimed);
-    function claim(uint256 campaignId, address account, uint256 epoch, bytes memory hookData)
-        external
-        returns (uint256 claimed);
+        returns (
+            uint8 numberOfPeriods,
+            uint256 totalRewardAmount,
+            uint256 maxRewardPerVote,
+            uint256 endTimestamp
+        );
+
+    function claim(
+        uint256 campaignId,
+        uint256 epoch,
+        bytes memory hookData,
+        address receiver
+    ) external returns (uint256 claimed);
+
+    function claim(
+        uint256 campaignId,
+        address account,
+        uint256 epoch,
+        bytes memory hookData
+    ) external returns (uint256 claimed);
+
     function closeCampaign(uint256 campaignId) external;
+
     function createCampaign(
         uint256 chainId,
         address gauge,
@@ -109,42 +143,103 @@ interface IVotemarket {
         address hook,
         bool isWhitelist
     ) external returns (uint256 campaignId);
+
     function currentEpoch() external view returns (uint256);
+
     function customFeeByManager(address) external view returns (uint256);
+
     function fee() external view returns (uint256);
+
     function feeCollector() external view returns (address);
+
     function futureGovernance() external view returns (address);
-    function getAddressesByCampaign(uint256 campaignId) external view returns (address[] memory);
-    function getCampaign(uint256 campaignId) external view returns (Campaign memory);
-    function getCampaignUpgrade(uint256 campaignId, uint256 epoch) external view returns (CampaignUpgrade memory);
-    function getPeriodPerCampaign(uint256 campaignId, uint256 epoch) external view returns (Period memory);
-    function getRemainingPeriods(uint256 campaignId, uint256 epoch) external view returns (uint256 periodsLeft);
+
+    function getAddressesByCampaign(uint256 campaignId)
+        external
+        view
+        returns (address[] memory);
+
+    function getCampaign(uint256 campaignId)
+        external
+        view
+        returns (Campaign memory);
+
+    function getCampaignUpgrade(uint256 campaignId, uint256 epoch)
+        external
+        view
+        returns (CampaignUpgrade memory);
+
+    function getPeriodPerCampaign(uint256 campaignId, uint256 epoch)
+        external
+        view
+        returns (Period memory);
+
+    function getRemainingPeriods(uint256 campaignId, uint256 epoch)
+        external
+        view
+        returns (uint256 periodsLeft);
+
     function governance() external view returns (address);
-    function increaseTotalRewardAmount(uint256 campaignId, uint256 totalRewardAmount) external;
+
+    function increaseTotalRewardAmount(
+        uint256 campaignId,
+        uint256 totalRewardAmount
+    ) external;
+
     function isClosedCampaign(uint256) external view returns (bool);
+
     function isProtected(address) external view returns (bool);
+
     function manageCampaign(
         uint256 campaignId,
         uint8 numberOfPeriods,
         uint256 totalRewardAmount,
         uint256 maxRewardPerVote
     ) external;
+
     function periodByCampaignId(uint256, uint256)
         external
         view
-        returns (uint256 rewardPerPeriod, uint256 rewardPerVote, uint256 leftover, bool updated);
+        returns (
+            uint256 rewardPerPeriod,
+            uint256 rewardPerVote,
+            uint256 leftover,
+            bool updated
+        );
+
     function recipients(address) external view returns (address);
+
     function remote() external view returns (address);
+
     function setCustomFee(address _account, uint256 _fee) external;
+
     function setFee(uint256 _fee) external;
+
     function setFeeCollector(address _feeCollector) external;
+
     function setIsProtected(address _account, bool _isProtected) external;
+
     function setRecipient(address _recipient) external;
+
     function setRecipient(address _account, address _recipient) external;
+
     function setRemote(address _remote) external;
-    function totalClaimedByAccount(uint256, uint256, address) external view returns (uint256);
+
+    function totalClaimedByAccount(
+        uint256,
+        uint256,
+        address
+    ) external view returns (uint256);
+
     function totalClaimedByCampaignId(uint256) external view returns (uint256);
+
     function transferGovernance(address _futureGovernance) external;
-    function updateEpoch(uint256 campaignId, uint256 epoch, bytes memory hookData) external returns (uint256 epoch_);
+
+    function updateEpoch(
+        uint256 campaignId,
+        uint256 epoch,
+        bytes memory hookData
+    ) external returns (uint256 epoch_);
+
     function whitelistOnly(uint256) external view returns (bool);
 }
