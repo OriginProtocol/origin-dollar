affda22ac2d17f320d42fa41ce83d343ffbf2b31
Nicholas Addison <nick@addisonbrown.com.au>
2025-02-18 12:50:30 +1100

    Upgrade Base Vault with Curve AMO strategy

--- contracts/deploy/base/027_base_curve_amo_upgrade.js
+++ contracts/deploy/base/027_base_curve_amo_upgrade.js
@@ -7,10 +7,13 @@ module.exports = deployOnBase(
     deployName: "027_base_curve_amo_upgrade",
   },
   async ({ ethers }) => {
-    // Deploy Base Curve AMO proxy
+    // Get the SuperOETH, Vault and Curve AMO contracts
     const cOETHbProxy = await ethers.getContract("OETHBaseProxy");
     const cOETHbVaultProxy = await ethers.getContract("OETHBaseVaultProxy");
-
+    const cOETHbVault = await ethers.getContractAt(
+      "IVault",
+      cOETHbVaultProxy.address
+    );
     const cOETHBaseCurveAMOProxy = await ethers.getContract(
       "OETHBaseCurveAMOProxy"
     );
@@ -29,13 +32,36 @@ module.exports = deployOnBase(
       ]
     );
 
+    // Deploy new Vault Core implementation
+    const dOETHbVaultCore = await deployWithConfirmation("OETHBaseVaultCore", [
+      addresses.base.WETH,
+    ]);
+
+    // Deploy new Vault Admin implementation
+    const dOETHbVaultAdmin = await deployWithConfirmation(
+      "OETHBaseVaultAdmin",
+      [addresses.base.WETH]
+    );
+
     return {
       actions: [
-        // Upgrade the Base Curve AMO Strategy implementation
+        // 1. Upgrade the Base Curve AMO Strategy implementation
         {
           contract: cOETHBaseCurveAMOProxy,
           signature: "upgradeTo(address)",
           args: [dOETHBaseCurveAMOImpl.address],
+        },
+        // 2. Upgrade VaultCore
+        {
+          contract: cOETHbVaultProxy,
+          signature: "upgradeTo(address)",
+          args: [dOETHbVaultCore.address],
+        },
+        // 3. Upgrade VaultAdmin
+        {
+          contract: cOETHbVault,
+          signature: "setAdminImpl(address)",
+          args: [dOETHbVaultAdmin.address],
         },
       ],
     };
