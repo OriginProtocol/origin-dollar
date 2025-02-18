c6ded55bc958a670504a4b6bd9f299ac53a25e35
Clément <clemmoller@gmail.com>
2025-02-18 23:37:38 +0100

    fix: import IERC20Metadata.

--- contracts/contracts/token/WOETH.sol
+++ contracts/contracts/token/WOETH.sol
@@ -5,6 +5,7 @@ import { ERC4626 } from "../../lib/openzeppelin/contracts/token/ERC20/extensions
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 import { StableMath } from "../utils/StableMath.sol";
 import { Governable } from "../governance/Governable.sol";
