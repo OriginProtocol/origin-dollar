485879f87dd908ae41dabf5306088f09840368eb
Nicholas Addison <nick@addisonbrown.com.au>
2025-03-06 16:43:45 +1100

    Updated deploy script number

--- contracts/deploy/mainnet/125_oeth_vault_upgrade.js
+++ contracts/deploy/mainnet/125_oeth_vault_upgrade.js
@@ -3,7 +3,7 @@ const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
 
 module.exports = deploymentWithGovernanceProposal(
   {
-    deployName: "124_oeth_vault_upgrade",
+    deployName: "125_oeth_vault_upgrade",
     forceDeploy: false,
     //forceSkip: true,
     reduceQueueTime: true,
