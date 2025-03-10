82d26e4a800721922e633ff8635cfe966c098dde
Clément <clemmoller@gmail.com>
2025-03-10 19:29:44 +0100

    feat: Add new yield farming transaction batch for OsHOG and OsGHOG pools

---
+++ contracts/deploy/sonic/014_yf_tb.js
@@ -0,0 +1,66 @@
+const { deployOnSonic } = require("../../utils/deploy-l2");
+const addresses = require("../../utils/addresses");
+const { createPoolBoosterSonic } = require("../../utils/deploy.js");
+
+module.exports = deployOnSonic(
+  {
+    deployName: "014_yf_tb",
+  },
+  async ({ ethers }) => {
+    // ---------------------------------------------------------------------------------------------------------
+    // ---
+    // --- Contracts
+    // ---
+    // ---------------------------------------------------------------------------------------------------------
+    const cOSonic = await ethers.getContractAt(
+      "OSonic",
+      addresses.sonic.OSonicProxy
+    );
+
+    const cPoolBoosterFactorySwapxSingle = await ethers.getContract(
+      "PoolBoosterFactorySwapxSingle"
+    );
+
+    const SALT = ethers.BigNumber.from("1741630610"); // epoch as Monday 10th Mar 2025 6PM UTC
+
+    // ---------------------------------------------------------------------------------------------------------
+    // ---
+    // --- PoolBooster SwapxSingle
+    // ---
+    // ---------------------------------------------------------------------------------------------------------
+    const { actions: actionsSingle } = await createPoolBoosterSonic({
+      cOSonic,
+      factoryContract: cPoolBoosterFactorySwapxSingle,
+      pools: ["SwapX.OsHOG", "SwapX.OsGHOG"],
+      salt: SALT,
+      type: "Single",
+    });
+
+    // ---------------------------------------------------------------------------------------------------------
+    // ---
+    // --- Governance Actions
+    // ---
+    // ---------------------------------------------------------------------------------------------------------
+    return {
+      actions: [
+        {
+          // Plateform: SwapX
+          // Protocol: Moon Bay
+          // From: VolatileV1 AMM - MOON/OS --> To: EOA
+          contract: cOSonic,
+          signature: "undelegateYield(address)",
+          args: [addresses.sonic.SwapX.OsHOG.pool],
+        },
+        {
+          // Plateform: SwapX
+          // Protocol: Moon Bay
+          // From: VolatileV1 AMM - MOON/OS --> To: EOA
+          contract: cOSonic,
+          signature: "undelegateYield(address)",
+          args: [addresses.sonic.SwapX.OsGHOG.pool],
+        },
+        ...actionsSingle,
+      ],
+    };
+  }
+);
