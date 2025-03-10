8de84abcb92e75f8c26acfff14672a3a352f248a
Nicholas Addison <nick@addisonbrown.com.au>
2025-03-11 01:10:36 +1100

    Added pending withdrawals to sonicStaking Hardhat task

--- contracts/utils/sonic.js
+++ contracts/utils/sonic.js
@@ -36,6 +36,12 @@ async function snapSonicStaking(taskArguments) {
   const strategyBalance = await sonicStakingStrategy.checkBalance(ws.address, {
     blockTag,
   });
+  const pendingWithdrawals = await sonicStakingStrategy.pendingWithdrawals({
+    blockTag,
+  });
+  const pendingWithdrawalsPercentage = pendingWithdrawals
+    .mul(10000)
+    .div(strategyBalance);
 
   let totalStaked = ethers.BigNumber.from(0);
   let totalPendingRewards = ethers.BigNumber.from(0);
@@ -56,8 +62,10 @@ async function snapSonicStaking(taskArguments) {
     );
     totalPendingRewards = totalPendingRewards.add(pendingRewards);
 
-    console.log(`   Staked amount      : ${formatUnits(stakedAmount, 18)}`);
-    console.log(`   Pending rewards    : ${formatUnits(pendingRewards, 18)}`);
+    console.log(`   Staked amount          : ${formatUnits(stakedAmount, 18)}`);
+    console.log(
+      `   Pending rewards        : ${formatUnits(pendingRewards, 18)}`
+    );
   }
 
   const stakedPercent = totalStaked.mul(10000).div(strategyBalance);
@@ -65,18 +73,26 @@ async function snapSonicStaking(taskArguments) {
     .mul(10000)
     .div(strategyBalance);
   console.log(
-    `\nTotal Staked          : ${formatUnits(totalStaked, 18)} ${formatUnits(
-      stakedPercent,
-      2
-    )}%`
+    `\nTotal Staked              : ${formatUnits(
+      totalStaked,
+      18
+    )} ${formatUnits(stakedPercent, 2)}%`
   );
   console.log(
-    `Total pending rewards : ${formatUnits(
-      pendingRewardsPercentage,
+    `Total pending withdrawals : ${formatUnits(
+      pendingWithdrawals,
+      18
+    )} ${formatUnits(pendingWithdrawalsPercentage, 2)}%`
+  );
+  console.log(
+    `Total pending rewards     : ${formatUnits(
+      totalPendingRewards,
       18
     )} ${formatUnits(pendingRewardsPercentage, 2)}%`
   );
-  console.log(`Strategy balance      : ${formatUnits(strategyBalance, 18)}`);
+  console.log(
+    `Strategy balance          : ${formatUnits(strategyBalance, 18)}`
+  );
 }
 
 async function undelegateValidator({ id, amount }) {
