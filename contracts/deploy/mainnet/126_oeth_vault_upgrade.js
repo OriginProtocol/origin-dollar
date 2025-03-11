83c5c81a7a8f86d9c1549ae8e5282cc2a24b0310
Nicholas Addison <nick@addisonbrown.com.au>
2025-03-11 14:28:07 +1100

    Changed mainnet deploy number after merge with master

--- contracts/deploy/mainnet/126_oeth_vault_upgrade.js
+++ contracts/deploy/mainnet/126_oeth_vault_upgrade.js
@@ -3,7 +3,7 @@ const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
 
 module.exports = deploymentWithGovernanceProposal(
   {
-    deployName: "125_oeth_vault_upgrade",
+    deployName: "126_oeth_vault_upgrade",
     forceDeploy: false,
     //forceSkip: true,
     reduceQueueTime: true,
