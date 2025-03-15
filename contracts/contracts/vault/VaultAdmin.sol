cf6d7d80ef7c2886d085cb4d61aea14168789796
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-03-15 12:39:09 +0530

    Take token decimals into consideration in `removeAsset`

--- contracts/contracts/vault/VaultAdmin.sol
+++ contracts/contracts/vault/VaultAdmin.sol
@@ -389,10 +389,15 @@ contract VaultAdmin is VaultStorage {
      */
     function removeAsset(address _asset) external onlyGovernor {
         require(assets[_asset].isSupported, "Asset not supported");
+
+        // 1e13 for 18 decimals. And 10 for 6 decimals
+        uint256 maxDustBalance = uint256(1e13).scaleBy(
+            assets[_asset].decimals,
+            18
+        );
+
         require(
-            // TODO: Temporary change since Morpho has liquidity issues.
-            // Revert before merging
-            IVault(address(this)).checkBalance(_asset) <= 1e20,
+            IVault(address(this)).checkBalance(_asset) <= maxDustBalance,
             "Vault still holds asset"
         );
 
