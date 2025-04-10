3c25cee2b2c6e745f68f62a67a16511101455ac1
Domen Grabec <grabec@gmail.com>
2025-04-10 12:11:34 +0200

    switch to BUSL

--- contracts/contracts/token/WOETH.sol
+++ contracts/contracts/token/WOETH.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
 import { ERC4626 } from "../../lib/openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
