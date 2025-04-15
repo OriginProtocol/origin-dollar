84214c2a49c72f02745bb8d6d0c2c0eb5d96e6b6
Daniel Von Fange <daniel@leancoder.com>
2025-04-15 10:28:58 -0400

    BUSL, more

--- contracts/contracts/vault/OETHVaultCore.sol
+++ contracts/contracts/vault/OETHVaultCore.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
