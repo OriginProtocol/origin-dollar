26e1985b6e04e83526888d6590f9da9e7360ef24
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-04-22 17:50:30 +0530

    Change license to BUSL-1.1

--- contracts/contracts/mocks/MockOracleWeightedPool.sol
+++ contracts/contracts/mocks/MockOracleWeightedPool.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
 import { Variable, OracleAverageQuery, IOracleWeightedPool } from "../interfaces/balancer/IOracleWeightedPool.sol";
