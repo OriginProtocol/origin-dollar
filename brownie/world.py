9e83e6fb25aa03ffd2bd05de0c1bd302cea067fd
Nick Addison <nick@addisonbrown.com.au>
2025-04-25 11:32:44 +1000

    Run log 24 Apr - OUSD Curve AMO deposit (#2494)
    * Run log OUSD Curve AMO deposit
    
    * Updated comments in run log
    
    * Added run log script to burn OUSD from the Curve pool

--- brownie/world.py
+++ brownie/world.py
@@ -51,6 +51,7 @@ aave_strat = load_contract('aave_strat', AAVE_STRAT)
 comp_strat = load_contract('comp_strat', COMP_STRAT)
 convex_strat = load_contract('convex_strat', CONVEX_STRAT)
 ousd_meta_strat = load_contract('ousd_metastrat', OUSD_METASTRAT)
+ousd_curve_amo_strat = load_contract('ousd_curve_amo_strat', OUSD_CURVE_AMO_STRAT)
 morpho_comp_strat = load_contract('morpho_comp_strat', MORPHO_COMP_STRAT)
 morpho_aave_strat = load_contract('morpho_aave_strat', MORPHO_AAVE_STRAT)
 lusd_3pool_strat = load_contract('lusd_3pool_strat', LUSD_3POOL_STRAT)
@@ -64,6 +65,7 @@ lido_withdrawal_strat = load_contract('lido_withdrawal_strat', OETH_LIDO_WITHDRA
 
 ousd_metapool = load_contract("ousd_metapool", OUSD_METAPOOL)
 threepool = load_contract("threepool_swap", THREEPOOL)
+ousd_curve_pool = load_contract("ousd_curve_pool", OUSD_CURVE_POOL)
 
 aave_incentives_controller = load_contract('aave_incentives_controller', '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5')
 stkaave = load_contract('stkaave', '0x4da27a545c0c5B758a6BA100e3a049001de870f5')
