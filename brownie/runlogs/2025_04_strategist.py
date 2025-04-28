849af46049aaf0e00a9468df951042e5c6d4ff63
Nicholas Addison <nick@addisonbrown.com.au>
2025-04-29 09:14:34 +1000

    Run log deposit 256 WETH to 3rd native staking strategy

--- brownie/runlogs/2025_04_strategist.py
+++ brownie/runlogs/2025_04_strategist.py
@@ -119,3 +119,36 @@ def main():
     print("Pool Total  ", "{:.6f}".format(totalPool / 10**18))
     print("OUSD/USDC Curve prices before and after", "{:.6f}".format(price_before / 10**7), "{:.6f}".format(price_after / 10**7))
 
+
+# -------------------------------------
+# Apr 29, 2025 - Deposit to third native staking strategy
+# -------------------------------------
+from world import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+    # Before
+    txs.append(vault_oeth_core.rebase(std))
+    txs.append(oeth_vault_value_checker.takeSnapshot(std))
+
+    # Deposit WETH to Third Native Staking Strategy
+    txs.append(
+      vault_oeth_admin.depositToStrategy(
+        OETH_NATIVE_STAKING_3_STRAT, 
+        [WETH], 
+        # 8 validator
+        [256 * 10**18],
+        std
+      )
+    )
+
+    # After
+    vault_change = vault_oeth_core.totalValue() - oeth_vault_value_checker.snapshots(STRATEGIST)[0]
+    supply_change = oeth.totalSupply() - oeth_vault_value_checker.snapshots(STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(oeth_vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), std))
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
