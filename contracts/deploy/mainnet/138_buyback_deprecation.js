50466cf1c274feba3d6aeb61e19cece3b7b9f9a4
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-05-29 18:04:34 +0530

    Add deployment file to deprecate buyback

---
+++ contracts/deploy/mainnet/138_buyback_deprecation.js
@@ -0,0 +1,94 @@
+const addresses = require("../../utils/addresses");
+const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
+
+module.exports = deploymentWithGovernanceProposal(
+  {
+    deployName: "138_buyback_deprecation",
+    forceDeploy: false,
+    // forceSkip: true,
+    // reduceQueueTime: true,
+    deployerIsProposer: false,
+  },
+  async () => {
+    const cLegacyBuyback = await ethers.getContractAt(
+      "OUSDBuyback",
+      "0x77314eb392b2be47c014cde0706908b3307ad6a9"
+    );
+    const ogv = await ethers.getContractAt("IERC20", addresses.mainnet.OGV);
+    const ogvInLegacyBuyback = await ogv.balanceOf(cLegacyBuyback.address);
+
+    console.log("OGV in Legacy Buyback:", ogvInLegacyBuyback.toString());
+
+    const cOUSDBuybackProxy = await ethers.getContract("BuybackProxy");
+    const cOUSDBuyback = await ethers.getContractAt(
+      "OUSDBuyback",
+      cOUSDBuybackProxy.address
+    );
+    const cOETHBuybackProxy = await ethers.getContract("OETHBuybackProxy");
+    const cOETHBuyback = await ethers.getContractAt(
+      "OETHBuyback",
+      cOETHBuybackProxy.address
+    );
+    const cARMBuybackProxy = await ethers.getContract("ARMBuybackProxy");
+    const cARMBuyback = await ethers.getContractAt(
+      "ARMBuyback",
+      cARMBuybackProxy.address
+    );
+
+    const cOUSDVaultProxy = await ethers.getContract("VaultProxy");
+    const cOUSDVault = await ethers.getContractAt(
+      "IVault",
+      cOUSDVaultProxy.address
+    );
+    const cOETHVaultProxy = await ethers.getContract("OETHVaultProxy");
+    const cOETHVault = await ethers.getContractAt(
+      "IVault",
+      cOETHVaultProxy.address
+    );
+
+    return {
+      name: "Deprecate Buyback",
+      actions: [
+        {
+          // Send all OUSD fees to the Multichain Strategist
+          contract: cOUSDVault,
+          signature: "setTrusteeAddress(address)",
+          args: [addresses.multichainStrategist],
+        },
+        {
+          // Send all OETH fees to the Multichain Strategist
+          contract: cOETHVault,
+          signature: "setTrusteeAddress(address)",
+          args: [addresses.multichainStrategist],
+        },
+        // Transfer governance from all buyback contracts to the Multichain Strategist
+        {
+          contract: cOUSDBuyback,
+          signature: "transferGovernance(address)",
+          args: [addresses.multichainStrategist],
+        },
+        {
+          contract: cOETHBuyback,
+          signature: "transferGovernance(address)",
+          args: [addresses.multichainStrategist],
+        },
+        {
+          contract: cARMBuyback,
+          signature: "transferGovernance(address)",
+          args: [addresses.multichainStrategist],
+        },
+        {
+          contract: cLegacyBuyback,
+          signature: "transferGovernance(address)",
+          args: [addresses.multichainStrategist],
+        },
+        // Drain OGV from Legacy Buyback contract
+        {
+          contract: cLegacyBuyback,
+          signature: "transferToken(address,uint256)",
+          args: [addresses.mainnet.OGV, ogvInLegacyBuyback],
+        },
+      ],
+    };
+  }
+);
