7103796f9a33e5cddc379d5a11ec8069d21597ec
Nicholas Addison <nick@addisonbrown.com.au>
2025-06-24 09:47:38 +1000

    Add Sonic contracts to Brownie

--- brownie/world_sonic.py
+++ brownie/world_sonic.py
@@ -6,3 +6,6 @@ wos = load_contract('ERC20', WOS)
 
 vault_admin = load_contract('vault_admin', OS_VAULT_PROXY_ADDRESS)
 vault_core = load_contract('vault_core', OS_VAULT_PROXY_ADDRESS)
+vault_value_checker = load_contract('vault_value_checker', OS_VAULT_VALUE_CHECKER)
+
+swapx_amo_strat = load_contract('swapx_amo_strat', SWAPX_AMO_STRATEGY)
