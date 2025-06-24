adf67de7d5e2adc2a078ff24ba8b72c13dbec575
Clément <clemmoller@gmail.com>
2025-06-24 14:09:11 +0200

    feat: add deployment script.

---
+++ contracts/deploy/mainnet/143_ousd_upgrade_EIP7702.js
@@ -0,0 +1,35 @@
+const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
+
+module.exports = deploymentWithGovernanceProposal(
+  {
+    deployName: "143_ousd_upgrade_EIP7702",
+    forceDeploy: false,
+    //forceSkip: true,
+    reduceQueueTime: true,
+    deployerIsProposer: false,
+    proposalId:
+      "",
+  },
+  async ({ deployWithConfirmation }) => {
+    // Deployer Actions
+    // ----------------
+
+    // 1. Deploy new OUSD implementation without storage slot checks
+    const dOUSD = await deployWithConfirmation("OUSD", [], "OUSD", true);
+    const cOUSDProxy = await ethers.getContract("OUSDProxy");
+
+    // Governance Actions
+    // ----------------
+    return {
+      name: "Upgrade OUSD token contract",
+      actions: [
+        // 1. Upgrade the OUSD proxy to the new implementation
+        {
+          contract: cOUSDProxy,
+          signature: "upgradeTo(address)",
+          args: [dOUSD.address],
+        },
+      ],
+    };
+  }
+);
