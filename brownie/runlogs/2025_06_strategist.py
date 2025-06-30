c587cce636188fff6aafd1de82e8a9c6ae2dd7b5
Nick Addison <nick@addisonbrown.com.au>
2025-07-01 08:50:55 +1000

    Run log withdrawFromSFC and deposit to SwapX AMO (#2561)

--- brownie/runlogs/2025_06_strategist.py
+++ brownie/runlogs/2025_06_strategist.py
@@ -553,9 +553,9 @@ def main():
     txs.append(vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), {'from': SONIC_STRATEGIST}))
 
     print("-----")
-    print("Profit", "{:.6f}".format(profit / 10**18), profit)
-    print("OETH supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
-    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("Profit in wS", "{:.6f}".format(profit / 10**18), profit)
+    print("OS supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("OS Vault change ", "{:.6f}".format(vault_change / 10**18), vault_change)
     print("-----")
 
 
@@ -727,3 +727,43 @@ def main():
 
     # Unwrap WETH
     txs.append(weth.withdraw(expected_oethb, from_strategist))
+
+# -------------------------------------
+# June 30, 2025 - Claim S withdrawal and deposit wS to SwapX AMO on Sonic
+# -------------------------------------
+from world_sonic import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+
+    amount = 148000 * 10**18
+
+    # Before
+    txs.append(vault_core.rebase({'from': SONIC_STRATEGIST}))
+    txs.append(vault_value_checker.takeSnapshot({'from': SONIC_STRATEGIST}))
+
+    withdraw_id = 42
+    txs.append(sonic_staking_strat.withdrawFromSFC(withdraw_id, {'from': SONIC_STRATEGIST}))
+
+    # Deposit wS to SwapX AMO
+    txs.append(
+      vault_admin.depositToStrategy(
+        SWAPX_AMO_STRATEGY, 
+        [WS_SONIC],
+        [amount],
+        {'from': SONIC_STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(SONIC_STRATEGIST)[0]
+    supply_change = os.totalSupply() - vault_value_checker.snapshots(SONIC_STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (10 * 10**18), vault_change, (10 * 10**18), {'from': SONIC_STRATEGIST}))
+
+    print("-----")
+    print("Profit in wS", "{:.6f}".format(profit / 10**18), profit)
+    print("OS supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("OS Vault change ", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
--- brownie/world_sonic.py
+++ brownie/world_sonic.py
@@ -9,3 +9,4 @@ vault_core = load_contract('vault_core', OS_VAULT_PROXY_ADDRESS)
 vault_value_checker = load_contract('vault_value_checker', OS_VAULT_VALUE_CHECKER)
 
 swapx_amo_strat = load_contract('swapx_amo_strat', SWAPX_AMO_STRATEGY)
+sonic_staking_strat = load_contract('sonic_staking_strat', SONIC_STAKING_STRATEGY)
