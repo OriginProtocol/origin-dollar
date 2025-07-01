255d7f7efeb156e4a7af634a553b7ac5ac7270da
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-07-01 18:23:14 +0530

    Add hardcoded gaslimit

--- contracts/scripts/defender-actions/claimBribes.js
+++ contracts/scripts/defender-actions/claimBribes.js
@@ -11,14 +11,19 @@ const log = require("../../utils/logger")("action:claimBribes");
 
 // ClaimBribesSafeModule1 for Coinbase AERO Locker Safe
 const COINBASE_AERO_LOCKER_MODULE =
-  "0x60D3D6eC213d84DEa193dbd79673340061178893";
+  "0x60d3d6ec213d84dea193dbd79673340061178893";
 
 // ClaimBribesSafeModule3 for Old Guardian Safe
-const OLD_GUARDIAN_MODULE = "0x26179Ada0f7cb714c11A8190e1f517988c28E759";
+const OLD_GUARDIAN_MODULE = "0x26179ada0f7cb714c11a8190e1f517988c28e759";
 
 const moduleLabels = {
   [COINBASE_AERO_LOCKER_MODULE]: "Coinbase AERO Locker Safe",
   [OLD_GUARDIAN_MODULE]: "Old Guardian Safe",
+};
+
+const batchSizes = {
+  [COINBASE_AERO_LOCKER_MODULE]: 50,
+  [OLD_GUARDIAN_MODULE]: 50,
 };
 
 const MODULE_ABI = [
@@ -42,7 +47,7 @@ const handler = async (event) => {
   for (const moduleAddr of modules) {
     const module = new ethers.Contract(moduleAddr, MODULE_ABI, signer);
 
-    log(`Claiming bribes from ${moduleLabels[moduleAddr]}`);
+    log(`Claiming bribes from ${moduleLabels[moduleAddr.toLowerCase()]}`);
     await manageNFTsOnModule(module, signer);
     await claimBribesFromModule(module, signer);
   }
@@ -66,7 +71,7 @@ async function claimBribesFromModule(module, signer) {
   const nftIdsLength = (
     await module.connect(signer).getNFTIdsLength()
   ).toNumber();
-  const batchSize = 25;
+  const batchSize = batchSizes[module.address.toLowerCase()] || 50;
   const batchCount = Math.ceil(nftIdsLength / batchSize);
 
   log(`Found ${nftIdsLength} NFTs on the module`);
@@ -76,7 +81,9 @@ async function claimBribesFromModule(module, signer) {
     const start = i * batchSize;
     const end = Math.min(start + batchSize, nftIdsLength);
 
-    const tx = await module.connect(signer).claimBribes(start, end, true);
+    const tx = await module.connect(signer).claimBribes(start, end, true, {
+      gasLimit: 20000000,
+    });
     await logTxDetails(tx, `claimBribes (batch ${i + 1} of ${batchCount})`);
   }
 }
