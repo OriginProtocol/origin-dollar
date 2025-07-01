ab21f096ea0363f0f7ec549115d42839e6843e51
Clément <55331875+clement-ux@users.noreply.github.com>
2025-07-01 21:14:45 +0200

    Make OUSD EIP-7702 compatible and simplify yield delegation process. (#2543)
    * feat: make OUSD EIP-7702 compatible.
    
    * feat: add deployment script.
    
    * prettier
    
    * Simplify yield delegation process. (#2544)
    
    * feat: enable strategist to call `delegateYield()`.
    
    * feat: allow strategist to call `undelegateYield()`.
    
    * feat: add deployment script.
    
    * test: add test for OETH upgrade.
    
    * test: add test for eip7702 user.
    
    * feat: merge ousd and oeth upgrade deployment.
    
    * fix: enhance some tests.
    
    * feat: add upgrade script for EIP7702 on OETHb.
    
    * Only perform a code check if it is possible the address is a smart wallet
    
    * Fix fork test by using real EIP7702 code
    
    ---------
    
    Co-authored-by: Daniel Von Fange <daniel@leancoder.com>

--- contracts/test/token/oeth.mainnet.fork-test.js
+++ contracts/test/token/oeth.mainnet.fork-test.js
@@ -1,7 +1,8 @@
 const { expect } = require("chai");
-
+const { impersonateAndFund } = require("../../utils/signers");
 const { loadDefaultFixture } = require("./../_fixture");
-const { isCI } = require("./../helpers");
+const { isCI, ethUnits, advanceTime } = require("./../helpers");
+const hre = require("hardhat");
 
 /**
  * Regarding hardcoded addresses:
@@ -36,6 +37,62 @@ describe("ForkTest: OETH", function () {
         "0xa4c637e0f704745d182e4d38cab7e7485321d059";
       // 2 equals OptIn
       expect(await oeth.rebaseState(eigenLayerStrategyContract)).to.be.equal(2);
+    });
+    it("Should delegate or undelegate yield with strategist", async () => {
+      const { oeth, strategist, matt, josh } = fixture;
+      const impersonatedStrategist = await impersonateAndFund(
+        strategist.address
+      );
+      expect(
+        await oeth
+          .connect(impersonatedStrategist)
+          .delegateYield(matt.address, josh.address)
+      ).to.not.be.revertedWith("Caller is not the Strategist or Governor");
+      expect(
+        await oeth.connect(impersonatedStrategist).undelegateYield(matt.address)
+      ).to.not.be.revertedWith("Caller is not the Strategist or Governor");
+    });
+    it("Should earn yield even if EIP7702 user", async () => {
+      const { oeth, oethVault, weth, usdc, josh } = fixture;
+      const eip770UserAddress = josh.address;
+      await hre.network.provider.send("hardhat_setCode", [
+        eip770UserAddress,
+        "0xef010063c0c19a282a1b52b07dd5a65b58948a07dae32b",
+      ]);
+      const eip770User = await impersonateAndFund(eip770UserAddress);
+
+      // Mint some OETH to the eip7702 user
+      await weth.connect(eip770User).deposit({ value: ethUnits("13") });
+      await weth.connect(eip770User).approve(oethVault.address, ethUnits("3"));
+      await oethVault
+        .connect(eip770User)
+        .mint(weth.address, ethUnits("3"), ethUnits("0"));
+
+      // EIP7702 keep 1 OETH and transfer 1 OETH to a smart contract (USDC in this case)
+      // and transfer 1 OETH to a wallet (josh in this case)
+      await oeth.connect(eip770User).transfer(usdc.address, ethUnits("1"));
+      await oeth.connect(eip770User).transfer(josh.address, ethUnits("1"));
+      const eip7702UserBalanceBefore = await oeth.balanceOf(eip770UserAddress);
+      const scBalanceBefore = await oeth.balanceOf(usdc.address);
+      const joshBalanceBefore = await oeth.balanceOf(josh.address);
+
+      // Simulate yield
+      await weth
+        .connect(eip770User)
+        .transfer(oethVault.address, ethUnits("10"));
+      // Simulate time jump
+      await advanceTime(24 * 60 * 60); // 1 day
+      // Rebase the OETH vault
+      await oethVault.rebase();
+      const eip7702UserBalanceAfter = await oeth.balanceOf(eip770UserAddress);
+      const scBalanceAfter = await oeth.balanceOf(usdc.address);
+      const joshBalanceAfter = await oeth.balanceOf(josh.address);
+
+      // Ensure the balance has increased i.e. yield was earned for both eip7702 user and wallet (josh)
+      expect(joshBalanceAfter).to.be.gt(joshBalanceBefore);
+      expect(eip7702UserBalanceAfter).to.be.gt(eip7702UserBalanceBefore);
+      // Ensure the smart contract balance has not changed
+      expect(scBalanceAfter).to.be.eq(scBalanceBefore);
     });
   });
 });
