e8d8405b3d5041edc8098a8b6d075ccc677630f2
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-07-02 16:56:17 +0530

    Update deploy script

--- contracts/deploy/mainnet/145_deploy_xogn_rewards_module.js
+++ contracts/deploy/mainnet/145_deploy_xogn_rewards_module.js
@@ -1,7 +1,5 @@
 const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
 
-const addresses = require("../../utils/addresses");
-
 module.exports = deploymentWithGovernanceProposal(
   {
     deployName: "145_deploy_xogn_rewards_module",
@@ -11,19 +9,31 @@ module.exports = deploymentWithGovernanceProposal(
     proposalId: "",
   },
   async ({ deployWithConfirmation }) => {
-    await deployWithConfirmation("CollectXOGNRewardsModule", [
-      addresses.mainnet.Guardian,
-      // Defender Relayer
-      "0x4b91827516f79d6F6a1F292eD99671663b09169a",
-    ]);
-    const cCollectXOGNRewardsModule = await ethers.getContract(
-      "CollectXOGNRewardsModule"
-    );
+    const safeAddresses = [
+      "0x5c8228e709D7F91209DE898F6a7B8c6035A7B78f",
+      "0x69497A2A170c138876F05Df01bFfDd5C4b651CF2",
+      "0x684b38997afbBBC055e0BEB6d536686Ebd171bdB",
+      "0xe555EFA16d38747F9e496926b576FD1ebD31DeCa",
+      "0x6E75645EeDCCCAA0f472323Afce8f82B875C8CB9",
+    ];
 
-    console.log(
-      "CollectXOGNRewardsModule deployed to",
-      cCollectXOGNRewardsModule.address
-    );
+    let i = 0;
+    for (const safeAddress of safeAddresses) {
+      i++;
+      const moduleName = `CollectXOGNRewardsModule${i}`;
+      await deployWithConfirmation("CollectXOGNRewardsModule", [
+        safeAddress,
+        // Defender Relayer
+        "0x4b91827516f79d6F6a1F292eD99671663b09169a",
+        moduleName,
+      ]);
+      const cCollectXOGNRewardsModule = await ethers.getContract(moduleName);
+
+      console.log(
+        `CollectXOGNRewardsModule${i} (for ${safeAddress}) deployed to`,
+        cCollectXOGNRewardsModule.address
+      );
+    }
 
     return {
       actions: [],
