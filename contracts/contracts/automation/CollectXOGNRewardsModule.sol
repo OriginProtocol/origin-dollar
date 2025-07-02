05dc07f6ccc96824ff49aee148a3ce872942b5cc
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-07-02 16:59:07 +0530

    Fix checksum

--- contracts/contracts/automation/CollectXOGNRewardsModule.sol
+++ contracts/contracts/automation/CollectXOGNRewardsModule.sol
@@ -14,7 +14,7 @@ contract CollectXOGNRewardsModule is AbstractSafeModule {
     address public constant rewardsSource =
         0x7609c88E5880e934dd3A75bCFef44E31b1Badb8b;
     IERC20 public constant ogn =
-        IERC20(0x8207c1ffc5b6804f6024322ccf34f29c3541ae26);
+        IERC20(0x8207c1FfC5B6804F6024322CcF34F29c3541Ae26);
 
     constructor(address _safeContract, address operator)
         AbstractSafeModule(_safeContract)
