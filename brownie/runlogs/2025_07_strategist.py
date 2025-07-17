035161c85212848ca6b24ab2c5cb4499246c89ae
Nicholas Addison <nick@addisonbrown.com.au>
2025-07-18 09:12:14 +1000

    Add Sonic claim from validator and deposit into the SwapX AMO

--- brownie/runlogs/2025_07_strategist.py
+++ brownie/runlogs/2025_07_strategist.py
@@ -375,3 +375,48 @@ def main():
     print("Pool OETH  ", "{:.6f}".format(oethPoolBalance / 10**18), oethPoolBalance * 100 / totalPool)
     print("Pool Total ", "{:.6f}".format(totalPool / 10**18), totalPool)
     print("Sell 10 OETH Curve prices before and after", "{:.6f}".format(weth_out_before / 10**18), "{:.6f}".format(weth_out_after / 10**18))
+
+
+# -------------------------------------
+# July 18, 2025 - Claim validator withdrawal and deposit SwapX AMO on Sonic
+# -------------------------------------
+from world_sonic import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+
+    amount = 84000 * 10**18
+
+    # Before
+    txs.append(vault_core.rebase({'from': SONIC_STRATEGIST}))
+    txs.append(vault_value_checker.takeSnapshot({'from': SONIC_STRATEGIST}))
+
+    txs.append(
+      sonic_staking_strat.withdrawFromSFC(
+        44,
+        {'from': SONIC_STRATEGIST}
+      )
+    )
+
+    # Claim withdrawal from the Validator
+    txs.append(
+      vault_admin.depositToStrategy(
+        SWAPX_AMO_STRATEGY, 
+        [WS_SONIC],
+        [amount],
+        {'from': SONIC_STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(SONIC_STRATEGIST)[0]
+    supply_change = os.totalSupply() - vault_value_checker.snapshots(SONIC_STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (10 * 10**18), vault_change, (10 * 10**18), {'from': SONIC_STRATEGIST}))
+
+    print("-----")
+    print("Profit in wS", "{:.6f}".format(profit / 10**18), profit)
+    print("OS supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("OS Vault change ", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
