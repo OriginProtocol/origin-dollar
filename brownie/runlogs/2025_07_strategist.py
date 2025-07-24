a550253063a757ebbeb62c1ba579b65c828394e8
Clément <55331875+clement-ux@users.noreply.github.com>
2025-07-24 16:31:00 +0200

    add deposit functionality for 127 wS on SwapX AMO and log profit details (#2599)

--- brownie/runlogs/2025_07_strategist.py
+++ brownie/runlogs/2025_07_strategist.py
@@ -420,3 +420,40 @@ def main():
     print("OS supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
     print("OS Vault change ", "{:.6f}".format(vault_change / 10**18), vault_change)
     print("-----")
+
+# -------------------------------------
+# July 24, 2025 - Deposit 127 wS on SwapX AMO on Sonic
+# -------------------------------------
+from world_sonic import *
+
+def main():
+  with TemporaryForkForReallocations() as txs:
+
+    amount = 127 * 10**18
+
+    # Before
+    txs.append(vault_core.rebase({'from': SONIC_STRATEGIST}))
+    txs.append(vault_value_checker.takeSnapshot({'from': SONIC_STRATEGIST}))
+
+    # Claim withdrawal from the Validator
+    txs.append(
+      vault_admin.depositToStrategy(
+        SWAPX_AMO_STRATEGY, 
+        [WS_SONIC],
+        [amount],
+        {'from': SONIC_STRATEGIST}
+      )
+    )
+
+    # After
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(SONIC_STRATEGIST)[0]
+    supply_change = os.totalSupply() - vault_value_checker.snapshots(SONIC_STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (10 * 10**18), vault_change, (10 * 10**18), {'from': SONIC_STRATEGIST}))
+
+    print("-----")
+    print("Profit in wS", "{:.6f}".format(profit / 10**18), profit)
+    print("OS supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("OS Vault change ", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
