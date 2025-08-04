3ec33d0aac372e3af9684a4da840e813316aab2c
Clément <clemmoller@gmail.com>
2025-08-04 08:55:54 +0200

    refactor: remove unused ARM Proxy contract interactions and clean up comments

--- contracts/deploy/sonic/024_increase_timelock_delay.js
+++ contracts/deploy/sonic/024_increase_timelock_delay.js
@@ -8,28 +8,15 @@ module.exports = deployOnSonic(
   },
   async ({ ethers }) => {
     // 1. Deploy new OS implementation
-    const cARMProxy = await ethers.getContractAt(
-      ["function setOwner(address) external"],
-      "0x2F872623d1E1Af5835b08b0E49aAd2d81d649D30"
-    );
     const cTimelock = await ethers.getContractAt(
       "ITimelockController",
       addresses.sonic.timelock
     );
 
-    console.log("ARM Proxy:", cARMProxy.address);
-    console.log("Timelock:", cTimelock.address);
-
     return {
       name: "Upgrade OS token contract",
       actions: [
-        // 1. Upgrade the OSonic proxy to the new implementation
-        //{
-        //  contract: cARMProxy,
-        //  signature: "setOwner(address)",
-        //  args: [addresses.sonic.timelock],
-        //},
-        // 2. Update delay to 2d
+        // 1. Update delay to 2d
         {
           contract: cTimelock,
           signature: "updateDelay(uint256)",
