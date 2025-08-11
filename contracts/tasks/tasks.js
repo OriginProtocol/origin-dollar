ac2c3b1a3143d1949de27c833a6f0d95e1dbe465
Nicholas Addison <nick@addisonbrown.com.au>
2025-08-11 16:20:51 +1000

    Added tenderlyUpload Hardhat task

--- contracts/tasks/tasks.js
+++ contracts/tasks/tasks.js
@@ -104,7 +104,7 @@ const {
   snapValidators,
 } = require("./validator");
 
-const { tenderlySync } = require("./tenderly");
+const { tenderlySync, tenderlyUpload } = require("./tenderly");
 const { setDefaultValidator, snapSonicStaking } = require("../utils/sonic");
 const {
   undelegateValidator,
@@ -1837,5 +1837,12 @@ subtask(
   "Fetches all contracts from deployment descriptors and uploads them to Tenderly if they are not there yet."
 ).setAction(tenderlySync);
 task("tenderlySync").setAction(async (_, __, runSuper) => {
+  return runSuper();
+});
+
+subtask("tenderlyUpload", "Uploads a contract to Tenderly.")
+  .addParam("name", "The contract's name", undefined, types.string)
+  .setAction(tenderlyUpload);
+task("tenderlyUpload").setAction(async (_, __, runSuper) => {
   return runSuper();
 });
