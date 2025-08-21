072fea512b037e19739a184fa4f9ad864a1840f3
Clément <clemmoller@gmail.com>
2025-08-21 20:20:17 +0200

    feat: add function to deposit 125 WETH on Curve AMO; update run method in Runlogs_2025_08_Base

--- contracts/scripts/runlogs/2025_08.s.sol
+++ contracts/scripts/runlogs/2025_08.s.sol
@@ -77,6 +77,7 @@ contract Runlogs_2025_08_Mainnet is SetupMainnet {
 contract Runlogs_2025_08_Base is SetupBase {
   function run() public {
     _2025_08_18();
+    _2025_08_21();
   }
 
   // ------------------------------------------------------------------
@@ -102,6 +103,66 @@ contract Runlogs_2025_08_Base is SetupBase {
 
     // Main action
     uint256 amountToDeposit = 90 ether;
+    address[] memory assets = new address[](1);
+    assets[0] = address(weth);
+    uint256[] memory amounts = new uint256[](1);
+    amounts[0] = amountToDeposit;
+    oethVaultAdmin.depositToStrategy(address(oethWethCurveAMO), assets, amounts);
+
+    // After
+    (uint256 vaultValueAfter, uint256 totalSuplyAfter,) =
+      oethVaultValueChecker.snapshots(strategist);
+    int256 vaultChange = int256(oethVaultCore.totalValue()) - int256(vaultValueAfter);
+    int256 supplyChange = int256(oeth.totalSupply()) - int256(totalSuplyAfter);
+    int256 profit = vaultChange - supplyChange;
+
+    oethVaultValueChecker.checkDelta(profit, 1 ether, vaultChange, 10 ether);
+
+    console.log("-----");
+    console.log("Profit            : %18e ", profit);
+    console.log("OETH Supply change: %18e ", supplyChange);
+    console.log("Vault value change: %18e ", vaultChange);
+
+    // AMO pool after
+    uint256 wethPoolBalanceAfter = weth.balanceOf(address(oethWethCurvePool));
+    uint256 oethPoolBalanceAfter = oeth.balanceOf(address(oethWethCurvePool));
+    uint256 totalPoolAfter = wethPoolBalanceAfter + oethPoolBalanceAfter;
+    uint256 wethOutAfter = oethWethCurvePool.get_dy(0, 1, 10 ether);
+
+    console.log("-----");
+    console.log("Curve OETH/WETH Pool after");
+    console.log("WETH Pool  %18e", wethPoolBalanceAfter);
+    console.log("OETH Pool  %18e", oethPoolBalanceAfter);
+    console.log("Total Pool %18e", totalPoolAfter);
+    console.log(
+      "Sell 10 OETH Curve prices before and after: %18e || %18e", wethOutBefore, wethOutAfter
+    );
+    vm.stopBroadcast();
+  }
+
+  // ------------------------------------------------------------------
+  // August 21, 2025 - Deposit 125 WETH on Curve AMO
+  // ------------------------------------------------------------------
+  function _2025_08_21() internal {
+    vm.startBroadcast(strategist);
+    // Before
+    oethVaultCore.rebase();
+    oethVaultValueChecker.takeSnapshot();
+
+    // AMO pool before
+    uint256 wethPoolBalanceBefore = weth.balanceOf(address(oethWethCurvePool));
+    uint256 oethPoolBalanceBefore = oeth.balanceOf(address(oethWethCurvePool));
+    uint256 totalPoolBefore = wethPoolBalanceBefore + oethPoolBalanceBefore;
+    uint256 wethOutBefore = oethWethCurvePool.get_dy(0, 1, 10 ether);
+
+    console.log("-----");
+    console.log("Curve OETH/WETH Pool before");
+    console.log("WETH Pool  %18e", wethPoolBalanceBefore);
+    console.log("OETH Pool  %18e", oethPoolBalanceBefore);
+    console.log("Total Pool %18e", totalPoolBefore);
+
+    // Main action
+    uint256 amountToDeposit = 125 ether;
     address[] memory assets = new address[](1);
     assets[0] = address(weth);
     uint256[] memory amounts = new uint256[](1);
