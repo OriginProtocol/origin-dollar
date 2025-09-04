2a5078e314eaae5e1bb55db79c32843af8287c3a
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-09-04 10:43:10 +0400

    Add runlog

--- contracts/scripts/runlogs/2025_09.s.sol
+++ contracts/scripts/runlogs/2025_09.s.sol
@@ -12,7 +12,38 @@ import { Sonic } from "./utils/Addresses.sol";
 import { console } from "forge-std/console.sol";
 
 contract Runlogs_2025_09_Mainnet is SetupMainnet {
-  function run() public { }
+  function run() public {
+    _2025_09_03();
+  }
+
+  function _2025_09_03() internal {
+    vm.startBroadcast(strategist);
+
+    oethVaultCore.rebase();
+    oethVaultValueChecker.takeSnapshot();
+
+    address[] memory assets = new address[](1);
+    assets[0] = address(weth);
+    uint256[] memory amounts = new uint256[](1);
+    amounts[0] = 7 ether;
+    oethVaultAdmin.withdrawFromStrategy(address(oethWethCurveAMO), assets, amounts);
+
+    (uint256 vaultValueAfter, uint256 totalSupplyAfter,) =
+      oethVaultValueChecker.snapshots(strategist);
+    int256 vaultChange = int256(oethVaultCore.totalValue()) - int256(vaultValueAfter);
+    int256 supplyChange = int256(oeth.totalSupply()) - int256(totalSupplyAfter);
+    int256 profit = vaultChange - supplyChange;
+    oethVaultValueChecker.checkDelta(profit, 0.1 ether, vaultChange, 1 ether);
+
+    console.log("-----");
+    console.log("Profit             : %18e", profit);
+    console.log("Amount withdrawn   : %18e", amounts[0], "ether");
+    console.log("Supply change      : %18e", supplyChange);
+    console.log("Vault value change : %18e", vaultChange);
+
+
+    vm.stopBroadcast();
+  }
 }
 
 contract Runlogs_2025_09_Base is SetupBase {
