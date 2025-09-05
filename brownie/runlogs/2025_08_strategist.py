f5be9512bfb4bab52e8cc1e82803cdcf0b9991ac
Domen Grabec <grabec@gmail.com>
2025-09-05 15:39:02 +0200

    add the deposit to Curve AMO runlog (#2616)

--- brownie/runlogs/2025_08_strategist.py
+++ brownie/runlogs/2025_08_strategist.py
@@ -1,3 +1,20 @@
+# -------------------------------------
+# August 7, 2025 - Deposit to Curve AMO
+# -------------------------------------
+
+from world_base import *
+def main():
+  with TemporaryForkForReallocations() as txs:
+    txs.append(vault_core.rebase(from_strategist))
+    txs.append(vault_value_checker.takeSnapshot(from_strategist))
+
+    txs.append(vault_admin.depositToStrategy(OETHB_CURVE_AMO_STRATEGY, [WETH_BASE], [4050 * 10**18], from_strategist))
+
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(MULTICHAIN_STRATEGIST)[0]
+    supply_change = oethb.totalSupply() - vault_value_checker.snapshots(MULTICHAIN_STRATEGIST)[1]
+    profit = vault_change - supply_change
+
+    txs.append(vault_value_checker.checkDelta(profit, (0.1 * 10**18), vault_change, (1 * 10**18), from_strategist))
 
 # -------------------------------------
 # August 11, 2025 - Claim validator withdrawal and deposit SwapX AMO on Sonic
