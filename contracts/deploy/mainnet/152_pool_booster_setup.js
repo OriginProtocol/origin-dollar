2090743a8c15a8dac0a5c83c174718aada24d424
Domen Grabec <grabec@gmail.com>
2025-10-02 14:12:26 +0200

    improve test

--- contracts/deploy/mainnet/152_pool_booster_setup.js
+++ contracts/deploy/mainnet/152_pool_booster_setup.js
@@ -66,7 +66,8 @@ module.exports = deploymentWithGovernanceProposal(
       "PoolBoosterFactoryMerkl",
       [
         oeth.address,
-        addresses.mainnet.Timelock,
+        // so we can create a Merkl pool booster fast via a multichain strategist and kick off yield forwarding
+        addresses.multichainStrategist,
         cPoolBoostCentralRegistryProxy.address,
         addresses.mainnet.MerklDistributor,
       ]
@@ -78,7 +79,6 @@ module.exports = deploymentWithGovernanceProposal(
     console.log(
       `Pool Booster Merkl Factory deployed to ${cPoolBoosterMerklFactory.address}`
     );
-    const salt = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Merkl Morpho PB OUSD/USDC v1"));
 
     return {
       name: "Upgrade PoolBoosterCentralRegistry and deploy PoolBoosterFactoryMerkl",
@@ -89,22 +89,6 @@ module.exports = deploymentWithGovernanceProposal(
           signature: "approveFactory(address)",
           args: [dPoolBoosterFactoryMerkl.address],
         },
-        {
-          // set the factory as an approved one
-          contract: cPoolBoosterMerklFactory,
-          signature: "createPoolBoosterMerkl(uint32,address,uint32,bytes,uint256)",
-          args: [
-            45, // campaignType: MORPHOBORROW meaning we incentivise the borrowing side: lowering USDC borrow rate
-            addresses.mainnet.MorphoOusdUsdcMarket.substring(0, 42), // trimmed market address for tracking purposes
-            60 * 60 * 24 * 7, // campaing duration: 7 days
-            // Built in the UI following these steps: 
-            // - https://www.notion.so/originprotocol/How-to-Borrow-Booster-27e84d46f53c80d4b657ca2fc3f6554b
-            // - "https://www.dropbox.com/scl/fi/gtkgwmvnfjrovzhbjg417/Screenshot-2025-10-02-at-10.24.33.png?rlkey=oyqhmex7wrvxzps3h8dmr6g1l&dl=0",
-            "0xb8fef900b383db2dbbf4458c7f46acf5b140f26d603a6d1829963f241b82510e00000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000000e000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000120000000000000000000000000000000000000000000000000000000000000014000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-            salt// salt
-
-          ],
-        }
       ],
     };
   }
