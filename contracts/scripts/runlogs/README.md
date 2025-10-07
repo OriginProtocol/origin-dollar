c89a33cb7acb93ee75e63b9aa1f7ca760f3288ab
Nicholas Addison <nick@addisonbrown.com.au>
2025-10-07 15:03:57 +1100

    Run log 10 Oct 25 - deposit SSV to clusters

--- contracts/scripts/runlogs/README.md
+++ contracts/scripts/runlogs/README.md
@@ -1,7 +1,9 @@
 # How to use foundry runlogs?
 
 ## 1. Ensure all dependencies are installed
+
 At the root of the repo run:
+
 ```bash
 forge soldeer install
 cd contract
@@ -9,38 +11,48 @@ yarn install
 ```
 
 ## 2. Execute runlogs
+
 In the `contracts` folder run:
+
 ```bash
 forge script Runlogs_2025_08_Mainnet
 ```
+
 > Note: adjust the year, month and chain accordingly.
 
 This generates 2 files (that are `broadcast-ready` for execution) under `contracts/broadcast`:
+
 - run-latest.json
 - run-1755456667783.json (with the timestamp corresponding to the execution time)
 
 ## 3. Convert runlogs into Safe-compatible JSON
+
 Since these transactions are meant to be executed from the Safe, it is not possible to use `cast`.
 
-To convert a `broadcast-ready` into a Safe-compatible JSON file, use the forge script: `scripts/runlogs/utils/BroadcastConvertor.sol`. 
+To convert a `broadcast-ready` into a Safe-compatible JSON file, use the forge script: `scripts/runlogs/utils/BroadcastConvertor.sol`.
 
 In the `contracts` folder run:
+
 ```bash
-forge script BroadcastConvertor --sig "run(string)" contracts/broadcast/2025_09.sol/146/dry-run/
+forge script BroadcastConvertor --sig "run(string)" contracts/broadcast/2025_09.s.sol/146/dry-run/
 ```
+
 > Note adjust the input accordingly:
 > first the path to the run file, but stop at the dry-run folder.
 
 This creates, by default, a file named `run-latest-safe.json` in the same location as the input file, ready to be imported into the Safe UI.
 
 ### Timelock targeted ?
+
 If on the script, the address used inside `startBroadcast()` is a `Timelock`:
-- the Safe-compatible JSON will be adjusted to target the `scheduleBatch` and `executeBatch` functions on the `Timelock` contract. 
+
+- the Safe-compatible JSON will be adjusted to target the `scheduleBatch` and `executeBatch` functions on the `Timelock` contract.
 - two files will be generated: `run-latest-schedule` and `run-latest-execute`.
 
-
 ## 4. How generates Safe JSON in just one command?
+
 In the `contracts` folder:
+
 ```makefile
 make script
 or
--- contracts/scripts/runlogs/utils/Setup.sol
+++ contracts/scripts/runlogs/utils/Setup.sol
@@ -24,21 +24,20 @@ import { OSonic } from "contracts/contracts/token/OSonic.sol";
 import { CurveAMOStrategy } from "contracts/contracts/strategies/CurveAMOStrategy.sol";
 import { BaseCurveAMOStrategy } from "contracts/contracts/strategies/BaseCurveAMOStrategy.sol";
 import { SonicStakingStrategy } from "contracts/contracts/strategies/sonic/SonicStakingStrategy.sol";
-import { AerodromeAMOStrategy } from
-  "contracts/contracts/strategies/aerodrome/AerodromeAMOStrategy.sol";
+import { AerodromeAMOStrategy } from "contracts/contracts/strategies/aerodrome/AerodromeAMOStrategy.sol";
 
 // Contracts - ARM
 import { ISonicARM } from "contracts/contracts/interfaces/arm/ISonicARM.sol";
 
 // Contracts - Pool Booster
-import { PoolBoosterFactoryMerkl } from
-  "contracts/contracts/poolBooster/PoolBoosterFactoryMerkl.sol";
-import { PoolBoostCentralRegistry } from
-  "contracts/contracts/poolBooster/PoolBoostCentralRegistry.sol";
+import { PoolBoosterFactoryMerkl } from "contracts/contracts/poolBooster/PoolBoosterFactoryMerkl.sol";
+import { PoolBoostCentralRegistry } from "contracts/contracts/poolBooster/PoolBoostCentralRegistry.sol";
 
 // Interfaces
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IWETH9 } from "contracts/contracts/interfaces/IWETH9.sol";
 import { ICurveStableSwapNG } from "contracts/contracts/interfaces/ICurveStableSwapNG.sol";
+import { ISSVNetwork } from "contracts/contracts/interfaces/ISSVNetwork.sol";
 
 // Helpers
 import { CrossChain, Mainnet, Base, Sonic } from "./Addresses.sol";
@@ -48,94 +47,102 @@ import { Script } from "forge-std/Script.sol";
 import { Test } from "forge-std/Test.sol";
 
 abstract contract SetupMainnet is Test, Script {
-  // Governance
-  address public strategist = CrossChain.STRATEGIST;
+    // Governance
+    address public strategist = CrossChain.STRATEGIST;
 
-  // OUSD
-  OUSD public ousd = OUSD(Mainnet.OUSD);
+    // OUSD
+    OUSD public ousd = OUSD(Mainnet.OUSD);
 
-  // OETH
-  OETH public oeth = OETH(Mainnet.OETH);
-  WOETH public woeth = WOETH(Mainnet.WOETH);
-  OETHVaultCore public oethVaultCore = OETHVaultCore(Mainnet.OETH_VAULT);
-  OETHVaultAdmin public oethVaultAdmin = OETHVaultAdmin(Mainnet.OETH_VAULT);
-  CurveAMOStrategy public oethWethCurveAMO = CurveAMOStrategy(Mainnet.OETH_WETH_CURVE_AMO);
-  OETHVaultValueChecker public oethVaultValueChecker =
-    OETHVaultValueChecker(Mainnet.OETH_VAULT_VALUE_CHECKER);
+    // OETH
+    OETH public oeth = OETH(Mainnet.OETH);
+    WOETH public woeth = WOETH(Mainnet.WOETH);
+    OETHVaultCore public oethVaultCore = OETHVaultCore(Mainnet.OETH_VAULT);
+    OETHVaultAdmin public oethVaultAdmin = OETHVaultAdmin(Mainnet.OETH_VAULT);
+    CurveAMOStrategy public oethWethCurveAMO =
+        CurveAMOStrategy(Mainnet.OETH_WETH_CURVE_AMO);
+    OETHVaultValueChecker public oethVaultValueChecker =
+        OETHVaultValueChecker(Mainnet.OETH_VAULT_VALUE_CHECKER);
 
-  // Pool Booster
-  PoolBoosterFactoryMerkl public poolBoosterFactoryMerkl =
-    PoolBoosterFactoryMerkl(Mainnet.POOL_BOOSTER_FACTORY_MERKL);
-  PoolBoostCentralRegistry public poolBoosterCentralRegistry =
-    PoolBoostCentralRegistry(Mainnet.POOL_BOOSTER_CENTRAL_REGISTRY);
+    // Pool Booster
+    PoolBoosterFactoryMerkl public poolBoosterFactoryMerkl =
+        PoolBoosterFactoryMerkl(Mainnet.POOL_BOOSTER_FACTORY_MERKL);
+    PoolBoostCentralRegistry public poolBoosterCentralRegistry =
+        PoolBoostCentralRegistry(Mainnet.POOL_BOOSTER_CENTRAL_REGISTRY);
 
-  // Interfaces
-  IWETH9 public weth = IWETH9(Mainnet.WETH);
-  ICurveStableSwapNG public oethWethCurvePool = ICurveStableSwapNG(Mainnet.OETH_WETH_CURVE_POOL);
+    // Interfaces
+    IWETH9 public weth = IWETH9(Mainnet.WETH);
+    ICurveStableSwapNG public oethWethCurvePool =
+        ICurveStableSwapNG(Mainnet.OETH_WETH_CURVE_POOL);
+    IERC20 public ssv = IERC20(Mainnet.SSV);
+    ISSVNetwork public ssvNetwork = ISSVNetwork(Mainnet.SSV_NETWORK);
 
-  function setUp() public {
-    // Note: to ensure perfect simulation, don't fix block number, it will be automatically set to the latest block
-    vm.createSelectFork(vm.envString("PROVIDER_URL"));
-  }
+    function setUp() public {
+        // Note: to ensure perfect simulation, don't fix block number, it will be automatically set to the latest block
+        vm.createSelectFork(vm.envString("PROVIDER_URL"));
+    }
 }
 
 abstract contract SetupBase is Test, Script {
-  // Governance
-  address public strategist = CrossChain.STRATEGIST;
+    // Governance
+    address public strategist = CrossChain.STRATEGIST;
 
-  // OETH
-  OETHBase public oeth = OETHBase(Base.OETHB);
-  WOETHBase public woeth = WOETHBase(Base.WOETHB);
-  OETHBaseVaultCore public oethVaultCore = OETHBaseVaultCore(Base.OETHB_VAULT);
-  OETHBaseVaultAdmin public oethVaultAdmin = OETHBaseVaultAdmin(Base.OETHB_VAULT);
-  BaseCurveAMOStrategy public oethWethCurveAMO = BaseCurveAMOStrategy(Base.OETHB_WETH_CURVE_AMO);
-  AerodromeAMOStrategy public oethWethAerodromeAMO =
-    AerodromeAMOStrategy(Base.OETHB_WETH_AERODROME_POOL);
-  OETHVaultValueChecker public oethVaultValueChecker =
-    OETHVaultValueChecker(Base.OETHB_VAULT_VALUE_CHECKER);
+    // OETH
+    OETHBase public oeth = OETHBase(Base.OETHB);
+    WOETHBase public woeth = WOETHBase(Base.WOETHB);
+    OETHBaseVaultCore public oethVaultCore =
+        OETHBaseVaultCore(Base.OETHB_VAULT);
+    OETHBaseVaultAdmin public oethVaultAdmin =
+        OETHBaseVaultAdmin(Base.OETHB_VAULT);
+    BaseCurveAMOStrategy public oethWethCurveAMO =
+        BaseCurveAMOStrategy(Base.OETHB_WETH_CURVE_AMO);
+    AerodromeAMOStrategy public oethWethAerodromeAMO =
+        AerodromeAMOStrategy(Base.OETHB_WETH_AERODROME_POOL);
+    OETHVaultValueChecker public oethVaultValueChecker =
+        OETHVaultValueChecker(Base.OETHB_VAULT_VALUE_CHECKER);
 
-  // Pool Booster
-  PoolBoosterFactoryMerkl public poolBoosterFactoryMerkl =
-    PoolBoosterFactoryMerkl(Base.POOL_BOOSTER_FACTORY_MERKL);
-  PoolBoostCentralRegistry public poolBoosterCentralRegistry =
-    PoolBoostCentralRegistry(Base.POOL_BOOSTER_CENTRAL_REGISTRY);
+    // Pool Booster
+    PoolBoosterFactoryMerkl public poolBoosterFactoryMerkl =
+        PoolBoosterFactoryMerkl(Base.POOL_BOOSTER_FACTORY_MERKL);
+    PoolBoostCentralRegistry public poolBoosterCentralRegistry =
+        PoolBoostCentralRegistry(Base.POOL_BOOSTER_CENTRAL_REGISTRY);
 
-  // Interfaces
-  IWETH9 public weth = IWETH9(Base.WETH);
-  ICurveStableSwapNG public oethWethCurvePool = ICurveStableSwapNG(Base.OETHB_WETH_CURVE_POOL);
+    // Interfaces
+    IWETH9 public weth = IWETH9(Base.WETH);
+    ICurveStableSwapNG public oethWethCurvePool =
+        ICurveStableSwapNG(Base.OETHB_WETH_CURVE_POOL);
 
-  function setUp() public {
-    // Note: to ensure perfect simulation, don't fix block number, it will be automatically set to the latest block
-    vm.createSelectFork(vm.envString("BASE_PROVIDER_URL"));
-  }
+    function setUp() public {
+        // Note: to ensure perfect simulation, don't fix block number, it will be automatically set to the latest block
+        vm.createSelectFork(vm.envString("BASE_PROVIDER_URL"));
+    }
 }
 
 abstract contract SetupSonic is Test, Script {
-  // Governance
-  address public governor = Sonic.GOVERNOR;
-  address public timelock = Sonic.TIMELOCK;
-  address public strategist = CrossChain.STRATEGIST;
-  address public localStrategist = Sonic.STRATEGIST;
+    // Governance
+    address public governor = Sonic.GOVERNOR;
+    address public timelock = Sonic.TIMELOCK;
+    address public strategist = CrossChain.STRATEGIST;
+    address public localStrategist = Sonic.STRATEGIST;
 
-  // OS
-  OSonic public os = OSonic(Sonic.OS);
-  OSonicVaultCore public osVaultCore = OSonicVaultCore(Sonic.OS_VAULT);
-  OSonicVaultAdmin public osVaultAdmin = OSonicVaultAdmin(Sonic.OS_VAULT);
-  OETHVaultValueChecker public osVaultValueChecker =
-    OETHVaultValueChecker(Sonic.OS_VAULT_VALUE_CHECKER);
+    // OS
+    OSonic public os = OSonic(Sonic.OS);
+    OSonicVaultCore public osVaultCore = OSonicVaultCore(Sonic.OS_VAULT);
+    OSonicVaultAdmin public osVaultAdmin = OSonicVaultAdmin(Sonic.OS_VAULT);
+    OETHVaultValueChecker public osVaultValueChecker =
+        OETHVaultValueChecker(Sonic.OS_VAULT_VALUE_CHECKER);
 
-  // Interfaces
-  IWETH9 public ws = IWETH9(Sonic.WS);
+    // Interfaces
+    IWETH9 public ws = IWETH9(Sonic.WS);
 
-  // ARM
-  ISonicARM public arm = ISonicARM(Sonic.ARM);
+    // ARM
+    ISonicARM public arm = ISonicARM(Sonic.ARM);
 
-  // Staking strategy
-  SonicStakingStrategy public stakingStrategy =
-    SonicStakingStrategy(payable(Sonic.STAKING_STRATEGY));
+    // Staking strategy
+    SonicStakingStrategy public stakingStrategy =
+        SonicStakingStrategy(payable(Sonic.STAKING_STRATEGY));
 
-  function setUp() public {
-    // Note: to ensure perfect simulation, don't fix block number, it will be automatically set to the latest block
-    vm.createSelectFork(vm.envString("SONIC_PROVIDER_URL"));
-  }
+    function setUp() public {
+        // Note: to ensure perfect simulation, don't fix block number, it will be automatically set to the latest block
+        vm.createSelectFork(vm.envString("SONIC_PROVIDER_URL"));
+    }
 }
