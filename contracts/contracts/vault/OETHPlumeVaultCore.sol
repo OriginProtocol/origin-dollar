153244ea8395258cf3caa70651e472464589f2d2
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-10-28 09:19:04 +0400

    Remove unused libraries and imports

--- contracts/contracts/vault/OETHPlumeVaultCore.sol
+++ contracts/contracts/vault/OETHPlumeVaultCore.sol
@@ -1,21 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import { StableMath } from "../utils/StableMath.sol";
 import { OETHVaultCore } from "./OETHVaultCore.sol";
-
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IStrategy } from "../interfaces/IStrategy.sol";
 
 /**
  * @title OETH Plume VaultCore Contract
  * @author Origin Protocol Inc
  */
 contract OETHPlumeVaultCore is OETHVaultCore {
-    using SafeERC20 for IERC20;
-    using StableMath for uint256;
-
     constructor(address _weth) OETHVaultCore(_weth) {}
 
     // @inheritdoc OETHVaultCore
