fe66bc3037833978987ea481c59e8002be13765c
Domen Grabec <grabec@gmail.com>
2025-12-19 23:09:42 +0100

    add cross chain unit test basic files

---
+++ contracts/contracts/mocks/crosschain/CCTPMessageTransmitterMock.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import { ICCTPMessageTransmitter } from "../../interfaces/cctp/ICCTP.sol";
+import { IERC20 } from "../../utils/InitializableAbstractStrategy.sol";
+
+/**
+ * @title Mock conctract simulating the functionality of the CCTPTokenMessenger contract
+ *        for the porposes of unit testing. 
+ * @author Origin Protocol Inc
+ */
+
+contract CCTPMessageTransmitterMock is ICCTPMessageTransmitter {
+    IERC20 public usdc;
+    ICCTPMessageTransmitter public cctpMessageTransmitter;
+    uint256 public nonce = 0;
+
+    
+    // Full message with header
+    struct Message {
+        uint32 version;
+        uint32 sourceDomain;
+        uint32 destinationDomain;
+        bytes32 recipient;
+        bytes32 sender;
+        bytes32 destinationCaller;
+        uint32 minFinalityThreshold;
+        bool isTokenTransfer;
+        uint256 tokenAmount;
+        bytes messageBody;
+    }
+
+    Message[] public messages;
+
+    constructor(address _usdc, address _cctpMessageTransmitter) {
+        usdc = IERC20(_usdc);
+        cctpMessageTransmitter = ICCTPMessageTransmitter(_cctpMessageTransmitter);
+    }
+
+    function sendMessage(
+        uint32 destinationDomain,
+        bytes32 recipient,
+        bytes32 destinationCaller,
+        uint32 minFinalityThreshold,
+        bytes memory messageBody
+    ) external override {
+        bytes32 nonceHash = keccak256(abi.encodePacked(nonce));
+        nonce++;
+
+        Message memory message = Message({
+            version: 1,
+            sourceDomain: 1,
+            destinationDomain: destinationDomain,
+            recipient: recipient,
+            sender: bytes32(uint256(uint160(msg.sender))),
+            destinationCaller: destinationCaller,
+            minFinalityThreshold: minFinalityThreshold,
+            isTokenTransfer: false,
+            tokenAmount: 0,
+            messageBody: messageBody
+        });
+        
+        messages.push(message);
+    };
+
+    function sendTokenTransferMessage(
+        uint32 destinationDomain,
+        bytes32 recipient,
+        bytes32 destinationCaller,
+        uint32 minFinalityThreshold,
+        uint256 tokenAmount,
+        bytes memory messageBody
+    ) external {
+        bytes32 nonceHash = keccak256(abi.encodePacked(nonce));
+        nonce++;
+
+        Message memory message = Message({
+            version: 1,
+            sourceDomain: 1,
+            destinationDomain: destinationDomain,
+            recipient: recipient,
+            sender: bytes32(uint256(uint160(msg.sender))),
+            destinationCaller: destinationCaller,
+            minFinalityThreshold: minFinalityThreshold,
+            isTokenTransfer: true,
+            tokenAmount: tokenAmount,
+            messageBody: messageBody
+        });
+        
+        messages.push(message);
+    };
+
+    function receiveMessage(bytes calldata message, bytes calldata attestation)
+        external
+        override
+        returns (bool) {
+        // For mock, assume we can decode and push, but simplified: just push the bytes as body or something
+        // To properly decode, we'd need the header parsing logic
+        // For now, emit or log, but to store, perhaps add a function later
+        return true;
+    }
+
+    function addMessage(Message memory msg) external {
+        messages.push(msg);
+    }
+
+    function removeFront() external returns (Message memory) {
+        require(messages.length > 0, "No messages");
+        Message memory removed = messages[0];
+        // Shift array
+        for (uint256 i = 0; i < messages.length - 1; i++) {
+            messages[i] = messages[i + 1];
+        }
+        messages.pop();
+        return removed;
+    }
+
+    function removeBack() external returns (Message memory) {
+        require(messages.length > 0, "No messages");
+        return messages.pop();
+    }
+
+    function getMessagesLength() external view returns (uint256) {
+        return messages.length;
+    }
+}
---
+++ contracts/contracts/mocks/crosschain/CCTPTokenMessengerMock.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import { ICCTPTokenMessenger } from "../../interfaces/cctp/ICCTP.sol";
+import { CCTPMessageTransmitterMock } from "./CCTPMessageTransmitterMock.sol";
+import { IERC20 } from "../../utils/InitializableAbstractStrategy.sol";
+
+/**
+ * @title Mock conctract simulating the functionality of the CCTPTokenMessenger contract
+ *        for the porposes of unit testing. 
+ * @author Origin Protocol Inc
+ */
+
+contract CCTPTokenMessengerMock is ICCTPTokenMessenger{
+    IERC20 public usdc;
+    CCTPMessageTransmitterMock public cctpMessageTransmitterMock;
+
+    constructor(address _usdc, address _cctpMessageTransmitterMock) {
+        usdc = IERC20(_usdc);
+        cctpMessageTransmitterMock = CCTPMessageTransmitterMock(_cctpMessageTransmitterMock);
+    }
+
+    function depositForBurn(
+        uint256 amount,
+        uint32 destinationDomain,
+        bytes32 mintRecipient,
+        address burnToken,
+        bytes32 destinationCaller,
+        uint256 maxFee,
+        uint32 minFinalityThreshold
+    ) external override {
+        revert("Not implemented");
+    };
+
+    function depositForBurnWithHook(
+        uint256 amount,
+        uint32 destinationDomain,
+        bytes32 mintRecipient,
+        address burnToken,
+        bytes32 destinationCaller,
+        uint256 maxFee,
+        uint32 minFinalityThreshold,
+        bytes memory hookData
+        ) external override {
+        require(burnToken == address(usdc), "Invalid burn token");
+
+        usdc.transferFrom(msg.sender, address(this), maxFee);
+        uint256 destinationAmount = amount - maxFee;
+        usdc.transferFrom(msg.sender, address(cctpMessageTransmitterMock), destinationAmount);
+        cctpMessageTransmitterMock.sendTokenTransferMessage(
+            destinationDomain,
+            mintRecipient,
+            destinationCaller,
+            minFinalityThreshold,
+            destinationAmount,
+            hookData
+        );
+    };
+
+    function getMinFeeAmount(uint256 amount) external view override returns (uint256) {
+        return 0;
+    };
+}
