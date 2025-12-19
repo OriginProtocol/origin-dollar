122fa3219f37eaa8565cb6fbc2b0e0c89e0e3f87
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-12-19 15:20:04 +0400

    Cleanup code

--- contracts/contracts/strategies/crosschain/AbstractCCTPIntegrator.sol
+++ contracts/contracts/strategies/crosschain/AbstractCCTPIntegrator.sol
@@ -8,14 +8,27 @@ import { ICCTPTokenMessenger, ICCTPMessageTransmitter, IMessageHandlerV2 } from 
 
 import { Governable } from "../../governance/Governable.sol";
 import { BytesHelper } from "../../utils/BytesHelper.sol";
-import { AbstractCCTPMessageRelayer } from "./AbstractCCTPMessageRelayer.sol";
 import "../../utils/Helpers.sol";
 
-abstract contract AbstractCCTPIntegrator is
-    Governable,
-    IMessageHandlerV2,
-    AbstractCCTPMessageRelayer
-{
+// CCTP Message Header fields
+// Ref: https://developers.circle.com/cctp/technical-guide#message-header
+uint8 constant VERSION_INDEX = 0;
+uint8 constant SOURCE_DOMAIN_INDEX = 4;
+uint8 constant SENDER_INDEX = 44;
+uint8 constant RECIPIENT_INDEX = 76;
+uint8 constant MESSAGE_BODY_INDEX = 148;
+
+// Message body V2 fields
+// Ref: https://developers.circle.com/cctp/technical-guide#message-body
+// Ref: https://github.com/circlefin/evm-cctp-contracts/blob/master/src/messages/v2/BurnMessageV2.sol
+uint8 constant BURN_MESSAGE_V2_VERSION_INDEX = 0;
+uint8 constant BURN_MESSAGE_V2_RECIPIENT_INDEX = 36;
+uint8 constant BURN_MESSAGE_V2_AMOUNT_INDEX = 68;
+uint8 constant BURN_MESSAGE_V2_MESSAGE_SENDER_INDEX = 100;
+uint8 constant BURN_MESSAGE_V2_FEE_EXECUTED_INDEX = 164;
+uint8 constant BURN_MESSAGE_V2_HOOK_DATA_INDEX = 228;
+
+abstract contract AbstractCCTPIntegrator is Governable, IMessageHandlerV2 {
     using SafeERC20 for IERC20;
 
     using BytesHelper for bytes;
@@ -23,11 +36,23 @@ abstract contract AbstractCCTPIntegrator is
     event CCTPMinFinalityThresholdSet(uint32 minFinalityThreshold);
     event CCTPFeePremiumBpsSet(uint32 feePremiumBps);
 
+    uint32 public constant CCTP_MESSAGE_VERSION = 1;
+    uint32 public constant ORIGIN_MESSAGE_VERSION = 1010;
+
+    // CCTP contracts
+    // This implementation assumes that remote and local chains have these contracts
+    // deployed on the same addresses.
+    ICCTPMessageTransmitter public immutable cctpMessageTransmitter;
+    ICCTPTokenMessenger public immutable cctpTokenMessenger;
+
     // USDC address on local chain
     address public immutable baseToken;
 
-    // Strategy address on destination chain
-    address public immutable destinationStrategy;
+    // Domain ID of the chain from which messages are accepted
+    uint32 public immutable peerDomainID;
+
+    // Strategy address on other chain
+    address public immutable peerStrategy;
 
     // CCTP params
     uint32 public minFinalityThreshold;
@@ -55,18 +80,18 @@ abstract contract AbstractCCTPIntegrator is
         address cctpTokenMessenger;
         address cctpMessageTransmitter;
         uint32 peerDomainID;
-        address destinationStrategy;
+        address peerStrategy;
         address baseToken;
     }
 
-    constructor(CCTPIntegrationConfig memory _config)
-        AbstractCCTPMessageRelayer(
-            _config.cctpMessageTransmitter,
-            _config.cctpTokenMessenger,
-            _config.peerDomainID
-        )
-    {
-        destinationStrategy = _config.destinationStrategy;
+    constructor(CCTPIntegrationConfig memory _config) {
+        cctpMessageTransmitter = ICCTPMessageTransmitter(
+            _config.cctpMessageTransmitter
+        );
+        cctpTokenMessenger = ICCTPTokenMessenger(_config.cctpTokenMessenger);
+        peerDomainID = _config.peerDomainID;
+
+        peerStrategy = _config.peerStrategy;
         baseToken = _config.baseToken;
 
         // Just a sanity check to ensure the base token is USDC
@@ -163,7 +188,7 @@ abstract contract AbstractCCTPIntegrator is
 
         // Extract address from bytes32 (CCTP stores addresses as right-padded bytes32)
         address senderAddress = address(uint160(uint256(sender)));
-        require(senderAddress == destinationStrategy, "Unknown Sender");
+        require(senderAddress == peerStrategy, "Unknown Sender");
 
         _onMessageReceived(messageBody);
 
@@ -190,9 +215,9 @@ abstract contract AbstractCCTPIntegrator is
         cctpTokenMessenger.depositForBurnWithHook(
             tokenAmount,
             peerDomainID,
-            bytes32(uint256(uint160(destinationStrategy))),
+            bytes32(uint256(uint160(peerStrategy))),
             address(baseToken),
-            bytes32(uint256(uint160(destinationStrategy))),
+            bytes32(uint256(uint160(peerStrategy))),
             maxFee,
             minFinalityThreshold,
             hookData
@@ -206,7 +231,7 @@ abstract contract AbstractCCTPIntegrator is
     {
         // uint32 bytes 0 to 4 is Origin message version
         // uint32 bytes 4 to 8 is Message type
-        return message.extractSlice(0, 4).decodeUint32();
+        return message.extractUint32(0);
     }
 
     function _getMessageType(bytes memory message)
@@ -216,7 +241,7 @@ abstract contract AbstractCCTPIntegrator is
     {
         // uint32 bytes 0 to 4 is Origin message version
         // uint32 bytes 4 to 8 is Message type
-        return message.extractSlice(4, 8).decodeUint32();
+        return message.extractUint32(4);
     }
 
     function _verifyMessageVersionAndType(
@@ -245,8 +270,8 @@ abstract contract AbstractCCTPIntegrator is
     function _sendMessage(bytes memory message) internal virtual {
         cctpMessageTransmitter.sendMessage(
             peerDomainID,
-            bytes32(uint256(uint160(destinationStrategy))),
-            bytes32(uint256(uint160(destinationStrategy))),
+            bytes32(uint256(uint160(peerStrategy))),
+            bytes32(uint256(uint160(peerStrategy))),
             minFinalityThreshold,
             message
         );
@@ -290,4 +315,119 @@ abstract contract AbstractCCTPIntegrator is
 
         return nonce;
     }
+
+    function _decodeMessageHeader(bytes memory message)
+        internal
+        pure
+        returns (
+            uint32 version,
+            uint32 sourceDomainID,
+            address sender,
+            address recipient,
+            bytes memory messageBody
+        )
+    {
+        version = message.extractUint32(VERSION_INDEX);
+        sourceDomainID = message.extractUint32(SOURCE_DOMAIN_INDEX);
+        // Address of MessageTransmitterV2 caller on source domain
+        sender = message.extractAddress(SENDER_INDEX);
+        // Address to handle message body on destination domain
+        recipient = message.extractAddress(RECIPIENT_INDEX);
+        messageBody = message.extractSlice(MESSAGE_BODY_INDEX, message.length);
+    }
+
+    function relay(bytes memory message, bytes memory attestation) external {
+        (
+            uint32 version,
+            uint32 sourceDomainID,
+            address sender,
+            address recipient,
+            bytes memory messageBody
+        ) = _decodeMessageHeader(message);
+
+        // Ensure that it's a CCTP message
+        require(
+            version == CCTP_MESSAGE_VERSION,
+            "Invalid CCTP message version"
+        );
+
+        // Ensure that the source domain is the peer domain
+        require(sourceDomainID == peerDomainID, "Unknown Source Domain");
+
+        // Ensure message body version
+        version = messageBody.extractUint32(BURN_MESSAGE_V2_VERSION_INDEX);
+
+        // TODO: what if the sender sends another type of a message not just the burn message?
+        bool isBurnMessageV1 = sender == address(cctpTokenMessenger);
+
+        if (isBurnMessageV1) {
+            // Handle burn message
+            require(
+                version == 1 &&
+                    messageBody.length >= BURN_MESSAGE_V2_HOOK_DATA_INDEX,
+                "Invalid burn message"
+            );
+
+            // Address of caller of depositForBurn (or depositForBurnWithCaller) on source domain
+            sender = messageBody.extractAddress(
+                BURN_MESSAGE_V2_MESSAGE_SENDER_INDEX
+            );
+
+            recipient = messageBody.extractAddress(
+                BURN_MESSAGE_V2_RECIPIENT_INDEX
+            );
+        } else {
+            // We handle only Burn message or our custom messagee
+            require(
+                version == ORIGIN_MESSAGE_VERSION,
+                "Unsupported message version"
+            );
+        }
+
+        require(sender == recipient, "Sender and recipient must be the same");
+        require(sender == peerStrategy, "Incorrect sender/recipient address");
+
+        // Relay the message
+        // This step also mints USDC and transfers it to the recipient wallet
+        bool relaySuccess = cctpMessageTransmitter.receiveMessage(
+            message,
+            attestation
+        );
+        require(relaySuccess, "Receive message failed");
+
+        if (isBurnMessageV1) {
+            bytes memory hookData = messageBody.extractSlice(
+                BURN_MESSAGE_V2_HOOK_DATA_INDEX,
+                messageBody.length
+            );
+
+            uint256 tokenAmount = messageBody.extractUint256(
+                BURN_MESSAGE_V2_AMOUNT_INDEX
+            );
+
+            uint256 feeExecuted = messageBody.extractUint256(
+                BURN_MESSAGE_V2_FEE_EXECUTED_INDEX
+            );
+
+            _onTokenReceived(tokenAmount - feeExecuted, feeExecuted, hookData);
+        }
+    }
+
+    /**
+     * @dev Called when the USDC is received from the CCTP
+     * @param tokenAmount The actual amount of USDC received (amount sent - fee executed)
+     * @param feeExecuted The fee executed
+     * @param payload The payload of the message (hook data)
+     */
+    function _onTokenReceived(
+        uint256 tokenAmount,
+        uint256 feeExecuted,
+        bytes memory payload
+    ) internal virtual;
+
+    /**
+     * @dev Called when the message is received
+     * @param payload The payload of the message
+     */
+    function _onMessageReceived(bytes memory payload) internal virtual;
 }
--- contracts/contracts/utils/BytesHelper.sol
+++ contracts/contracts/utils/BytesHelper.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
+uint256 constant UINT32_LENGTH = 4;
+uint256 constant UINT64_LENGTH = 8;
+uint256 constant UINT256_LENGTH = 32;
+uint256 constant ADDRESS_LENGTH = 32;
+
 library BytesHelper {
     /**
      * @dev Extract a slice from bytes memory
@@ -31,5 +36,40 @@ library BytesHelper {
     function decodeUint32(bytes memory data) internal pure returns (uint32) {
         require(data.length == 4, "Invalid data length");
         return uint32(uint256(bytes32(data)) >> 224);
+    }
+
+    function extractUint32(bytes memory data, uint256 start)
+        internal
+        pure
+        returns (uint32)
+    {
+        return decodeUint32(extractSlice(data, start, start + UINT32_LENGTH));
+    }
+
+    function decodeAddress(bytes memory data) internal pure returns (address) {
+        // We expect the data to be padded with 0s, so length is 32 not 20
+        require(data.length == 32, "Invalid data length");
+        return abi.decode(data, (address));
+    }
+
+    function extractAddress(bytes memory data, uint256 start)
+        internal
+        pure
+        returns (address)
+    {
+        return decodeAddress(extractSlice(data, start, start + ADDRESS_LENGTH));
+    }
+
+    function decodeUint256(bytes memory data) internal pure returns (uint256) {
+        require(data.length == 32, "Invalid data length");
+        return abi.decode(data, (uint256));
+    }
+
+    function extractUint256(bytes memory data, uint256 start)
+        internal
+        pure
+        returns (uint256)
+    {
+        return decodeUint256(extractSlice(data, start, start + UINT256_LENGTH));
     }
 }
