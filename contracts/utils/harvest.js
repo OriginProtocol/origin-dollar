a01eb4d81ee2f82e9f78336a7353c0795e1d9cfc
Nicholas Addison <nick@addisonbrown.com.au>
2025-12-30 13:14:10 +1100

    Fix Harvest Action

--- contracts/utils/harvest.js
+++ contracts/utils/harvest.js
@@ -1,4 +1,4 @@
-const { ethers } = require("ethers");
+const ethers = require("ethers");
 const { parseEther, formatUnits } = require("ethers/lib/utils");
 
 const addresses = require("./addresses");
