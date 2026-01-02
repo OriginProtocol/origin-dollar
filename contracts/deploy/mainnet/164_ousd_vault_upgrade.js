ebe97cddf7138cde9e6f9930167eba5d194ef7ad
Clément <clemmoller@gmail.com>
2026-01-02 12:55:52 +0100

    increase ousd vault upgrade number

--- contracts/deploy/mainnet/164_ousd_vault_upgrade.js
+++ contracts/deploy/mainnet/164_ousd_vault_upgrade.js
@@ -3,7 +3,7 @@ const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
 
 module.exports = deploymentWithGovernanceProposal(
   {
-    deployName: "159_ousd_vault_upgrade",
+    deployName: "164_ousd_vault_upgrade",
     forceDeploy: false,
     //forceSkip: true,
     reduceQueueTime: true,
