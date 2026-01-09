015bd561f5063db52449e92b49316555abad9f87
Domen Grabec <grabec@gmail.com>
2026-01-09 13:14:53 +0100

    simplify _withdraw

--- contracts/contracts/strategies/crosschain/CrossChainMasterStrategy.sol
+++ contracts/contracts/strategies/crosschain/CrossChainMasterStrategy.sol
@@ -92,8 +92,7 @@ contract CrossChainMasterStrategy is
         uint256 _amount
     ) external override onlyVault nonReentrant {
         require(_recipient == vaultAddress, "Only Vault can withdraw");
-
-        _withdraw(_asset, _recipient, _amount);
+        _withdraw(_asset, _amount);
     }
 
     /// @inheritdoc InitializableAbstractStrategy
@@ -107,7 +106,6 @@ contract CrossChainMasterStrategy is
 
         _withdraw(
             usdcToken,
-            vaultAddress,
             _remoteBalance > MAX_TRANSFER_AMOUNT
                 ? MAX_TRANSFER_AMOUNT
                 : _remoteBalance
@@ -249,18 +247,12 @@ contract CrossChainMasterStrategy is
     /**
      * @dev Send a withdraw request to the remote strategy
      * @param _asset Address of the asset to withdraw
-     * @param _recipient Address to receive the withdrawn asset
      * @param _amount Amount of the asset to withdraw
      */
-    function _withdraw(
-        address _asset,
-        address _recipient,
-        uint256 _amount
-    ) internal virtual {
+    function _withdraw(address _asset, uint256 _amount) internal virtual {
         require(_asset == usdcToken, "Unsupported asset");
         // Withdraw at least 1 USDC
         require(_amount > 1e6, "Withdraw amount too small");
-        require(_recipient == vaultAddress, "Only Vault can withdraw");
         require(
             _amount <= remoteStrategyBalance,
             "Withdraw amount exceeds remote strategy balance"
