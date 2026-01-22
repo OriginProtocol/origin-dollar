b1c3b5b172357be84e107e014113e1a83a2ea14c
Nicholas Addison <nick@addisonbrown.com.au>
2026-01-23 10:53:29 +1100

    Deploy script for gov prop to call resetFirstDeposit on the staking strategy

---
+++ contracts/deploy/mainnet/166_reset_first_deposit.js
@@ -0,0 +1,32 @@
+const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
+
+module.exports = deploymentWithGovernanceProposal(
+  {
+    deployName: "166_reset_first_deposit",
+    forceDeploy: false,
+    reduceQueueTime: true,
+    deployerIsProposer: false,
+    proposalId: "",
+  },
+  async () => {
+    // Current contracts
+    const cStakingStrategyProxy = await ethers.getContract(
+      "CompoundingStakingSSVStrategyProxy"
+    );
+    const cStakingStrategy = await ethers.getContractAt(
+      "CompoundingStakingSSVStrategy",
+      cStakingStrategyProxy.address
+    );
+
+    return {
+      name: `Allow the initial 1 ETH validator deposits to be made two at a time.`,
+      actions: [
+        {
+          contract: cStakingStrategy,
+          signature: "resetFirstDeposit()",
+          args: [],
+        },
+      ],
+    };
+  }
+);
