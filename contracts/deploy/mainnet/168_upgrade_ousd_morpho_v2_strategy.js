cfe1e1606902a9ced97205add8539193e03d2a70
Domen Grabec <grabec@gmail.com>
2026-02-03 11:51:30 +0100

    add deploy file

---
+++ contracts/deploy/mainnet/168_upgrade_ousd_morpho_v2_strategy.js
@@ -0,0 +1,37 @@
+const addresses = require("../../utils/addresses");
+const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
+
+module.exports = deploymentWithGovernanceProposal(
+  {
+    deployName: "168_upgrade_ousd_morpho_v2_strategy",
+    forceDeploy: false,
+    reduceQueueTime: true,
+    deployerIsProposer: false,
+    proposalId: "",
+  },
+  async ({ deployWithConfirmation }) => {
+    const cVaultProxy = await ethers.getContract("VaultProxy");
+    const cOUSDMorphoV2StrategyProxy = await ethers.getContract(
+      "OUSDMorphoV2StrategyProxy"
+    );
+
+    const dMorphoV2StrategyImpl = await deployWithConfirmation(
+      "MorphoV2Strategy",
+      [
+        [addresses.mainnet.MorphoOUSDv2Vault, cVaultProxy.address],
+        addresses.mainnet.USDC,
+      ]
+    );
+
+    return {
+      name: "Upgrade OUSD Morpho V2 strategy implementation",
+      actions: [
+        {
+          contract: cOUSDMorphoV2StrategyProxy,
+          signature: "upgradeTo(address)",
+          args: [dMorphoV2StrategyImpl.address],
+        },
+      ],
+    };
+  }
+);
