f8cf49e1620efbc2b5e3ad38def48401de646192
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2026-02-16 21:06:09 +0400

    more delete

--- contracts/deploy/mainnet/000_mock.js
+++ contracts/deploy/mainnet/000_mock.js
@@ -9,21 +9,6 @@ const {
   bytecode: FACTORY_BYTECODE,
 } = require("@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json");
 
-const {
-  abi: ROUTER_ABI,
-  bytecode: ROUTER_BYTECODE,
-} = require("@uniswap/v3-periphery/artifacts/contracts/SwapRouter.sol/SwapRouter.json");
-
-const {
-  abi: MANAGER_ABI,
-  bytecode: MANAGER_BYTECODE,
-} = require("@uniswap/v3-periphery/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json");
-
-const {
-  abi: QUOTER_ABI,
-  bytecode: QUOTER_BYTECODE,
-} = require("@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json");
-
 const deployMocks = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployerAddr, governorAddr } = await getNamedAccounts();
@@ -50,7 +35,6 @@ const deployMocks = async ({ getNamedAccounts, deployments }) => {
   const mockWETH = await ethers.getContract("MockWETH");
   await replaceContractAt(addresses.mainnet.WETH, mockWETH);
   await hardhatSetBalance(addresses.mainnet.WETH, "999999999999999");
-  const weth = await ethers.getContractAt("MockWETH", addresses.mainnet.WETH);
 
   await deploy("MockOGN", {
     from: deployerAddr,
@@ -86,33 +70,6 @@ const deployMocks = async ({ getNamedAccounts, deployments }) => {
 
   const usdc = await ethers.getContract("MockUSDC");
 
-  // Deploy mock chainlink oracle price feeds.
-  await deploy("MockChainlinkOracleFeedUSDS", {
-    from: deployerAddr,
-    contract: "MockChainlinkOracleFeed",
-    args: [parseUnits("1", 8).toString(), 8], // 1 USDS = 1 USD, 8 digits decimal.
-  });
-  await deploy("MockChainlinkOracleFeedUSDT", {
-    from: deployerAddr,
-    contract: "MockChainlinkOracleFeed",
-    args: [parseUnits("1", 8).toString(), 8], // 1 USDT = 1 USD, 8 digits decimal.
-  });
-  await deploy("MockChainlinkOracleFeedUSDC", {
-    from: deployerAddr,
-    contract: "MockChainlinkOracleFeed",
-    args: [parseUnits("1", 8).toString(), 8], // 1 USDC = 1 USD, 8 digits decimal.
-  });
-  await deploy("MockChainlinkOracleFeedNonStandardToken", {
-    from: deployerAddr,
-    contract: "MockChainlinkOracleFeed",
-    args: [parseUnits("1", 8).toString(), 8], // 1 = 1 USD, 8 digits decimal.
-  });
-  await deploy("MockChainlinkOracleFeedWETHETH", {
-    from: deployerAddr,
-    contract: "MockChainlinkOracleFeed",
-    args: [parseUnits("1", 18).toString(), 18], // 1 WETH = 1 ETH , 18 digits decimal.
-  });
-
   // Deploy mock Uniswap router
   await deploy("MockUniswapRouter", {
     from: deployerAddr,
@@ -127,46 +84,12 @@ const deployMocks = async ({ getNamedAccounts, deployments }) => {
     contract: "MockNonRebasing",
   });
 
-  const factory = await deploy("MockUniswapV3Factory", {
+  await deploy("MockUniswapV3Factory", {
     from: deployerAddr,
     contract: {
       abi: FACTORY_ABI,
       bytecode: FACTORY_BYTECODE,
     },
-  });
-
-  await deploy("MockUniswapV3Router", {
-    from: deployerAddr,
-    contract: {
-      abi: ROUTER_ABI,
-      bytecode: ROUTER_BYTECODE,
-    },
-    args: [factory.address, weth.address],
-  });
-
-  await deploy("MockUniswapV3NonfungiblePositionManager", {
-    from: deployerAddr,
-    contract: {
-      abi: MANAGER_ABI,
-      bytecode: MANAGER_BYTECODE,
-    },
-    /*
-     * The last constructor argument should be of type "NonfungibleTokenPositionDescriptor", but
-     * the bytecode of that seems to be corrupt - hardhat doesn't want to deploy it. Shouldn't be a
-     * problem as long as we don't call the `tokenUri` function:
-     * https://github.com/Uniswap/uniswap-v3-periphery/blob/79c708f357df69f7b3a494467e0f501810a11146/contracts/NonfungiblePositionManager.sol#L189-L192
-     *
-     */
-    args: [factory.address, weth.address, factory.address],
-  });
-
-  await deploy("MockUniswapV3Quoter", {
-    from: deployerAddr,
-    contract: {
-      abi: QUOTER_ABI,
-      bytecode: QUOTER_BYTECODE,
-    },
-    args: [factory.address, weth.address],
   });
 
   await deploy("MockBeaconProofs", { from: deployerAddr });
