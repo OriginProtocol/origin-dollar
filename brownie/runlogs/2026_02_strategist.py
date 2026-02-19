740a7a62cc2f703ce2a2c709d0e77569d135ae59
Nicholas Addison <nick@addisonbrown.com.au>
2026-02-19 16:12:19 +1100

    Run log Allocate 100k USDC to the Cross-chain strategy

--- brownie/runlogs/2026_02_strategist.py
+++ brownie/runlogs/2026_02_strategist.py
@@ -78,3 +78,38 @@ def main():
         {'from': STRATEGIST}
       )
     )
+
+# -------------------------------------------
+# Feb 19 2026 - Allocate 100k USDC to the Cross-chain strategy
+# -------------------------------------------
+from world import *
+def main():
+  with TemporaryForkForReallocations() as txs:
+    txs.append(vault_core.rebase({'from': MULTICHAIN_STRATEGIST}))
+    txs.append(vault_value_checker.takeSnapshot({'from': MULTICHAIN_STRATEGIST}))
+
+    txs.append(vault_admin.withdrawFromStrategy(
+      MORPHO_OUSD_V2_STRAT,
+      [usdc],
+      [138_000 * 10**6],
+      {'from': MULTICHAIN_STRATEGIST}
+    ))
+
+    txs.append(vault_admin.depositToStrategy(
+      CROSSCHAIN_MORPHO_V2_BASE_MASTER_STRATEGY,
+      [usdc],
+      [100_000 * 10**6],
+      {'from': MULTICHAIN_STRATEGIST}
+    ))
+    vault_change = vault_core.totalValue() - vault_value_checker.snapshots(MULTICHAIN_STRATEGIST)[0]
+    supply_change = ousd.totalSupply() - vault_value_checker.snapshots(MULTICHAIN_STRATEGIST)[1]
+    profit = vault_change - supply_change
+    txs.append(vault_value_checker.checkDelta(profit, (1 * 10**18), vault_change, (1 * 10**18), {'from': MULTICHAIN_STRATEGIST}))    
+    print("-----")
+    print("Profit", "{:.6f}".format(profit / 10**18), profit)
+    print("USDC supply change", "{:.6f}".format(supply_change / 10**18), supply_change)
+    print("Vault Change", "{:.6f}".format(vault_change / 10**18), vault_change)
+    print("-----")
+
+    vault_usdc = usdc.balanceOf(VAULT_PROXY_ADDRESS)
+    print("Vault USDC", "{:.6f}".format(vault_usdc / 10**6), vault_usdc)
