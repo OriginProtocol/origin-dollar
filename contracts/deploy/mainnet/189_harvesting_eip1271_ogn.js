7481780d608b6f11e2475058ff02cf47ee3e4ed8
Clément <55331875+clement-ux@users.noreply.github.com>
2026-04-02 17:17:14 +0200

    Deploy 187 and 188: CoW harvesting EIP-1271 for OETH and OGN (#2858)
    * Deploy 187 and 188
    
    * prettier
    
    * Move 187/188 to 188/189

---
+++ contracts/deploy/mainnet/189_harvesting_eip1271_ogn.js
@@ -0,0 +1,33 @@
+const addresses = require("../../utils/addresses");
+const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
+
+module.exports = deploymentWithGovernanceProposal(
+  {
+    deployName: "189_harvesting_eip1271_ogn",
+    forceDeploy: true,
+    reduceQueueTime: true,
+    deployerIsProposer: false,
+    proposalId: "",
+  },
+  async ({ deployWithConfirmation }) => {
+    const botAddress = "0x7aD5C91914DE8D24316038567856d26BabE24C9E";
+
+    await deployWithConfirmation(
+      "harvesting_eip1271_ogn",
+      [
+        addresses.multichainStrategist,
+        botAddress,
+        addresses.composableCoW,
+        addresses.GPv2VaultRelayer,
+      ],
+      "HarvestingEIP1271"
+    );
+
+    const cHarvestingEIP1271 = await ethers.getContract("HarvestingEIP1271");
+    console.log(`HarvestingEIP1271 deployed to ${cHarvestingEIP1271.address}`);
+
+    return {
+      actions: [],
+    };
+  }
+);
