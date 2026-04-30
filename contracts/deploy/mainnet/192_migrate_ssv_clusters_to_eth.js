c28e62528532080c43e8e415f3a859165121f525
Nicholas Addison <nick@addisonbrown.com.au>
2026-04-30 21:06:29 +1000

    Updated Claude file with SSV cluster payment details

--- contracts/deploy/mainnet/192_migrate_ssv_clusters_to_eth.js
+++ contracts/deploy/mainnet/192_migrate_ssv_clusters_to_eth.js
@@ -1,21 +1,33 @@
 const { deploymentWithGovernanceProposal } = require("../../utils/deploy");
 const { getClusterInfo, splitOperatorIds } = require("../../utils/ssv");
 
+// ETH Payment estimator for SSV Clusters
+// https://ssv-eth-forecasting.vercel.app/
+
 const strategyConfigs = [
   {
     proxyName: "NativeStakingSSVStrategy2Proxy",
     contractName: "NativeStakingSSVStrategy",
     operatorids: "752,753,754,755",
+    // Another 63 validators will be migrated early May
+    // The remaining validators can be consolidated at the end of May
+    // 40 days with current number of validators should be more than enough
+    ethValue: 0.2498,
   },
   {
     proxyName: "NativeStakingSSVStrategy3Proxy",
     contractName: "NativeStakingSSVStrategy",
     operatorids: "338,339,340,341",
+    // No ETH as this cluster no longer has any validators
+    // Need to upgrade to claim the SSV left in the cluster
+    ethValue: 0,
   },
   {
     proxyName: "CompoundingStakingSSVStrategyProxy",
     contractName: "CompoundingStakingSSVStrategy",
     operatorids: "2070,2071,2072,2073",
+    // This gives 90 days operations with the current validator balances.
+    ethValue: 0.597136,
   },
 ];
 
@@ -29,7 +41,6 @@ module.exports = deploymentWithGovernanceProposal(
     proposalId: "",
   },
   async ({ ethers }) => {
-    const ethValue = ethers.utils.parseEther("1");
     const { chainId } = await ethers.provider.getNetwork();
     const actions = [];
 
@@ -45,6 +56,9 @@ module.exports = deploymentWithGovernanceProposal(
         operatorids: operatorIds.join(","),
         ownerAddress: strategy.address,
       });
+      const ethValue = strategyConfig.ethValue
+        ? ethers.utils.parseEther(strategyConfig.ethValue.toString())
+        : 0;
 
       actions.push({
         contract: strategy,
