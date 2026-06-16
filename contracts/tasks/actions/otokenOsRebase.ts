8f08d4ecda6172358d94fe3323f01c134165a0d4
Domen Grabec <grabec@gmail.com>
2026-06-16 14:35:09 +0200

    add dripper collet to the sonic rebase

--- contracts/tasks/actions/otokenOsRebase.ts
+++ contracts/tasks/actions/otokenOsRebase.ts
@@ -5,16 +5,37 @@ const GAS_MULTIPLIER = 1.1;
 
 action({
   name: "otokenOsRebase",
-  description: "Rebase OS on Sonic",
+  description: "Collect OS dripper and rebase OS on Sonic",
   chains: [146],
   run: async ({ signer, log }) => {
     const ethers = hre.ethers;
+    const osDripperProxy = await ethers.getContract("OSonicDripperProxy");
     const oSonicVaultProxy = await ethers.getContract("OSonicVaultProxy");
+    const osDripper = await ethers.getContractAt(
+      "IDripper",
+      osDripperProxy.address
+    );
     const oSonicVault = await ethers.getContractAt(
       "IVault",
       oSonicVaultProxy.address
     );
+
+    const osDripperWithSigner = osDripper.connect(signer);
     const oSonicVaultWithSigner = oSonicVault.connect(signer);
+
+    // OS collect with gas estimation + 10% buffer
+    log.info("Estimating gas for OS collect");
+    const osCollectGas = await osDripperWithSigner.estimateGas.collect();
+    const osCollectGasLimit = osCollectGas
+      .mul(Math.floor(GAS_MULTIPLIER * 100))
+      .div(100);
+    const osCollectTx = await osDripperWithSigner.collect({
+      gasLimit: osCollectGasLimit,
+    });
+    await logTxDetails(
+      osCollectTx,
+      `collect (gasLimit: ${osCollectGasLimit.toString()})`
+    );
 
     // OS rebase with gas estimation + 10% buffer
     log.info("Estimating gas for OS rebase");
