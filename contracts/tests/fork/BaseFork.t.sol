08b2e0a7e77eddcf6c31ca04e44582d92abdb3a2
Domen Grabec <grabec@gmail.com>
2026-07-20 20:37:01 +0200

    fix error message

--- contracts/tests/fork/BaseFork.t.sol
+++ contracts/tests/fork/BaseFork.t.sol
@@ -6,8 +6,8 @@ import {Base} from "tests/Base.t.sol";
 
 abstract contract BaseFork is Base {
     function _createAndSelectForkMainnet() internal virtual {
-        // Check if the MAINNET_URL is set.
-        require(vm.envExists("MAINNET_PROVIDER_URL"), "MAINNET_URL not set");
+        // Check if the MAINNET_PROVIDER_URL is set.
+        require(vm.envExists("MAINNET_PROVIDER_URL"), "MAINNET_PROVIDER_URL not set");
 
         // Create and select a fork.
         forkIdMainnet = vm.envExists("FORK_BLOCK_NUMBER_MAINNET")
@@ -17,8 +17,8 @@ abstract contract BaseFork is Base {
     }
 
     function _createAndSelectForkBase() internal virtual {
-        // Check if the BASE_URL is set.
-        require(vm.envExists("BASE_PROVIDER_URL"), "BASE_URL not set");
+        // Check if the BASE_PROVIDER_URL is set.
+        require(vm.envExists("BASE_PROVIDER_URL"), "BASE_PROVIDER_URL not set");
 
         // Create and select a fork.
         forkIdBase = vm.envExists("FORK_BLOCK_NUMBER_BASE")
@@ -28,8 +28,8 @@ abstract contract BaseFork is Base {
     }
 
     function _createAndSelectForkArbitrum() internal virtual {
-        // Check if the ARBITRUM_URL is set.
-        require(vm.envExists("ARBITRUM_PROVIDER_URL"), "ARBITRUM_URL not set");
+        // Check if the ARBITRUM_PROVIDER_URL is set.
+        require(vm.envExists("ARBITRUM_PROVIDER_URL"), "ARBITRUM_PROVIDER_URL not set");
 
         // Create and select a fork.
         forkIdArbitrum = vm.envExists("FORK_BLOCK_NUMBER_ARBITRUM")
@@ -39,8 +39,8 @@ abstract contract BaseFork is Base {
     }
 
     function _createAndSelectForkHyperEVM() internal virtual {
-        // Check if the HYPEREVM_URL is set.
-        require(vm.envExists("HYPEREVM_PROVIDER_URL"), "HYPEREVM_URL not set");
+        // Check if the HYPEREVM_PROVIDER_URL is set.
+        require(vm.envExists("HYPEREVM_PROVIDER_URL"), "HYPEREVM_PROVIDER_URL not set");
 
         // Create and select a fork.
         forkIdHyperEVM = vm.envExists("FORK_BLOCK_NUMBER_HYPEREVM")
